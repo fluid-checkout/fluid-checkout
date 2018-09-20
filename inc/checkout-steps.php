5ca284e9db004ae2319dca8600d8c607a41cdae8
Diego Versiani <diegoversiani@gmail.com>
2018-09-19 23:29:15 -0300

    Add todo comment to remove svg icon and theme specific classes.

--- inc/checkout-steps.php
+++ inc/checkout-steps.php
@@ -54,6 +54,7 @@ class FluidCheckoutSteps extends FluidCheckout {
    * @param [String] $button_html Place Order button html.
    */
   public function add_back_button_order_button_html( $button_html ) {
+    // TODO: Remove svg icon and theme specific classes from button
     $actions_html = '<div class="wfc-actions"><a href="#wfc-wrapper" class="wfc-prev button button-grey-clear button-icon button-icon--left button--big">Back <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-left"><polyline points="15 18 9 12 15 6"></polyline></svg></a> ' . $button_html . '</div>';
     return $actions_html;
   }
