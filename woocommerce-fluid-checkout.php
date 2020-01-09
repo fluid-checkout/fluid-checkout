97eaa1d7ad1a559453707f9466c737ea0114eeb4
Diego Versiani <diegoversiani@gmail.com>
2020-01-10 00:09:17 +0800

    Change version to 1.0.10

--- woocommerce-fluid-checkout.php
+++ woocommerce-fluid-checkout.php
@@ -4,7 +4,7 @@ Plugin Name: WooCommerce Fluid Checkout
 Plugin URI: https://fluidweb.co/
 Description: A simple multi-step checkout fluid experience for any WooCommerce store.
 Text Domain: woocommerce-fluid-checkout
-Version: 1.0.10-dev-1
+Version: 1.0.10
 Author: Fluidweb.co
 Author URI: https://fluidweb.co/
 License: GPLv2
@@ -48,8 +48,8 @@ class FluidCheckout {
   public static $directory_url;
   const PLUGIN = 'WooCommerce Fluid Checkout';
 	// Version is updated automatically by gulp
-	const VERSION = '1.0.10-dev-1';
-	const ASSET_VERSION = '-1010-dev-1';
+	const VERSION = '1.0.10';
+	const ASSET_VERSION = '-1010';
 
 
 
