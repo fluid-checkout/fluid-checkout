2dd41424281d14e8cce1b478babf7c189c5e3a11
Diego Versiani <diegoversiani@gmail.com>
2020-07-31 19:49:59 +0100

    Add multi-step-enhanced layout structure

--- inc/layouts/multi-step-enhanced/checkout-layout.php
+++ inc/layouts/multi-step-enhanced/checkout-layout.php
@@ -27,21 +27,18 @@ class FluidCheckoutLayout_MultiStepEnhanced extends FluidCheckout {
 		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_assets' ), 10 );
 		
 		// // Template loader
-		// add_filter( 'woocommerce_locate_template', array( $this, 'locate_template' ), 20, 3 );
+		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template' ), 30, 3 );
 
-		// // Steps display order
-		// add_action( 'wfc_checkout_before_steps', array( $this, 'output_checkout_progress_bar' ), 10 );
-		// add_action( 'wfc_checkout_steps', array( $this, 'output_step_customer_contact' ), 10 );
-		// add_action( 'wfc_checkout_steps', array( $this, 'output_step_shipping' ), 50 );
-		// add_action( 'wfc_checkout_steps', array( $this, 'output_step_payment' ), 100 );
-
-		// // Payment
-		// remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
-		// add_action( 'wfc_checkout_payment', 'woocommerce_checkout_payment', 20 );
+		// Payment
+		remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
+		remove_action( 'wfc_checkout_payment', array( FluidCheckoutLayout_MultiStep::instance(), 'output_order_review' ), 10 );
+		remove_action( 'wfc_checkout_payment', array( FluidCheckoutLayout_MultiStep::instance(), 'output_checkout_place_order' ), 30 );
+		remove_action( 'woocommerce_order_button_html', array( FluidCheckoutLayout_MultiStep::instance(), 'get_payment_step_actions_html' ), 20 );
 		
 		// Order Review
-        add_action( 'wfc_checkout_after_steps', array( $this, 'output_checkout_order_review_wrapper' ), 10 );
-        
+		add_action( 'wfc_checkout_after_steps', array( $this, 'output_checkout_order_review_wrapper' ), 10 );
+		add_action( 'wfc_checkout_order_review_wrapper', array( FluidCheckoutLayout_MultiStep::instance(), 'output_order_review' ), 10 );
+		add_action( 'wfc_checkout_order_review_wrapper', array( FluidCheckoutLayout_MultiStep::instance(), 'output_checkout_place_order' ), 30 );
 		
 	}
 
@@ -106,131 +103,14 @@ class FluidCheckoutLayout_MultiStepEnhanced extends FluidCheckout {
 
 
 	/**
-	 * Output start tag for a checkout step.
+	 * Output order review section wrapper
 	 */
-	public function output_step_start_tag( $step_label ) {
+	public function output_checkout_order_review_wrapper() {
 		?>
-		<section class="wfc-frame" data-label="<?php echo esc_attr( $step_label ) ?>">
-		<?php
-	}
-	/**
-	 * Output end tag for a checkout step.
-	 */
-	public function output_step_end_tag() {
-		?>
-		</section>
-		<?php
-	}
-
-
-
-	/**
-	 * Output the checkout progress bar
-	 */
-	public function output_checkout_progress_bar() {
-		?>
-		<div class="wfc-checkout-progress-bar wfc-row wfc-header">
-			<div id="wfc-progressbar"><?php echo apply_filters( 'wfc_progressbar_steps_placeholder', '<div class="wfc-step current"></div><div class="wfc-step"></div><div class="wfc-step"></div>' ); ?></div>
+		<div class="wfc-checkout-order-review-wrapper">
+			<?php do_action( 'wfc_checkout_order_review_wrapper' ) ?>
 		</div>
 		<?php
-	}
-
-
-
-	/**
-	 * Output step: Contact Details
-	 */
-	public function output_step_customer_contact() {
-		$this->output_step_start_tag( apply_filters( 'wfc_billing_step_title', __( 'Billing', 'woocommerce-fluid-checkout' ) ) );
-		do_action( 'woocommerce_checkout_before_customer_details' );
-		do_action( 'woocommerce_checkout_billing' );
-		echo $this->get_billing_step_actions_html();
-		$this->output_step_end_tag();
-	}
-
-
-
-	/**
-	 * Output step: Shipping
-	 */
-	public function output_step_shipping() {
-		$this->output_step_start_tag( apply_filters( 'wfc_shipping_step_title', __( 'Shipping', 'woocommerce-fluid-checkout' ) ) );
-		do_action( 'woocommerce_checkout_shipping' );
-		do_action( 'woocommerce_checkout_after_customer_details' );
-		echo $this->get_shipping_step_actions_html();
-		$this->output_step_end_tag();
-	}
-
-
-
-	/**
-	 * Output step: Payment
-	 */
-	public function output_step_payment() {
-		$this->output_step_start_tag( apply_filters( 'wfc_payment_step_title', __( 'Payment', 'woocommerce-fluid-checkout' ) ) );
-		do_action( 'wfc_checkout_payment' );
-		$this->output_step_end_tag();
-	}
-
-
-
-	/**
-	 * Output checkout place order button
-	 */
-	public function output_checkout_place_order() {
-		wc_get_template(
-			'checkout/place-order.php',
-			array(
-				'checkout'           => WC()->checkout(),
-				'order_button_text'  => apply_filters( 'woocommerce_order_button_text', __( 'Place order', 'woocommerce' ) ),
-			)
-		);
-	}
-
-
-
-	/**
-	 * Output Order Review
-	 */
-	public function output_checkout_order_review() {
-		wc_get_template(
-			'checkout/order-review.php',
-			array(
-				'checkout'           => WC()->checkout(),
-				'order_review_title' => apply_filters( 'wfc_order_review_title', __( 'Your order', 'woocommerce' ) ),
-			)
-		);
-	}
-
-
-
-	/**
-	 * Add back button html to place order button on checkout.
-	 */
-	public function get_billing_step_actions_html() {
-		$actions_html = '<div class="wfc-actions"><button class="wfc-next button alt">' . __( 'Proceed to Shipping', 'woocommerce-fluid-checkout' ) . '</button></div>';
-		return apply_filters( 'wfc_billing_step_actions_html', $actions_html );
-	}
-
-
-
-	/**
-	 * Add back button html to place order button on checkout.
-	 */
-	public function get_shipping_step_actions_html() {
-		$actions_html = '<div class="wfc-actions"><button class="wfc-prev">' . _x( 'Back', 'Previous step button', 'woocommerce-fluid-checkout' ) . '</button> <button class="wfc-next button alt">' . __( 'Proceed to Payment', 'woocommerce-fluid-checkout' ) . '</button></div>';
-		return apply_filters( 'wfc_shipping_step_actions_html', $actions_html );
-	}
-
-
-
-	/**
-	 * Add back button html to place order button on checkout.
-	 * @param [String] $button_html Place Order button html.
-	 */
-	public function get_payment_step_actions_html( $button_html ) {
-		$actions_html = '<div class="wfc-actions"><button class="wfc-prev">' . _x( 'Back', 'Previous step button', 'woocommerce-fluid-checkout' ) . '</button> ' . $button_html . '</div>';
-		return apply_filters( 'wfc_payment_step_actions_html', $actions_html, $button_html );
 	}
 
 }
