04a713c8c89129bde623b0921108209a9784a8b5
Diego Versiani <diegoversiani@gmail.com>
2020-07-31 02:40:59 +0100

    Rename scripts and styles for multi step layout

--- inc/layouts/multi-step/checkout-layout.php
+++ inc/layouts/multi-step/checkout-layout.php
@@ -105,7 +105,7 @@ class FluidCheckoutLayout_MultiStep extends FluidCheckout {
 	 * Add page body class for feature detection
 	 */
 	public function add_body_class( $classes ) {
-		return array_merge( $classes, array( 'has-wfc-checkout-layout', 'has-wfc-checkout-layout--default' ) );
+		return array_merge( $classes, array( 'has-wfc-checkout-layout', 'has-wfc-checkout-layout--multi-step' ) );
 	}
 
 
@@ -114,11 +114,10 @@ class FluidCheckoutLayout_MultiStep extends FluidCheckout {
 	 * Enqueue scripts
 	 */
 	public function enqueue_assets() {
-		// wp_enqueue_style( 'wfc-checkout-layout', self::$directory_url . 'css/checkout-layout'. self::$asset_version . '.css', NULL, NULL );
-		wp_enqueue_style( 'wfc-checkout-layout--default', self::$directory_url . 'css/checkout-layout--default'. self::$asset_version . '.css', NULL, NULL );
+		wp_enqueue_style( 'wfc-checkout-layout--multi-step', self::$directory_url . 'css/checkout-layout--multi-step'. self::$asset_version . '.css', NULL, NULL );
 		
-		wp_enqueue_script( 'wfc-checkout-layout--multi-step', self::$directory_url . 'js/checkout-layout'. self::$asset_version . '.js', NULL, NULL, true );
-		wp_add_inline_script( 'wfc-checkout-layout--multi-step', 'window.addEventListener("load",function(){CheckoutSteps.init();})' );
+		wp_enqueue_script( 'wfc-checkout-steps', self::$directory_url . 'js/checkout-steps'. self::$asset_version . '.js', NULL, NULL, true );
+		wp_add_inline_script( 'wfc-checkout-steps', 'window.addEventListener("load",function(){CheckoutSteps.init();})' );
 	}
 
 
