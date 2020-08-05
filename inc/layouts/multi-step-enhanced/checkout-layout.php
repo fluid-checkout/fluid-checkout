7018cf518a043b078d930862d66ad0c3dea6e19d
Diego Versiani <diegoversiani@gmail.com>
2020-08-05 23:54:00 +0100

    Fix contact section title filter hook

--- inc/layouts/multi-step-enhanced/checkout-layout.php
+++ inc/layouts/multi-step-enhanced/checkout-layout.php
@@ -178,7 +178,7 @@ class FluidCheckoutLayout_MultiStepEnhanced extends FluidCheckout {
 			array(
 				'checkout'          => WC()->checkout(),
 				'display_fields'    => $contact_fields,
-				'section_title'  	=> apply_filters( 'wfc_checkout_contact_details_step_ section_title', __( 'Contact details', 'woocommerce-fluid-checkout' ) ),
+				'section_title'  	=> apply_filters( 'wfc_checkout_contact_step_section_title', __( 'Contact details', 'woocommerce-fluid-checkout' ) ),
 				'user_data'			=> $user_data,
 			)
 		);
