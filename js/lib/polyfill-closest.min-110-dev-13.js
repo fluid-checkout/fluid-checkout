e3a447b61d9d719b17c03c96fd815c5ae9c6b5c0
Diego Versiani <diegoversiani@gmail.com>
2020-08-08 17:13:33 +0100

    1.1.0-dev-13

---
+++ js/lib/polyfill-closest.min-110-dev-13.js
@@ -0,0 +1,2 @@
+Element.prototype.matches||(Element.prototype.matches=Element.prototype.msMatchesSelector||Element.prototype.webkitMatchesSelector),Element.prototype.closest||(Element.prototype.closest=function(e){var t=this;do{if(t.matches(e))return t;t=t.parentElement||t.parentNode}while(null!==t&&1===t.nodeType);return null});
+//# sourceMappingURL=maps/polyfill-closest.min.js.map
