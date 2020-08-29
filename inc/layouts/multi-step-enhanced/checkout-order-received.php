bc43fd0ed761eb62eaa9fb0e38f2205c513a6d90
Diego Versiani <diegoversiani@gmail.com>
2020-08-29 16:27:15 +0100

    Add order received widget areas

--- inc/layouts/multi-step-enhanced/checkout-order-received.php
+++ inc/layouts/multi-step-enhanced/checkout-order-received.php
@@ -5,7 +5,6 @@
  */
 class FluidCheckout_OrderReceived extends FluidCheckout {
 
-	private $address_book_entries_per_user = array();
 
 
 	/**
@@ -34,6 +33,11 @@ class FluidCheckout_OrderReceived extends FluidCheckout {
 	 */
 	public function hooks() {
 		add_filter( 'body_class', array( $this, 'add_body_class' ) );
+
+		// Widget Areas
+		add_action( 'widgets_init', array( $this, 'register_order_received_widgets_areas' ), 50 );
+		add_action( 'woocommerce_before_thankyou', array( $this, 'output_sidebar_order_received_before' ), 50 );
+		add_action( 'woocommerce_thankyou', array( $this, 'output_sidebar_order_review_outside' ), 50 );
 	}
 
 
@@ -48,6 +52,51 @@ class FluidCheckout_OrderReceived extends FluidCheckout {
 		$classes[] = 'has-wfc-order-received';
 
 		return $classes;
+	}
+
+
+
+	/**
+	 * Register widget areas for the checkout pages
+	 */
+	function register_order_received_widgets_areas() {
+		register_sidebar( array(
+			'name'			=> __( 'Order Confirmation - Before', 'lobsteranywhere-customizations' ),
+			'id'			=> 'wfc_order_received_before',
+			'description'	=> __( 'Display widgets on order confirmation page before the order details.', 'lobsteranywhere-customizations' ),
+			'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'	=> '</aside>',
+			'before_title'	=> '<h5 class="widget-title">',
+			'after_title'	=> '</h5>',
+		) );
+
+		register_sidebar( array(
+			'name'			=> __( 'Order Confirmation - After', 'lobsteranywhere-customizations' ),
+			'id'			=> 'wfc_order_received_after',
+			'description'	=> __( 'Display widgets after the order confirmation page after the order details.', 'lobsteranywhere-customizations' ),
+			'before_widget'	=> '<aside id="%1$s" class="widget %2$s">',
+			'after_widget'	=> '</aside>',
+			'before_title'	=> '<h5 class="widget-title">',
+			'after_title'	=> '</h5>',
+		) );
+	}
+
+	/**
+	 * Output widget area inside order review section
+	 */
+	function output_sidebar_order_received_before() {
+		if ( is_active_sidebar( 'wfc_order_received_before' ) ) :
+			dynamic_sidebar( 'wfc_order_received_before' );
+		endif;
+	}
+
+	/**
+	 * Output widget area outside order review section
+	 */
+	function output_sidebar_order_review_outside() {
+		if ( is_active_sidebar( 'wfc_order_received_after' ) ) :
+			dynamic_sidebar( 'wfc_order_received_after' );
+		endif;
 	}
 
 }
