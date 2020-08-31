d73f4c428bbd5e975cb88c4d61685bd08b0c3ff5
Diego Versiani <diegoversiani@gmail.com>
2020-08-31 14:44:02 +0100

    Test billing and shipping address values to show "same as shipping address" at order received details

--- inc/layouts/multi-step-enhanced/checkout-order-received.php
+++ inc/layouts/multi-step-enhanced/checkout-order-received.php
@@ -38,6 +38,11 @@ class FluidCheckout_OrderReceived extends FluidCheckout {
 		add_action( 'widgets_init', array( $this, 'register_order_received_widgets_areas' ), 50 );
 		add_action( 'woocommerce_before_thankyou', array( $this, 'output_sidebar_order_received_before' ), 50 );
 		add_action( 'woocommerce_thankyou', array( $this, 'output_sidebar_order_review_outside' ), 50 );
+
+		// Order Details
+		add_filter( 'woocommerce_get_order_item_totals', array( $this, 'move_shipping_address_order_received_details_before_payment' ), 10, 3 );
+		add_filter( 'woocommerce_get_order_item_totals', array( $this, 'add_shipping_address_order_received_details' ), 20, 3 );
+		add_filter( 'woocommerce_get_order_item_totals', array( $this, 'add_billing_address_order_received_details' ), 20, 3 );
 	}
 
 
@@ -97,6 +102,89 @@ class FluidCheckout_OrderReceived extends FluidCheckout {
 		if ( is_active_sidebar( 'wfc_order_received_after' ) ) :
 			dynamic_sidebar( 'wfc_order_received_after' );
 		endif;
+	}
+
+
+
+
+
+	/**
+	 * Move order item details position for shipping row to before payment method
+	 */
+	public function move_shipping_address_order_received_details_before_payment( $total_rows, $order, $tax_display ) {
+		// Bail if shipping or payment method not present
+		if ( ! array_key_exists( 'shipping', $total_rows ) || ! array_key_exists( 'payment_method', $total_rows ) ) { return; }
+
+		// Get shipping row values
+		$shipping_row = $total_rows[ 'shipping' ];
+
+		// Get token position
+		$position_index = array_search( 'payment_method', array_keys( $total_rows ) ) - 1;
+
+		// Insert after token position
+		$new_total_rows  = array_slice( $total_rows, 0, $position_index );
+		$new_total_rows[ 'shipping' ] = $shipping_row;
+		$new_total_rows = array_merge( $new_total_rows, array_slice( $total_rows, $position_index, count( $total_rows ) ) );
+	
+		return $new_total_rows;
+	}
+	
+	/**
+	 * Add shipping address to order details totals.
+	 */
+	public function add_shipping_address_order_received_details( $total_rows, $order, $tax_display ) {
+		// Get token position
+		$position_index = array_search( 'shipping', array_keys( $total_rows ) ) + 1;
+	
+		// Insert after token position
+		$new_total_rows  = array_slice( $total_rows, 0, $position_index );
+		$new_total_rows[ 'shipping_address' ] = array(
+			'label' => __( 'Shipping address:', 'woocommerce-fluid-checkout' ),
+			'value' => $order->get_formatted_shipping_address(),
+		);
+		$new_total_rows = array_merge( $new_total_rows, array_slice( $total_rows, $position_index, count( $total_rows ) ) );
+	
+		return $new_total_rows;
+	}
+
+	/**
+	 * Add billing address to order details totals.
+	 */
+	public function add_billing_address_order_received_details( $total_rows, $order, $tax_display ) {
+		// Get token position
+		$position_index = array_search( 'payment_method', array_keys( $total_rows ) ) + 1;
+
+		// Get billing address value
+		$billing_address_value = $order->get_formatted_billing_address();
+
+		// Check for billing address same as shipping address
+		$same_as_shipping = true;
+		$address_fields =  apply_filters( 'wfc_address_fields_for_same_as_shipping_address', array( 'first_name', 'last_name', 'address_1', 'address_2', 'city', 'state', 'country', 'postcode' ) );
+		if ( class_exists( 'FluidCheckout_AddressBook' ) && get_option( 'wfc_enable_address_book', 'true' ) === 'true' ) {
+			$shipping_address = $order->get_address( 'shipping' );
+			$billing_address = $order->get_address( 'billing' );
+			
+			foreach ( $billing_address as $field_key => $value ) {
+				if ( in_array( $field_key, $address_fields ) && array_key_exists( $field_key, $shipping_address ) && $billing_address[ $field_key ] != $shipping_address[ $field_key ] ) {
+					$same_as_shipping = false;
+					break;
+				}
+			}
+		
+			if ( $same_as_shipping ) {
+				$billing_address_value = __( 'Same as shipping address', 'woocommerce-fluid-checkout' );
+			}
+		}
+	
+		// Insert after token position
+		$new_total_rows  = array_slice( $total_rows, 0, $position_index );
+		$new_total_rows[ 'billing_address' ] = array(
+			'label' => __( 'Billing address:', 'woocommerce-fluid-checkout' ),
+			'value' => $billing_address_value,
+		);
+		$new_total_rows = array_merge( $new_total_rows, array_slice( $total_rows, $position_index, count( $total_rows ) ) );
+	
+		return $new_total_rows;
 	}
 
 }
