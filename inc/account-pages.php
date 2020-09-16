5570e6d7fa4441808019ff28fdcebb0de4cedf1d
Diego Versiani <diegoversiani@gmail.com>
2020-09-16 19:42:15 +0100

    Add structure for account pages title and dashboard items

--- inc/account-pages.php
+++ inc/account-pages.php
@@ -18,9 +18,6 @@ class FluidCheckout_AccountPages extends FluidCheckout {
 	 * Initialize class.
 	 */
 	public function init() {
-		// Bail if account pages not enabled
-		if ( get_option( 'wfc_enable_account_pages', 'true' ) !== 'true' ) { return; }
-
 		$this->hooks();
 	}
 
@@ -30,14 +27,23 @@ class FluidCheckout_AccountPages extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Body Class
 		add_filter( 'body_class', array( $this, 'add_body_class' ) );
+
+		// Default dashboard page
+		add_action( 'woocommerce_account_dashboard', array( $this, 'output_default_account_dashboard_content' ) );
 		
 		// Endpoint title
 		if ( get_option( 'wfc_enable_account_pages_endpoint_title', 'true' ) === 'true' ) {
 			remove_filter( 'the_title', 'wc_page_endpoint_title' );
 			add_filter( 'the_title', array( $this, 'maybe_add_endpoint_title' ), 50, 2 );
 		}
-			
+		
+		// Dashboard
+		if ( get_option( 'wfc_enable_account_pages', 'true' ) === 'true' ) {
+			remove_action( 'woocommerce_account_dashboard', array( $this, 'output_default_account_dashboard_content' ) );
+			add_action( 'woocommerce_account_dashboard', array( $this, 'output_account_dashboard_items' ) );
+		}
 	}
 
 
@@ -55,7 +61,109 @@ class FluidCheckout_AccountPages extends FluidCheckout {
 
 		return $classes;
 	}
-	
+
+
+
+
+
+	/**
+	 * Output the default dashboard page content
+	 */
+	public function output_default_account_dashboard_content() {
+		wc_get_template(
+			'myaccount/dashboard-default.php'
+		);
+	}
+
+
+
+	/**
+	 * Get data for account dashboard endpoint items
+	 */
+	public function get_account_dashboard_endpoint_items( $remove_missing = true ) {
+		// Get WooCommerce account endpoints
+		$wc_nav_endpoints = wc_get_account_menu_items();
+
+		// Get endpoint details from settings or default
+		$dashboard_endpoints = get_option( 'wfc_account_dashboard_endpoints', array(
+			'orders'		=> array(
+				'label' 		=> $wc_nav_endpoints[ 'orders' ],
+				'description' 	=> __( 'View orders&apos; details and statuses', 'woocommerce-fluid-checkout' ),
+				'image_type'	=> 'image_icon_class',
+				'image_icon_class' 	=> 'fa fa-shopping-basket',
+			),
+			'downloads'		=> array(
+				'label' 		=> $wc_nav_endpoints[ 'downloads' ],
+				'description' 	=> __( 'View your downloadable files', 'woocommerce-fluid-checkout' ),
+				'image_type'	=> 'image_icon_class',
+				'image_icon_class' 	=> 'fa fa-file-archive',
+			),
+			'edit-address'		=> array(
+				'label' 		=> $wc_nav_endpoints[ 'edit-address' ],
+				'description' 	=> _n( 'Manage saved shipping and billing address', 'Edit your address', (int) wc_shipping_enabled(), 'woocommerce-fluid-checkout' ),
+				'image_type'	=> 'image_icon_class',
+				'image_icon_class' 	=> 'fa fa-address-book',
+			),
+			'payment-methods'		=> array(
+				'label' 		=> $wc_nav_endpoints[ 'payment-methods' ],
+				'description' 	=> __( 'Manage saved payment information', 'woocommerce-fluid-checkout' ),
+				'image_type'	=> 'image_icon_class',
+				'image_icon_class' 	=> 'fa fa-credit-card',
+			),
+			'edit-account'		=> array(
+				'label' 		=> $wc_nav_endpoints[ 'edit-account' ],
+				'description' 	=> __( 'Update your personal information and password', 'woocommerce-fluid-checkout' ),
+				'image_type'	=> 'image_icon_class',
+				'image_icon_class' 	=> 'fa fa-user',
+			),
+		) );
+
+		// Remove missing WooCommerce endpoints
+		if ( $remove_missing ) {
+			foreach ( $dashboard_endpoints as $endpoint_id => $endpoint ) {
+				if ( ! array_key_exists( $endpoint_id, $wc_nav_endpoints ) ) {
+					unset( $dashboard_endpoints[ $endpoint_id ] );
+				}
+			}
+		}
+
+		return apply_filters( 'wfc_account_dashboard_endpoints', $dashboard_endpoints );
+	}
+
+
+
+	/**
+	 * Get image html for an account dashboard endpoint item
+	 */
+	public function get_account_dashboard_endpoint_image_html( $endpoint, $endpoint_values ) {
+		$html = '';
+
+		$image_type = $endpoint_values[ 'image_type' ];
+
+		if ( $image_type === 'image_url' ) {
+			$html = sprintf( '<img src="%s" alt="%s" aria-hidden="true" role="presentation" />', esc_url( $endpoint_values[ 'image_url' ] ), esc_html( $endpoint_values[ 'label' ] ) );
+		}
+		else if ( $image_type === 'image_icon_class' ) {
+			$html = sprintf( '<i class="%s" aria-hidden="true" role="presentation"></i>', esc_attr( $endpoint_values[ 'image_icon_class' ] ) );
+		}
+
+		return apply_filters( 'wfc_account_dashboard_endpoint_image_html', $html, $endpoint, $endpoint_values );
+	}
+
+
+
+	/**
+	 * Output dashboard page content with endpoint blocks
+	 */
+	public function output_account_dashboard_items() {
+		wc_get_template(
+			'myaccount/dashboard-endpoints.php',
+			array(
+				'dashboard_endpoints' => $this->get_account_dashboard_endpoint_items(),
+			)
+		);
+	}
+
 
 
 
---
+++ templates/myaccount/dashboard-endpoints.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * My Account Dashboard
+ *
+ * Shows the first intro screen on the account dashboard.
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/myaccount/dashboard-endpoints.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package woocommerce-fluid-checkout
+ * @version 1.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<nav class="wfc-dashboard-navigation">
+	<ul>
+		<?php foreach ( $dashboard_endpoints as $endpoint => $endpoint_values ) :
+			$image_html = FluidCheckout_AccountPages::instance()->get_account_dashboard_endpoint_image_html( $endpoint, $endpoint_values );
+			?>
+			<li class="wfc-dashboard-navigation__item <?php echo wc_get_account_menu_item_classes( $endpoint ); ?>">
+				<a href="<?php echo esc_url( wc_get_account_endpoint_url( $endpoint ) ); ?>">
+					<?php if ( ! empty( $image_html ) ) : ?>
+						<div class="wfc-dashboard-navigation__item-image"><?php echo $image_html; ?></div>
+					<?php endif; ?>
+					<div class="wfc-dashboard-navigation__item-label"><?php echo esc_html( $endpoint_values['label'] ); ?></div>
+					<div class="wfc-dashboard-navigation__item-description"><?php echo wp_kses_post( $endpoint_values['description'] ); ?></div>
+				</a>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+</nav>
