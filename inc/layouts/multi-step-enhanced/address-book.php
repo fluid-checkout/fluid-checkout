356604cfa2c9181beaea3a09cfb8b6bc5009bc47
Diego Versiani <diegoversiani@gmail.com>
2020-10-08 23:55:22 +0100

    Fix persisted addresses data get lost when order payment fails

--- inc/layouts/multi-step-enhanced/address-book.php
+++ inc/layouts/multi-step-enhanced/address-book.php
@@ -71,12 +71,12 @@ class FluidCheckout_AddressBook extends FluidCheckout {
 		// Persist shipping address selected
 		add_action( 'wp_ajax_wfc_set_shipping_address_selected_session', array( $this, 'set_shipping_address_selected_session' ) );
 		add_action( 'wp_ajax_nopriv_wfc_set_shipping_address_selected_session', array( $this, 'set_shipping_address_selected_session' ) );
-		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'unset_shipping_address_selected_session' ), 10, 1 );
+		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'maybe_unset_shipping_address_session_on_order_update' ), 10, 1 );
 
 		// Persist billing address selected
 		add_action( 'wp_ajax_wfc_set_billing_address_selected_session', array( $this, 'set_billing_address_selected_session' ) );
 		add_action( 'wp_ajax_nopriv_wfc_set_billing_address_selected_session', array( $this, 'set_billing_address_selected_session' ) );
-		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'unset_billing_address_selected_session' ), 10, 1 );
+		add_action( 'woocommerce_checkout_update_order_meta', array( $this, 'maybe_unset_billing_address_session_on_order_update' ), 10, 1 );
 
 		// Order Review Shipping Info
 		if ( get_option( 'wfc_order_review_display_shipping_address', 'true' ) === 'true' ) {
@@ -1122,6 +1122,33 @@ class FluidCheckout_AddressBook extends FluidCheckout {
 	 */
 	public function get_shipping_address_entry_checked_state( $address_entry, $first = false ) {
 		return $this->get_address_entry_checked_state( 'shipping', $address_entry, $first );
+	}
+
+
+
+
+	/**
+	 * Maybe unset shipping address session on order update.
+	 **/
+	public function maybe_unset_shipping_address_session_on_order_update( $order_id, $data ) {
+		$order = wc_get_order( $order_id );
+		
+		// Bail if order pending or failed
+		if ( ! $order || $order->has_status( array( 'pending', 'failed' ) ) ) { return; }
+
+		$this->unset_shipping_address_selected_session();
+	}
+
+	/**
+	 * Maybe unset billing address session on order update.
+	 **/
+	public function maybe_unset_billing_address_session_on_order_update( $order_id, $data ) {
+		$order = wc_get_order( $order_id );
+		
+		// Bail if order pending or failed
+		if ( ! $order || $order->has_status( array( 'pending', 'failed' ) ) ) { return; }
+
+		$this->unset_billing_address_selected_session();
 	}
 
 
