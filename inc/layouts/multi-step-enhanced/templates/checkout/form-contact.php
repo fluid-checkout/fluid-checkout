e676e4f29996ca448fbdf609d6b46c2e7b1a4ae6
Luiggi Bellincanta <youremail@domain.com>
2020-12-16 11:30:36 -0300

    Fix translation on form-contact.php and po/pot files

--- inc/layouts/multi-step-enhanced/templates/checkout/form-contact.php
+++ inc/layouts/multi-step-enhanced/templates/checkout/form-contact.php
@@ -77,7 +77,7 @@ defined( 'ABSPATH' ) || exit;
 
 			<p class="form-row form-row-wide create-account">
 				<label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-					<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ); ?> type="checkbox" name="createaccount" value="1" /> <span><?php esc_html_e( 'Create an account?', 'woocommerce' ); ?></span>
+					<input class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" id="createaccount" <?php checked( ( true === $checkout->get_value( 'createaccount' ) || ( true === apply_filters( 'woocommerce_create_account_default_checked', false ) ) ), true ); ?> type="checkbox" name="createaccount" value="1" /> <span><?php esc_html_e( 'Create an account', 'woocommerce-fluid-checkout' ); ?></span>
 				</label>
 			</p>
 
