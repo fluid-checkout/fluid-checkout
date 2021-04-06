4300d5633f6c9d62fc945400e105db806ea1770d
Diego Versiani <diegoversiani@gmail.com>
2021-04-06 16:55:51 +0200

    Fix template file version number for form-contact.php

--- templates/checkout/form-contact.php
+++ templates/checkout/form-contact.php
@@ -13,7 +13,7 @@
  * @see     https://docs.woocommerce.com/document/template-structure/
  * @see     templates/checkout/form-billing.php
  * @package woocommerce-fluid-checkout
- * @version 3.6.0
+ * @version 1.2.0
  */
 
 defined( 'ABSPATH' ) || exit;
