a3caa69b2f920303019a7b3930578644b7849232
Diego Versiani <diegoversiani@gmail.com>
2021-05-21 03:35:13 +0200

    Remove unnecessary empty option description

--- inc/admin/admin-settings-checkout.php
+++ inc/admin/admin-settings-checkout.php
@@ -297,7 +297,6 @@ class WC_Settings_FluidCheckout_Checkout extends WC_Settings_Page {
 					),
 					array(
 						'desc'          => __( 'Display the coupon codes section title', 'woocommerce' ),
-						'desc_tip'      => __( '', 'woocommerce-fluid-checkout' ),
 						'id'            => 'wfc_display_coupon_code_section_title',
 						'type'          => 'checkbox',
 						'default'       => 'no',
