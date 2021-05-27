2e4fd927b9038402c62ddd340f6258eca149a72d
Diego Versiani <diegoversiani@gmail.com>
2021-05-27 08:50:58 +0200

    Change default class to `button` for the apply code button

--- inc/checkout-coupon-codes.php
+++ inc/checkout-coupon-codes.php
@@ -105,7 +105,7 @@ class FluidCheckout_CouponCodes extends FluidCheckout {
 		$this->checkout_steps()->output_expansible_form_section_start_tag( $key, apply_filters( 'wfc_expansible_section_toggle_label_'.$key, sprintf( __( 'Add %s', 'woocommerce-fluid-checkout' ), strtolower( $coupon_code_field_label ) ) ) );
 		woocommerce_form_field( $key, $coupon_code_field_args );
 		?>
-		<button type="button" class="wfc-coupon-code__apply <?php echo esc_attr( apply_filters( 'wfc_coupon_code_apply_button_classes', '' ) ); ?>" data-apply-coupon-button><?php echo $coupon_code_button_label; ?></button>
+		<button type="button" class="wfc-coupon-code__apply <?php echo esc_attr( apply_filters( 'wfc_coupon_code_apply_button_classes', 'button' ) ); ?>" data-apply-coupon-button><?php echo $coupon_code_button_label; ?></button>
 		<?php
 		$this->checkout_steps()->output_expansible_form_section_end_tag();
 	}
