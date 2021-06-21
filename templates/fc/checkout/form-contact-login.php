3ff01cfa54c109dd52d921b6f394a8727a617670
Diego Versiani <diegoversiani@gmail.com>
2021-06-21 12:05:45 +0200

    Fix translation for log in action at contact step

--- templates/fc/checkout/form-contact-login.php
+++ templates/fc/checkout/form-contact-login.php
@@ -20,15 +20,13 @@ defined( 'ABSPATH' ) || exit;
 
 <div class="fc-contact-login">
 
-	<div class="fc-contact-login__cta-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_cta_text', __( 'Already have an account?', 'fluid-checkout' ) ) ); ?></div>
-
-	<div class="fc-contact-login__action">
-		<a href="<?php echo esc_url( add_query_arg( '_redirect', 'checkout', wc_get_account_endpoint_url( 'dashboard' ) ) ); ?>" class="fc-contact-login__action-login" data-flyout-toggle data-flyout-target="[data-flyout-checkout-login]"><?php echo esc_html( apply_filters( 'fc_checkout_login_button_label', __( 'Log in for faster checkout', 'Log in link label at checkout contact step', 'fluid-checkout' ) ) ); ?></a>
-	</div>
+	<div class="fc-contact-login__cta-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_cta_text', __( 'Already have an account?', 'fluid-checkout' ) ) ); ?> <a href="<?php echo esc_url( add_query_arg( '_redirect', 'checkout', wc_get_account_endpoint_url( 'dashboard' ) ) ); ?>" class="fc-contact-login__action" data-flyout-toggle data-flyout-target="[data-flyout-checkout-login]"><?php echo esc_html( apply_filters( 'fc_checkout_login_button_label', _x( 'Log in', 'Log in link label at checkout contact step', 'fluid-checkout' ) ) ); ?></a></div>
 
 	<div class="fc-contact-login__separator">
 		<?php if ( 'yes' === get_option( 'woocommerce_enable_guest_checkout' ) ) : ?>
-			<span class="fc-contact-login__separator-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_separator_text', __( 'Or continue as a guest', 'fluid-checkout' ) ) ); ?></span>
+			<span class="fc-contact-login__separator-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_separator_text', _x( 'Or continue as a guest', 'Log in separator label at for when guest checkout is disabled', 'fluid-checkout' ) ) ); ?></span>
+		<?php else: ?>
+			<span class="fc-contact-login__separator-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_separator_text', _x( 'Or continue checkout below', 'Log in separator label at for when guest checkout is disabled', 'fluid-checkout' ) ) ); ?></span>
 		<?php endif; ?>
 	</div>
 
