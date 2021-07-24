cdb3e5eb8bdb89d30a45ae4ded946991326e0164
Diego Versiani <diegoversiani@gmail.com>
2021-07-24 13:58:39 +0200

    Remove review request notice that was based on order count

--- inc/admin/admin-notice-review-request.php
+++ inc/admin/admin-notice-review-request.php
@@ -20,7 +20,6 @@ class FluidCheckout_AdminNotices_ReviewRequest extends FluidCheckout {
 	 */
 	public function hooks() {
 		add_action( 'fc_admin_notices', array( $this, 'add_review_request_timed_notice' ), 10 );
-		add_action( 'fc_admin_notices', array( $this, 'add_review_request_hundredth_order_notice' ), 10 );
 	}
 
 
@@ -43,24 +42,6 @@ class FluidCheckout_AdminNotices_ReviewRequest extends FluidCheckout {
 			'actions' => array(
 				sprintf( '<a href="%s" class="button button-primary" target="_blank">%s</a>', esc_url( 'https://wordpress.org/support/plugin/fluid-checkout/reviews/' ), __( 'Rate Us Now', 'fluid-checkout' ) ),
 				sprintf( '<a href="%s" class="button button-secondary" target="_blank">%s</a>', esc_url( 'https://wordpress.org/support/plugin/fluid-checkout/' ), __( 'I Need Help!', 'fluid-checkout' ) ),
-			),
-		);
-
-		return $notices;
-	}
-
-	
-
-	/**
-	 * Add plugin review request notice.
-	 */
-	public static function add_review_request_hundredth_order_notice( $notices = array() ) {
-		$notices[] = array(
-			'name' => 'review_request_100_orders',
-			'title' => __( 'Congratulations on your 100th order with Fluid Checkout! 🎉', 'fluid-checkout' ),
-			'description' => __( 'You have just reached 100 orders placed using Fluid Checkout! How does that feel?<br>Please give us a quick rating, it works as a boost for us to keep working on the plugin :)', 'fluid-checkout' ),
-			'actions' => array(
-				sprintf( '<a href="%s" class="button button-primary" target="_blank">%s</a>', esc_url( 'https://wordpress.org/support/plugin/fluid-checkout/reviews/' ), __( 'Rate Us Now', 'fluid-checkout' ) ),
 			),
 		);
 
