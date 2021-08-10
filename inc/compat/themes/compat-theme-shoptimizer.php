9d16b1d00a2d65ab4846243381add967b2a9f15b
Diego Versiani <diegoversiani@gmail.com>
2021-08-10 15:54:56 +0200

    Fix code styles for shoptimizer theme compat class

--- inc/compat/themes/compat-theme-shoptimizer.php
+++ inc/compat/themes/compat-theme-shoptimizer.php
@@ -21,7 +21,6 @@ class FluidCheckout_ThemeCompat_Shoptimizer extends FluidCheckout {
 	public function hooks() {
 		// Late hooks
 		add_action( 'init', array( $this, 'late_hooks' ), 100 );
-
 	}
 
 
@@ -31,8 +30,8 @@ class FluidCheckout_ThemeCompat_Shoptimizer extends FluidCheckout {
 	 */
 	public function late_hooks() {
 		// Removes Coupon code from woocommerce after checkout form
-		remove_action( 'woocommerce_after_checkout_form', 'woocommerce_checkout_coupon_form' );
-		remove_action( 'woocommerce_before_cart', 'shoptimizer_cart_progress' );
+		remove_action( 'woocommerce_after_checkout_form', 'woocommerce_checkout_coupon_form', 10 );
+		remove_action( 'woocommerce_before_cart', 'shoptimizer_cart_progress', 10 );
 		remove_action( 'woocommerce_before_checkout_form', 'shoptimizer_cart_progress', 5 );
 	}
 }
