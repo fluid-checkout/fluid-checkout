0b9adaa7fbbf0c2061104b3fd528850aa3348c95
Diego Versiani <diegoversiani@gmail.com>
2021-08-12 19:23:17 +0200

    Change version number on template file "checkout/review-order.php"

--- templates/checkout/review-order.php
+++ templates/checkout/review-order.php
@@ -13,7 +13,7 @@
  * @see https://docs.woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates
  * @version 3.8.0
- * @fc-version 1.2.0
+ * @fc-version 1.2.8
  */
 
 defined( 'ABSPATH' ) || exit;
