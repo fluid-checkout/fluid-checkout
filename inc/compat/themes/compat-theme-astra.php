06df21dc6145bd125faa48d64fcba3607f73017c
Diego Versiani <diegoversiani@gmail.com>
2021-08-13 16:55:55 +0200

    Add compatibility with Astra theme

---
+++ inc/compat/themes/compat-theme-astra.php
@@ -0,0 +1,39 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Astra (by Brainstorm Force).
+ */
+class FluidCheckout_ThemeCompat_Astra extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+	}
+
+
+
+    /**
+	 * Add or remove late hooks.
+	 */
+	public function very_late_hooks() {
+		// Remove shipping fields from the billing section added by the theme
+        // @see themes/astra/inc/compatibility/woocommerce/class-astra-woocommerce.php:LN759
+        remove_action( 'woocommerce_checkout_billing', array( WC()->checkout(), 'checkout_form_shipping' ) );
+	}
+
+}
+
+FluidCheckout_ThemeCompat_Astra::instance();
