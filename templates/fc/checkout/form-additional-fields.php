c828b7ddda7cbc7408b1d3a4867ae24bb87570de
Diego Versiani <diegoversiani@gmail.com>
2021-08-21 11:15:30 +0200

    Fix substep "Additional notes" being displayed even when all fields are removed

--- templates/fc/checkout/form-additional-fields.php
+++ templates/fc/checkout/form-additional-fields.php
@@ -26,11 +26,7 @@ defined( 'ABSPATH' ) || exit;
 
 	<?php if ( apply_filters( 'woocommerce_enable_order_notes_field', 'yes' === get_option( 'woocommerce_enable_order_comments', 'yes' ) ) ) : ?>
 
-		<?php if ( apply_filters( 'fc_show_additional_information_section_title', ( ! WC()->cart->needs_shipping() || wc_ship_to_billing_address_only() ) ) ) : ?>
-
-			<h3><?php esc_html_e( 'Additional information', 'woocommerce' ); ?></h3>
-
-		<?php endif; ?>
+		<?php // CHANGE: Remove section title "Additional Information" as the section's title is already added as the substep title ?>
 
 		<div class="woocommerce-additional-fields__field-wrapper">
 			<?php foreach ( $checkout->get_checkout_fields( 'order' ) as $key => $field ) : ?>
