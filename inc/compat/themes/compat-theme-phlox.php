1245b2074b0b9572ae851e6e7d6ef193e2135b19
Diego Versiani <diegoversiani@gmail.com>
2021-10-25 09:34:39 +0200

    Fix conditional for Phlox theme compatibility

--- inc/compat/themes/compat-theme-phlox.php
+++ inc/compat/themes/compat-theme-phlox.php
@@ -29,8 +29,8 @@ class FluidCheckout_ThemeCompat_Phlox extends FluidCheckout {
 	 * Dequeue theme scripts unnecessary on checkout page and that interfere with Fluid Checkout scripts.
 	 */
 	public function maybe_dequeue_scripts() {
-		// Bail if not on checkout or cart page or doing AJAX call
-		if ( ! function_exists( 'is_checkout' ) || ( ! is_checkout() && ! is_cart() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) ) { return; }
+		// Bail if not on checkout page
+		if ( ! function_exists( 'is_checkout' ) || ! is_checkout() || is_order_received_page() ) { return; }
 
 		// Bail if use of theme header is enabled
 		if ( 'yes' !== get_option( 'fc_hide_site_header_footer_at_checkout', 'yes' ) ) { return; }
