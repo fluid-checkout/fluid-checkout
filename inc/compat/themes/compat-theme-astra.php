2f1719ce97985dfa090f31577f0cb209d1c45fa3
Luiggi Bellincanta <youremail@domain.com>
2021-11-12 16:18:29 -0300

    First commit

--- inc/compat/themes/compat-theme-astra.php
+++ inc/compat/themes/compat-theme-astra.php
@@ -36,7 +36,6 @@ class FluidCheckout_ThemeCompat_Astra extends FluidCheckout {
 		// @see themes/astra/inc/compatibility/woocommerce/class-astra-woocommerce.php:LN759
 		remove_action( 'woocommerce_checkout_billing', array( WC()->checkout(), 'checkout_form_shipping' ) );
 	}
-
 }
 
 FluidCheckout_ThemeCompat_Astra::instance();
