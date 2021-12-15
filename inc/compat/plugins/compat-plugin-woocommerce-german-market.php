b0a8165f3035d2e3e0bb23fa2b5ff4588ad675c8
Diego Versiani <diegoversiani@gmail.com>
2021-12-15 20:21:11 +0100

    Fix styles to apply only when German Market has changed the place order button placement.

--- inc/compat/plugins/compat-plugin-woocommerce-german-market.php
+++ inc/compat/plugins/compat-plugin-woocommerce-german-market.php
@@ -32,11 +32,14 @@ class FluidCheckout_WooCommerceGermanMarket extends FluidCheckout {
 	 * Add or remove late hooks.
 	 */
 	public function late_hooks() {
-		// Has place order button changes
+		// Has place order button placement changes
 		if ( has_filter( 'woocommerce_order_button_html', array( 'WGM_Template', 'remove_order_button_html' ) ) ) {
+			// General
+			add_filter( 'body_class', array( $this, 'add_body_class_button_placement' ), 10 );
+
 			// Place order button on payment section
 			add_filter( 'woocommerce_order_button_html', array( $this, 'retrieve_order_button_html' ), 9998 );
-			add_filter( 'woocommerce_order_button_html', array( $this, 'recover_order_button_html' ), 10000 );
+			add_filter( 'woocommerce_order_button_html', array( $this, 'restore_order_button_html' ), 10000 );
 			
 			// Checkout widgets
 			if ( class_exists( 'FluidCheckout_CheckoutWidgetAreas' ) ) {
@@ -60,14 +63,37 @@ class FluidCheckout_WooCommerceGermanMarket extends FluidCheckout {
 		return $classes;
 	}
 
+	/**
+	 * Add page body class for feature detection.
+	 *
+	 * @param array $classes Classes for the body element.
+	 */
+	public function add_body_class_button_placement( $classes ) {
+		// Bail if not on checkout page.
+		if( ! function_exists( 'is_checkout' ) || ! is_checkout() || is_order_received_page() ){ return $classes; }
+
+		$classes[] = 'has-fc-compat-german-market-button-placement';
+		return $classes;
+	}
 
 
+
+	/**
+	 * Retrieve and save the contents of the place order button to a local variable.
+	 *
+	 * @param   string  $button_html  The place order button html.
+	 */
 	public function retrieve_order_button_html( $button_html ) {
 		self::$button_html = $button_html;
 		return $button_html;
 	}
 
-	public function recover_order_button_html( $button_html ) {
+	/**
+	 * Restore the contents of the place order button from the local variable after German Market has remove it.
+	 *
+	 * @param   string  $button_html  The place order button html.
+	 */
+	public function restore_order_button_html( $button_html ) {
 		return self::$button_html;
 	}
 
