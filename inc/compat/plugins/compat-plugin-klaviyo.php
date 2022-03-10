88a18cb182630734f97bfeffd2fd48012b0eeadd
Diego Versiani <diegoversiani@gmail.com>
2022-03-10 00:38:19 -0300

    Fix code styles

--- inc/compat/plugins/compat-plugin-klaviyo.php
+++ inc/compat/plugins/compat-plugin-klaviyo.php
@@ -19,20 +19,22 @@ class FluidCheckout_Klaviyo extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-        // Bail if klaviyo class not exits
-        if ( ! class_exists( 'WooCommerceKlaviyo' ) ) { return; }
+		// Bail if klaviyo class not exits
+		if ( ! class_exists( 'WooCommerceKlaviyo' ) ) { return; }
 
-        add_filter( 'fc_checkout_contact_step_field_ids', [ $this, 'add_checkout_field' ], 10 );
+		add_filter( 'fc_checkout_contact_step_field_ids', array( $this, 'add_checkout_field' ), 10 );
 	}
 
-    /**
+
+
+	/**
 	 * Add the klaviyo checkbox field to the checkout page.
 	 */
 	public function add_checkout_field( $args ) {
 		$args[] = 'kl_newsletter_checkbox';
 		$args[] = 'kl_sms_consent_checkbox';
 
-        return $args;
+		return $args;
 	}
 
 }
