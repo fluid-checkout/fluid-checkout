03d96649570b0fa3af5a4dc6a89fd18e7823f19a
Diego Versiani <diegoversiani@gmail.com>
2022-03-10 00:09:26 -0300

    Fix code styles

--- inc/compat/plugins/compat-plugin-woo-mailerlite.php
+++ inc/compat/plugins/compat-plugin-woo-mailerlite.php
@@ -19,27 +19,27 @@ class FluidCheckout_WooMailerLite extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-        // Bail if woo mailer lite class not exits
-        if ( ! class_exists( 'Woo_Mailerlite' ) ) { return; }
+		// Bail if woo mailer lite class not exits
+		if ( ! class_exists( 'Woo_Mailerlite' ) || ! function_exists( 'woo_ml_get_option' ) ) { return; }
 
-        $checkout          = woo_ml_get_option('checkout', 'no' );
-        $checkout_position = woo_ml_get_option('checkout_position', 'checkout_billing' );
+		$checkout          = woo_ml_get_option( 'checkout', 'no' );
+		$checkout_position = woo_ml_get_option( 'checkout_position', 'checkout_billing' );
 
-        if ( 'yes' === $checkout ) {
-            remove_action( 'woocommerce_' . $checkout_position, 'woo_ml_checkout_label', 20 );
+		if ( 'yes' === $checkout ) {
+			remove_action( 'woocommerce_' . $checkout_position, 'woo_ml_checkout_label', 20 );
 
-            $checkout_new_position = 'fc_checkout_contact_after_fields';
+			$checkout_new_position = 'fc_checkout_contact_after_fields';
 
-            if ( 'checkout_billing' === $checkout_position ) {
-                $checkout_new_position = 'fc_before_checkout_billing_only_form';
-            } else if ( 'checkout_shipping' === $checkout_position ) {
-                $checkout_new_position = 'woocommerce_after_order_notes';
-            } else if ( 'review_order_before_submit' === $checkout_position ) {
-                $checkout_new_position = 'woocommerce_review_order_before_submit';
-            }
+			if ( 'checkout_billing' === $checkout_position ) {
+				$checkout_new_position = 'fc_before_checkout_billing_only_form';
+			} else if ( 'checkout_shipping' === $checkout_position ) {
+				$checkout_new_position = 'woocommerce_after_order_notes';
+			} else if ( 'review_order_before_submit' === $checkout_position ) {
+				$checkout_new_position = 'woocommerce_review_order_before_submit';
+			}
 
-            add_action( $checkout_new_position, 'woo_ml_checkout_label', 20 );
-        }
+			add_action( $checkout_new_position, 'woo_ml_checkout_label', 20 );
+		}
 	}
 
 }
