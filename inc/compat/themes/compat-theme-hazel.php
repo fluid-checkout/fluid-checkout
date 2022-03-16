4b3a41abaef055555b8084fa555ef67e0f265746
Alamgir Hossain <alamgircse.bd@gmail.com>
2022-03-16 23:24:57 +0600

    - requested changes fixed

--- inc/compat/themes/compat-theme-hazel.php
+++ inc/compat/themes/compat-theme-hazel.php
@@ -40,7 +40,7 @@ class FluidCheckout_ThemeCompat_Hazel extends FluidCheckout {
 		if ( isset( $qode_options_hazel['logo_image'] ) && $qode_options_hazel['logo_image'] != '' ) { $logo_image = $qode_options_hazel['logo_image']; } else { $logo_image =  get_template_directory_uri().'/img/logo.png'; };
 		if ( isset( $qode_options_hazel['logo_image_dark'] ) && $qode_options_hazel['logo_image_dark'] != '' ) { $logo_image_dark = $qode_options_hazel['logo_image_dark']; } else { $logo_image_dark =  get_template_directory_uri().'/img/logo_black.png'; };
 		?>
-		<a href="<?php echo apply_filters( 'fc_checkout_header_logo_home_url', esc_url( home_url( '/' ) ) ); ?>">
+		<a href="<?php echo esc_url( apply_filters( 'fc_checkout_header_logo_home_url', home_url( '/' ) ) ); ?>">
 			<?php if ( ! empty( $logo_image_dark ) ) : ?>
 				<img src="<?php echo esc_url($logo_image_dark); ?>" alt="<?php esc_attr_e( 'Logo', 'hazel' ); ?>"/>
 			<?php else: ?>
