9b83c6091a1bf2ce1c56ab759bef05cbfbd8b9b6
Diego Versiani <diegoversiani@gmail.com>
2022-05-20 09:17:11 +0200

    Move place order experimental feature settings to the layout section, and remove the advanced settings section

--- inc/admin/admin-settings-general.php
+++ inc/admin/admin-settings-general.php
@@ -141,6 +141,15 @@ class WC_Settings_FluidCheckout_General_Settings extends WC_Settings_Page {
 					),
 
 					array(
+						'title'             => __( 'Place order', 'fluid-checkout' ),
+						'desc'              => __( '(Experimental) Display an additional "Place order" and terms checkbox in the last checkout step.', 'fluid-checkout' ),
+						'id'                => 'fc_enable_checkout_place_order_in_last_step',
+						'default'           => 'no',
+						'type'              => 'checkbox',
+						'autoload'          => false,
+					),
+
+					array(
 						'type' => 'sectionend',
 						'id'   => 'fc_checkout_layout_options',
 					),
@@ -280,27 +289,6 @@ class WC_Settings_FluidCheckout_General_Settings extends WC_Settings_Page {
 					array(
 						'type' => 'sectionend',
 						'id'   => 'fc_checkout_features_options',
-					),
-
-					array(
-						'title' => __( 'Advanced', 'fluid-checkout' ),
-						'type'  => 'title',
-						'desc'  => '',
-						'id'    => 'fc_checkout_advanced_layout_options',
-					),
-	
-					array(
-						'title'             => __( 'Place order', 'fluid-checkout' ),
-						'desc'              => __( '(Experimental) Display an additional "Place order" and terms checkbox in the last checkout step.', 'fluid-checkout' ),
-						'id'                => 'fc_enable_checkout_place_order_in_last_step',
-						'default'           => 'no',
-						'type'              => 'checkbox',
-						'autoload'          => false,
-					),
-	
-					array(
-						'type' => 'sectionend',
-						'id'   => 'fc_checkout_advanced_layout_options',
 					),
 
 				)
