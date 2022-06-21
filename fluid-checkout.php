dcfa263d96d7afa4857511718eda90984a7f63cd
Diego Versiani <diego@fluidweb.co>
2022-06-21 10:20:45 +0200

    Bump tested up to WooCommerce 6.6

--- fluid-checkout.php
+++ fluid-checkout.php
@@ -9,7 +9,7 @@ Version: 2.0.0-beta-4
 Author: Fluid Checkout
 Author URI: https://fluidcheckout.com/
 WC requires at least: 5.0
-WC tested up to: 6.5.1
+WC tested up to: 6.6
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 License: GPLv3
 
