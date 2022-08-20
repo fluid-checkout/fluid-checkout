04abcfd944b40bca5753692af842da98a1cf7e24
Diego Versiani <diego@fluidweb.co>
2022-08-20 10:23:58 +0200

    Partial commit: Remove paypal buttons added with annonymous function

--- inc/compat/plugins/compat-plugin-woocommerce-paypal-payments.php
+++ inc/compat/plugins/compat-plugin-woocommerce-paypal-payments.php
@@ -31,12 +31,16 @@ class FluidCheckout_WooCommercePayPalPayments extends FluidCheckout {
 
 		if ( $smart_button_module ) {
 			// PayPal buttons
-			remove_action( 'woocommerce_review_order_after_payment', array( $smart_button_module, 'button_renderer' ), 10 );
+			$checkout_button_renderer_hook = apply_filters( 'woocommerce_paypal_payments_checkout_button_renderer_hook', 'woocommerce_review_order_after_payment' );
+			remove_action( $checkout_button_renderer_hook, array( $smart_button_module, 'button_renderer' ), 10 );
+			// TODO: Check which version had this changed
+			$this->remove_action_for_closure( $checkout_button_renderer_hook, 10 );
 
 			// Place order position
 			$place_order_position = get_option( 'fc_checkout_place_order_position', 'below_payment_section' );
 			if ( 'below_payment_section' === $place_order_position || 'both_payment_and_order_summary' === $place_order_position ) {
 				// PayPal buttons
+				// TODO: Add action for closure since version that changed it
 				add_action( 'fc_output_step_payment', array( $smart_button_module, 'button_renderer' ), 110 );
 
 				// Widget area after submit button
@@ -47,6 +51,7 @@ class FluidCheckout_WooCommercePayPalPayments extends FluidCheckout {
 			}
 			else if ( 'below_order_summary' === $place_order_position ) {
 				// PayPal buttons
+				// TODO: Add action for closure since version that changed it
 				add_action( 'woocommerce_review_order_after_submit', array( $smart_button_module, 'button_renderer' ), 40 );
 			}
 			
