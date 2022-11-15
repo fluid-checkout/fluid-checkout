a3cb143674ff280830137f98c8b53d7fb0f7f19e
Diego Versiani <diego@fluidweb.co>
2022-11-15 12:19:50 +0100

    Output the location pickup button to the shipping methods section

--- inc/compat/plugins/compat-plugin-woo-ups-pickup.php
+++ inc/compat/plugins/compat-plugin-woo-ups-pickup.php
@@ -21,6 +21,9 @@ class FluidCheckout_WooUPSPickup extends FluidCheckout {
 	public function hooks() {
 		// Very late hooks
 		add_action( 'woocommerce_after_template_part', array( $this, 'very_late_hooks' ), 1 );
+
+		// Template file loader
+		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template' ), 100, 3 );
 	}
 
 	/**
@@ -34,8 +37,45 @@ class FluidCheckout_WooUPSPickup extends FluidCheckout {
 		if ( class_exists( WC_Ups_PickUps::METHOD_CLASS_NAME ) ) {
 			$ups_pickup_class_object = $this->get_object_by_class_name_from_hooks( WC_Ups_PickUps::METHOD_CLASS_NAME );
 			remove_action( 'woocommerce_after_template_part', array( $ups_pickup_class_object, 'review_order_shipping_pickups_location' ), 10 );
-			add_action( 'woocommerce_after_template_part', array( $this, 'review_order_shipping_pickups_location' ), 10, 4 );
+			add_action( 'fc_shipping_methods_after_packages_inside', array( $this, 'review_order_shipping_pickups_location' ), 10, 4 );
 		}
+	}
+
+
+
+	/**
+	 * Locate template files from this plugin.
+	 */
+	public function locate_template( $template, $template_name, $template_path ) {
+		global $woocommerce;
+		$_template = null;
+
+		// Set template path to default value when not provided
+		if ( ! $template_path ) { $template_path = $woocommerce->template_url; };
+
+		// Get plugin path
+		$plugin_path  = self::$directory_path . 'templates/compat/plugins/woo-ups-pickup/';
+
+		// Get the template from this plugin, if it exists
+		if ( file_exists( $plugin_path . $template_name ) ) {
+			$_template = $plugin_path . $template_name;
+
+			// Look for template file in the theme
+			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
+				$_template = locate_template( array(
+					$template_path . $template_name,
+					$template_name,
+				) );
+			}
+		}
+
+		// Use default template
+		if ( ! $_template ) {
+			$_template = $template;
+		}
+
+		// Return what we found
+		return $_template;
 	}
 
 
@@ -48,19 +88,29 @@ class FluidCheckout_WooUPSPickup extends FluidCheckout {
 	 * @param $located
 	 * @param $args
 	 */
-	public function review_order_shipping_pickups_location( $template_name, $template_path, $located, $args ) {
-		global $wp_query;
-		$ups_pickup_class_object = $this->get_object_by_class_name_from_hooks( WC_Ups_PickUps::METHOD_CLASS_NAME );
-		$is_ajax = defined( 'WC_DOING_AJAX' ) && 'update_order_review' === $wp_query->get( 'wc-ajax' );
-		if ( 'fc/cart/shipping-methods-available.php' == $template_name && ( is_checkout() || $is_ajax ) ) {
-			$plugin_dir = WP_PLUGIN_DIR . '/woo-ups-pickup';
-			if ( is_dir( $plugin_dir ) ) {
-				include_once( $plugin_dir . '/templates/pickup-location.php');
-				$helper = new Ups\Helper\Ups();
+	public function review_order_shipping_pickups_location() {
+		// Check if plugin folder exists
+		$plugin_dir = WP_PLUGIN_DIR . '/woo-ups-pickup';
+		if ( ! is_dir( $plugin_dir ) ) { return; }
 
-				if ( ! $helper->isPickUpsProductsPointsOverTheMax() && $ups_pickup_class_object->id == $args['chosen_method'] ) {
-					include_once( $plugin_dir . '/templates/pickup-button-html.php' );
-				}
+		// Get shipping method object
+		$ups_pickup_class_object = $this->get_object_by_class_name_from_hooks( WC_Ups_PickUps::METHOD_CLASS_NAME );
+
+		// Load pickup locations template
+		include_once( $plugin_dir . '/includes/templates/pickup-location.php');
+
+		// Get shipping packages
+		$packages = WC()->shipping->get_packages();
+		foreach ( $packages as $i => $package ) {
+			// Get chosen shipping method for the current package
+			$chosen_method = isset( WC()->session->chosen_shipping_methods[ $i ] ) ? WC()->session->chosen_shipping_methods[ $i ] : '';
+
+			// Maybe load pickup locations button template
+			$helper = new Ups\Helper\Ups();
+			if ( ! $helper->isPickUpsProductsPointsOverTheMax() && $ups_pickup_class_object->id == $chosen_method ) {
+				wc_get_template( 'cart/pickup-button-html.php', array(
+					'shipping_method'           => $ups_pickup_class_object,
+				) );
 			}
 		}
 	}
---
+++ templates/compat/plugins/woo-ups-pickup/cart/pickup-button-html.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * PickUP location html template
+ *
+ * @package     WC-Shipping-Ups-Pickups
+ * @author      O.P.S.I (International Handling) Ltd
+ * @category    Shipping
+ * @copyright   Copyright: (c) 2016-2018 O.P.S.I (International Handling) Ltd
+ * @license     http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License v3.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+do_action( 'woocommerce_before_pickup_button_html' ); ?>
+<tr class="pickups_location" onclick="window.PickupsSDK.onClick();return;" style="cursor: pointer;">
+    <?php // CHANGE: Use variable `$shipping_method` passed as a template parameter, instead of `$this` used on the original template file ?>
+    <th colspan="1"><?php echo __("Service powered of PickUP", WC_Ups_PickUps::TEXT_DOMAIN) ?><br /><div class="ups-pickups-checked"><?php echo $shipping_method->settings["service_description"] ?></div> </th>
+    <td class="update_totals_on_change">
+        <div class="ups-pickups-desc"><?php echo __("Click here to select your PickUP location", WC_Ups_PickUps::TEXT_DOMAIN) ?></div>
+        <div onclick="window.PickupsSDK.onClick();return;" class="ups-pickups ups-pickups-48" data-provider="as453ffadfgds"></div>
+        <div class="ups-pickups-info"></div>
+    </td>
+</tr>
+<?php do_action( 'woocommerce_after_pickup_button_html' ); ?>
