6fc15be93b4be8f02c67660bc08ab8e845608ff6
Diego Versiani <diego@fluidweb.co>
2022-12-29 19:18:22 +0100

    Normalize the way to undo early hooks

--- inc/compat/plugins/compat-plugin-klarna-checkout-for-woocommerce.php
+++ inc/compat/plugins/compat-plugin-klarna-checkout-for-woocommerce.php
@@ -19,43 +19,53 @@ class FluidCheckout_KlarnaCheckoutForWooCommerce extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Very late hooks
-		add_action( 'wp', array( $this, 'maybe_remove_shipping_phone_hooks' ), 5 );
-		add_action( 'wp', array( $this, 'very_late_hooks' ), 300 );
+		// Undo hooks
+		add_action( 'wp', array( $this, 'maybe_undo_hooks_early' ), 5 ); // Before very late hooks
+		add_action( 'wp', array( $this, 'maybe_undo_hooks' ), 300 ); // After very late hooks
 
 		// Persisted data
 		add_filter( 'fc_checkout_update_before_unload', array( $this, 'disable_updated_before_unload' ), 10 );
 	}
 
 	/**
-	 * Maybe remove shipping phone field hooks.
+	 * Maybe undo hooks early.
 	 */
-	public function maybe_remove_shipping_phone_hooks() {
+	public function maybe_undo_hooks_early() {
 		// Bail if not at checkout page, and not an AJAX request to update checkout fragment
 		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
 
 		// Bail if Klarna is not the selected payment method
 		if ( 'kco' !== FluidCheckout_Steps::instance()->get_selected_payment_method() ) { return; }
 
-		// Shipping phone
-		if ( class_exists( 'FluidCheckout_CheckoutShippingPhoneField' ) ) {
-			FluidCheckout_CheckoutShippingPhoneField::instance()->undo_hooks_early();
+		// Undo hooks from Fluid Checkout classes
+		$features_list = FluidCheckout::instance()->get_features_list();
+		$skip_undo_hooks_classes = array( 'FluidCheckout_CheckoutPageTemplate', 'FluidCheckout_CheckoutWidgetAreas' );
+		foreach ( $features_list as $class_name => $args ) {
+			// Skip some classes
+			if ( in_array( $class_name, $skip_undo_hooks_classes ) ) { continue; }
+
+			// Skip classes that don't exist or do not have the undo hooks function
+			if ( ! class_exists( $class_name ) || ! method_exists( $class_name::instance(), 'undo_hooks_early' ) ) { continue; }
+
+			// Run undo hooks
+			$class_name::instance()->undo_hooks_early();
 		}
 	}
 
 	/**
-	 * Add or remove very late hooks.
+	 * Maybe undo hooks.
 	 */
-	public function very_late_hooks() {
+	public function maybe_undo_hooks() {
 		// Bail if not at checkout page, and not an AJAX request to update checkout fragment
 		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
 
 		// Bail if Klarna is not the selected payment method
 		if ( 'kco' !== FluidCheckout_Steps::instance()->get_selected_payment_method() ) { return; }
 
-		// Switch payment method button
-		remove_action( 'pco_wc_before_snippet', 'pco_wc_show_another_gateway_button', 20 );
-		add_action( 'pco_wc_after_order_review', 'pco_wc_show_another_gateway_button', 20 );
+		// Undo enqueue hooks
+		if ( class_exists( 'FluidCheckout_PRO_Enqueue' ) ) {
+			FluidCheckout_PRO_Enqueue::instance()->undo_hooks();
+		}
 
 		// Undo hooks from Fluid Checkout classes
 		$features_list = FluidCheckout::instance()->get_features_list();
