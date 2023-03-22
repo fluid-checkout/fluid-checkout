2626c21cea5766d93ae258cde375ccdd66c101df
Diego Versiani <diego@fluidweb.co>
2023-03-22 15:15:10 +0100

    Clarify comment about step render condition callback.

--- inc/checkout-steps.php
+++ inc/checkout-steps.php
@@ -1092,7 +1092,7 @@ class FluidCheckout_Steps extends FluidCheckout {
 
 		// Iterate all steps and check if they should be rendered
 		foreach ( $_checkout_steps as $step_index => $step_args ) {
-			// Maybe remove the step from the list
+			// Maybe remove the step from the list if it has a render condition callback and it returns `false`.
 			if ( array_key_exists( 'render_condition_callback', $step_args ) && ! call_user_func( $step_args[ 'render_condition_callback' ] ) ) {
 				unset( $_checkout_steps[ $step_index ] );
 			}
