6c98c92761b1e6f7094efb934cd0e51438f3d071
Diego Versiani <diego@fluidweb.co>
2023-03-29 22:42:36 +0200

    Improved: Compatibility with theme Flatsome, correctly recover field focus when updating checkout fragments if the theme feature Float Labels is activated.

--- js-src/compat/themes/flatsome/float-labels.js
+++ js-src/compat/themes/flatsome/float-labels.js
@@ -36,8 +36,66 @@
 	var maybeRebuildFloatLabels = function() {
 		// Add jQuery event listeners
 		if ( window.floatlabels && typeof floatlabels.rebuild === 'function' ) {
+			// Get current focused element
+			var currentFocusedElement = document.activeElement;
+			var value = currentFocusedElement.value;
+			
+			// Rebuild floating labels
 			floatlabels.rebuild();
+			
+			// Re-focus element
+			maybe_refocus_element( currentFocusedElement, value );
 		}
+	}
+
+	/**
+	 * Re-focus and keep value of the element that was previously focused.
+	 */
+	var maybe_refocus_element = function( currentFocusedElement, currentValue ) {
+		// Bail if no element to focus
+		if ( null === currentFocusedElement ) { return; }
+
+		requestAnimationFrame( function() {
+			var elementToFocus;
+
+			// Try findind the `select2` focusable element
+			if ( currentFocusedElement.matches( '.fc-select2-field' ) ) {
+				elementToFocus = document.querySelector( '.form-row[id="' + currentFocusedElement.id + '"] .select2-selection' );
+			}
+			// Try findind the the current focused element after updating updated element by ID
+			else if ( currentFocusedElement.id ) {
+				elementToFocus = document.getElementById( currentFocusedElement.id );
+			}
+			// Try findind the updated element by classes
+			else if ( currentFocusedElement.getAttribute( 'name' ) ) {
+				var nameAttr = currentFocusedElement.getAttribute( 'name' );
+				elementToFocus = document.querySelector( '[name="'+nameAttr+'"]' );
+			}
+
+			// Try setting focus if element is found
+			if ( elementToFocus && elementToFocus !== document.activeElement ) {
+				elementToFocus.focus();
+
+				// Set keyboard track position back to that previously to update
+				setTimeout( function(){
+					console.log( elementToFocus.selectionStart, elementToFocus.selectionEnd );
+
+					// Try to set the same track position
+					if( null !== elementToFocus.selectionStart && null !== elementToFocus.selectionEnd ) {
+						if ( currentFocusedElement.selectionStart && currentFocusedElement.selectionEnd ) {
+							elementToFocus.selectionStart = currentFocusedElement.selectionStart;
+							elementToFocus.selectionEnd = currentFocusedElement.selectionEnd;
+						}
+						// Otherwise try set the track position to the end of the field
+						// @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/setSelectionRange
+						// @see https://html.spec.whatwg.org/multipage/input.html#concept-input-apply
+						else {
+							elementToFocus.selectionStart = elementToFocus.selectionEnd = Number.MAX_SAFE_INTEGER || 10000;
+						}
+					}
+				}, 0 );
+			}
+		} );
 	}
 
 
