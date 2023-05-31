d13425af51897625448ddd95254e94eefee45630
Diego Versiani <diego@fluidweb.co>
2023-05-31 10:17:09 +0200

    Remove custom buttons styles for theme Enfold.

--- inc/compat/themes/compat-theme-enfold.php
+++ inc/compat/themes/compat-theme-enfold.php
@@ -29,9 +29,6 @@ class FluidCheckout_ThemeCompat_Enfold extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
-
-		// Button styles
-		add_filter( 'fc_apply_button_colors_styles', '__return_true' );
 	}
 
 
