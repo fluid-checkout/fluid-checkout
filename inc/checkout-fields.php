2d87e9cd588f15f51856cd68ca447a69180ae585
Diego Versiani <diego@fluidweb.co>
2023-08-29 20:58:15 +0200

    Remove shipping company hooks on undo_hooks.

--- inc/checkout-fields.php
+++ inc/checkout-fields.php
@@ -51,6 +51,7 @@ class FluidCheckout_CheckoutFields extends FluidCheckout {
 		// Checkout fields args
 		remove_filter( 'woocommerce_billing_fields', array( $this, 'change_checkout_field_args' ), 100 );
 		remove_filter( 'woocommerce_shipping_fields', array( $this, 'change_checkout_field_args' ), 100 );
+		remove_filter( 'woocommerce_shipping_fields', array( $this, 'maybe_change_shipping_company_field_args' ), 100 );
 		remove_filter( 'woocommerce_checkout_fields', array( $this, 'change_order_field_args' ), 100 );
 		remove_filter( 'woocommerce_default_address_fields', array( $this, 'change_default_locale_field_args' ), 100 );
 
