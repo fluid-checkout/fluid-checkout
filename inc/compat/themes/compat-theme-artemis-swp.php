826abd1ea5d6c3746b52d54fda7949ff9f590985
Diego Versiani <diego@fluidweb.co>
2023-09-12 15:43:27 +0200

    Fix hooks for theme Artemis

---
+++ inc/compat/themes/compat-theme-artemis-swp.php
@@ -0,0 +1,69 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Artemis-SWP (by SmartWPress).
+ */
+class FluidCheckout_ThemeCompat_ArtemisSWP extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false' );
+		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// Payment methods
+		remove_action( 'woocommerce_review_order_before_payment', 'artemis_swp_woocommerce_review_order_before_payment', 10 );
+
+		// Order summary
+		remove_action( 'woocommerce_checkout_order_review', 'artemis_swp_woocommerce_checkout_order_review', 1 );
+	}
+
+
+
+	/**
+	 * Add container class to the main content element.
+	 *
+	 * @param string $class Main content element classes.
+	 */
+	public function change_fc_content_section_class( $class ) {
+		// Bail if using the plugin's header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->get_hide_site_header_footer_at_checkout() ) { return $class; }
+
+		return $class . ' lc_swp_full';
+	}
+
+
+
+	/**
+	 * Change the sticky element relative ID.
+	 *
+	 * @param   array   $attributes    HTML element attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if using the plugin's header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->get_hide_site_header_footer_at_checkout() ) { return $attributes; }
+
+		$attributes['data-sticky-relative-to'] = '.lc_sticky_menu';
+
+		return $attributes;
+	}
+
+}
+
+FluidCheckout_ThemeCompat_ArtemisSWP::instance();
