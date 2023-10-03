9b409095f755adfc40bb59de51410e008c2d8620
Diego Versiani <diego@fluidweb.co>
2023-10-03 21:13:28 +0200

    Refactor move compat code related to PRO features.

--- inc/compat/plugins/compat-plugin-kadence-woo-extras.php
+++ inc/compat/plugins/compat-plugin-kadence-woo-extras.php
@@ -19,48 +19,31 @@ class FluidCheckout_KadenceWooExtras extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Very late hooks
-		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
-
 		// JS settings object
-		add_filter( 'fc_js_settings', array( $this, 'add_js_settings' ), 300 );
+		add_filter( 'fc_checkout_coupons_script_settings', array( $this, 'change_js_settings_coupon_codes' ), 300 );
 	}
 
+
+	
 	/**
-	 * Add or remove very late hooks.
+	 * Change JS settings to enable snackbar notices for coupon code messages.
 	 */
-	public function very_late_hooks() {
-		// Cart custom notices
-		$this->cart_notices_hooks();
+	public function get_updated_js_settings_for_snackbar( $settings ) {
+		$settings[ 'useGeneralNoticesSection' ] = 'yes';
+		$settings[ 'suppressSuccessMessages' ] = 'no';
+
+		return $settings;
 	}
 
 	/**
-	 * Cart custom notices hooks.
-	 */
-	public function cart_notices_hooks() {
-		// Bail if class is not available
-		$class_name = 'Kadence_Cart_Notice';
-		if ( ! class_exists( $class_name ) ) { return; }
-
-		// Get object
-		$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
-
-		// Bail if object is not available
-		if ( ! $class_object ) { return; }
-
-		// Cart custom notices
-		remove_action( 'woocommerce_before_cart_contents', array( $class_object, 'kt_custom_cart_notices' ), 10 );
-		add_action( 'fc_pro_cart_sections', array( $class_object, 'kt_custom_cart_notices' ), 6 );
-	}
-
-
-
-	/**
-	 * Add settings to the plugin settings JS object.
+	 * Change the JS settings for coupon codes.
 	 *
 	 * @param   array  $settings  JS settings object of the plugin.
 	 */
-	public function add_js_settings( $settings ) {
+	public function change_js_settings_coupon_codes( $settings ) {
+		// Bail if not on checkout page
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $settings; }
+
 		// Get plugin settings
 		$shopkit_settings = get_option( 'kt_woo_extras' );
 		if ( ! is_array( $shopkit_settings ) ) {
@@ -75,27 +58,10 @@ class FluidCheckout_KadenceWooExtras extends FluidCheckout {
 		if ( ! $snackbar ) { return $settings; }
 
 		// Bail if not using snackbar notices on specific pages (cart or checkout)
-		if ( 
-			( FluidCheckout_Steps::instance()->is_cart_page_or_fragment() && ( ! isset( $shopkit_settings[ 'snackbar_cart' ] ) || true != $shopkit_settings[ 'snackbar_cart' ] ) )
-			|| ( FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() && ( ! isset( $shopkit_settings[ 'snackbar_checkout' ] ) || true != $shopkit_settings[ 'snackbar_checkout' ] ) )
-			) {
-			return $settings;
-		}
+		if ( ! isset( $shopkit_settings[ 'snackbar_checkout' ] ) || true != $shopkit_settings[ 'snackbar_checkout' ] ) { return $settings; }
 
-		// Define new values for coupon code settings
-		$new_settings_coupons = array(
-			'useGeneralNoticesSection' => 'yes',
-			'suppressSuccessMessages' => 'no',
-		);
-
-		// Define new values for cart update settings
-		$new_settings_cart_update = array(
-			'scrollToNoticesEnabled' => 'no',
-		);
-
-		// Update settings
-		$settings[ 'checkoutCoupons' ] = array_merge( $settings[ 'checkoutCoupons' ], $new_settings_coupons );
-		$settings[ 'cartUpdate' ] = array_merge( $settings[ 'cartUpdate' ], $new_settings_cart_update );
+		// Change settings
+		$settings = $this->get_updated_js_settings_for_snackbar( $settings );
 
 		return $settings;
 	}
