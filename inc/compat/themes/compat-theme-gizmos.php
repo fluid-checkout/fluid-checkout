c72855c86d3ab3e3c1490c6c0ce25bd2dbf65b00
Diego Versiani <diego@fluidweb.co>
2023-10-31 19:35:14 +0100

    Change button styles.

--- inc/compat/themes/compat-theme-gizmos.php
+++ inc/compat/themes/compat-theme-gizmos.php
@@ -28,6 +28,13 @@ class FluidCheckout_ThemeCompat_Gizmos extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Buttons
+		add_filter( 'fc_apply_button_colors_styles', '__return_true', 10 );
+		add_filter( 'fc_apply_button_design_styles', '__return_true', 10 );
 	}
 
 	/**
@@ -37,6 +44,45 @@ class FluidCheckout_ThemeCompat_Gizmos extends FluidCheckout {
 		// Remove theme elements
 		remove_action( 'woocommerce_before_checkout_form', 'gizmos_add_main_woo_page_holder', 5 );
 		remove_action( 'woocommerce_after_checkout_form', 'gizmos_add_main_woo_page_holder_end', 20 );
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			// Buttons
+			':root body' => array(
+				// Primary button color
+				'--fluidcheckout--button--primary--border-color' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--primary--background-color' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--primary--text-color' => '#fff',
+				'--fluidcheckout--button--primary--border-color--hover' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--primary--background-color--hover' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--primary--text-color--hover' => '#fff',
+
+				// Secondary button color
+				'--fluidcheckout--button--secondary--border-color' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--secondary--background-color' => 'transparent',
+				'--fluidcheckout--button--secondary--text-color' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--secondary--border-color--hover' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--secondary--background-color--hover' => 'var( --qode-main-color );',
+				'--fluidcheckout--button--secondary--text-color--hover' => '#fff',
+
+				// Button design styles
+				'--fluidcheckout--button--height' => '50px',
+				'--fluidcheckout--button--border-radius' => '8px',
+				'--fluidcheckout--button--font-size' => '12px',
+				'--fluidcheckout--button--font-weight' => 'bold',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 
