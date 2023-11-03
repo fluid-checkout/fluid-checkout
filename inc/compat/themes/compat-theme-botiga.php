d63a953f9fe3df985631418d67188c3595e8c165
Diego Versiani <diego@fluidweb.co>
2023-11-03 17:50:31 +0100

    Fix order summary layout.

--- inc/compat/themes/compat-theme-botiga.php
+++ inc/compat/themes/compat-theme-botiga.php
@@ -19,8 +19,22 @@ class FluidCheckout_ThemeCompat_Botiga extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Late hooks
+		add_action( 'init', array( $this, 'late_hooks' ), 100 );
+
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
+	}
+
+
+
+	/**
+	 * Add or remove late hooks.
+	 */
+	public function late_hooks() {
+		// Remove theme elements
+		remove_action( 'woocommerce_checkout_before_order_review_heading', 'botiga_wrap_order_review_before', 5 );
+		remove_action( 'woocommerce_checkout_after_order_review', 'botiga_wrap_order_review_after', 15 );
 	}
 
 }
