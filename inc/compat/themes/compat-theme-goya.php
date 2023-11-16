e746cf94fa111811e0dc56b01f332f1d4ce59764
Diego Versiani <diego@fluidweb.co>
2023-11-16 16:43:25 +0100

    Set button colors to match the theme settings.

--- inc/compat/themes/compat-theme-goya.php
+++ inc/compat/themes/compat-theme-goya.php
@@ -26,6 +26,12 @@ class FluidCheckout_ThemeCompat_Goya extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Buttons
+		add_filter( 'fc_apply_button_colors_styles', '__return_true', 10 );
 	}
 
 
@@ -56,6 +62,41 @@ class FluidCheckout_ThemeCompat_Goya extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '.header-sticky .header';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Get theme colors
+		$primary_background_color = get_theme_mod( 'primary_buttons', '#282828' );
+		$primary_text_color = get_theme_mod( 'primary_buttons_text_color', '#fff' );
+		$secondary_background_color = get_theme_mod( 'second_buttons', '#282828' );
+		
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				'--fluidcheckout--button--primary--border-color' => $primary_background_color,
+				'--fluidcheckout--button--primary--background-color' => $primary_background_color,
+				'--fluidcheckout--button--primary--text-color' => $primary_text_color,
+				'--fluidcheckout--button--primary--border-color--hover' => $primary_background_color,
+				'--fluidcheckout--button--primary--background-color--hover' => $primary_background_color,
+				'--fluidcheckout--button--primary--text-color--hover' => $primary_text_color,
+
+				'--fluidcheckout--button--secondary--border-color' => 'currentColor',
+				'--fluidcheckout--button--secondary--background-color' => 'transparent',
+				'--fluidcheckout--button--secondary--text-color' => 'currentColor',
+				'--fluidcheckout--button--secondary--border-color--hover' => $primary_background_color,
+				'--fluidcheckout--button--secondary--background-color--hover' => 'transparent',
+				'--fluidcheckout--button--secondary--text-color--hover' => $primary_background_color,
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
