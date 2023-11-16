588f5668073cf7c8f8db130281b052ef96e05fec
Diego Versiani <diego@fluidweb.co>
2023-11-16 16:15:11 +0100

    Fix sticky elements refefence.

---
+++ inc/compat/themes/compat-theme-goya.php
@@ -0,0 +1,45 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Goya (by Everthemes).
+ */
+class FluidCheckout_ThemeCompat_Goya extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+	}
+
+
+
+	/**
+	 * Change the sticky element relative ID.
+	 *
+	 * @param   array   $attributes    HTML element attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $attributes; }
+
+		$attributes['data-sticky-relative-to'] = '.header-sticky .header';
+
+		return $attributes;
+	}
+
+}
+
+FluidCheckout_ThemeCompat_Goya::instance();
