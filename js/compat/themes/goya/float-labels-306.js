a5230d2f02223c59f0dd760eb72dd81a58543df5
Diego Versiani <diego@fluidweb.co>
2023-11-17 22:00:35 +0100

    Set delays for rebuilding float labels to fix styles with some field types.

--- js/compat/themes/goya/float-labels-306.js
+++ js/compat/themes/goya/float-labels-306.js
@@ -127,54 +127,6 @@
 		});
 	}
 
-	/**
-	 * Re-focus and keep value of the element that was previously focused.
-	 */
-	var maybe_refocus_element = function( currentFocusedElement, currentValue ) {
-		// Bail if no element to focus
-		if ( null === currentFocusedElement ) { return; }
-
-		requestAnimationFrame( function() {
-			var elementToFocus;
-
-			// Try findind the `select2` focusable element
-			if ( currentFocusedElement.matches( '.fc-select2-field' ) ) {
-				elementToFocus = document.querySelector( '.form-row[id="' + currentFocusedElement.id + '"] .select2-selection' );
-			}
-			// Try findind the the current focused element after updating updated element by ID
-			else if ( currentFocusedElement.id ) {
-				elementToFocus = document.getElementById( currentFocusedElement.id );
-			}
-			// Try findind the updated element by classes
-			else if ( currentFocusedElement.getAttribute( 'name' ) ) {
-				var nameAttr = currentFocusedElement.getAttribute( 'name' );
-				elementToFocus = document.querySelector( '[name="'+nameAttr+'"]' );
-			}
-
-			// Try setting focus if element is found
-			if ( elementToFocus && elementToFocus !== document.activeElement ) {
-				elementToFocus.focus();
-
-				// Set keyboard track position back to that previously to update
-				setTimeout( function(){
-					// Try to set the same track position
-					if( null !== elementToFocus.selectionStart && null !== elementToFocus.selectionEnd ) {
-						if ( currentFocusedElement.selectionStart && currentFocusedElement.selectionEnd ) {
-							elementToFocus.selectionStart = currentFocusedElement.selectionStart;
-							elementToFocus.selectionEnd = currentFocusedElement.selectionEnd;
-						}
-						// Otherwise try set the track position to the end of the field
-						// @see https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/setSelectionRange
-						// @see https://html.spec.whatwg.org/multipage/input.html#concept-input-apply
-						else {
-							elementToFocus.selectionStart = elementToFocus.selectionEnd = Number.MAX_SAFE_INTEGER || 10000;
-						}
-					}
-				}, 0 );
-			}
-		} );
-	}
-
 
 
 	/**
@@ -191,6 +143,8 @@
 
 		// Rebuild on initialization
 		setTimeout( maybeRebuildFloatLabels, 100 );
+		setTimeout( maybeRebuildFloatLabels, 500 );
+		setTimeout( maybeRebuildFloatLabels, 1000 );
 
 		_hasInitialized = true;
 	};
