06666c20d0d295d48a9fce590be58d5c9bdcea2a
Diego Versiani <diego@fluidweb.co>
2023-11-29 00:06:23 +0100

    Fix EU-VAT addon price on plugin settings dashboard.

--- inc/admin/admin-setting-type-fc-addons.php
+++ inc/admin/admin-setting-type-fc-addons.php
@@ -218,7 +218,7 @@ class FluidCheckout_Admin_SettingType_Addons extends FluidCheckout {
 							<?php elseif ( FluidCheckout::instance()->is_plugin_installed( 'fc-vat-assistant/fc-vat-assistant.php' ) ) : ?>
 								<a href="<?php echo esc_url( add_query_arg( array( 'fc_action' => 'activate_plugin', 'plugin' => 'fc-vat-assistant/fc-vat-assistant.php', '_wpnonce' => wp_create_nonce( 'fc-activate-plugin' ) ) ) ); ?>" class="button"><?php echo esc_html( __( 'Installed &mdash; Activate', 'fluid-checkout' ) ); ?></a>
 							<?php else : ?>
-								<a href="<?php echo esc_url( 'https://fluidcheckout.com/fc-eu-vat-assistant/?mtm_campaign=addons&mtm_kwd=fc-vat&mtm_source=lite-plugin' ); ?>" class="button button-primary" target="_blank"><?php echo esc_html( __( 'Get this add-on &mdash; 59 EUR', 'fluid-checkout' ) ); ?></a>
+								<a href="<?php echo esc_url( 'https://fluidcheckout.com/fc-eu-vat-assistant/?mtm_campaign=addons&mtm_kwd=fc-vat&mtm_source=lite-plugin' ); ?>" class="button button-primary" target="_blank"><?php echo esc_html( __( 'Get this add-on &mdash; 39 EUR', 'fluid-checkout' ) ); ?></a>
 							<?php endif; ?>
 						</div>
 					</li>
