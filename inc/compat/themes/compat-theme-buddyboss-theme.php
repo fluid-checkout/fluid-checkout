4d5a080accd6addd404209047bb63313600e1333
vladdedikov <vlad@vladdedikov.ru>
2023-12-21 11:26:33 +0400

    Replaces spaces with tabs

--- inc/compat/themes/compat-theme-buddyboss-theme.php
+++ inc/compat/themes/compat-theme-buddyboss-theme.php
@@ -19,12 +19,12 @@ class FluidCheckout_ThemeCompat_BuddyBoss extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-        // Dequeue
+		// Dequeue
 		add_action( 'wp_enqueue_scripts', array( $this, 'maybe_dequeue_scripts' ), 100 );
 	}
 
 
-    
+
 	/**
 	 * Dequeue theme scripts unnecessary on checkout page and that interfere with Fluid Checkout scripts.
 	 */
