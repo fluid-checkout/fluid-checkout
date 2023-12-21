1855ff70c92d2f1ddec931bf8d6a8297516753ae
Diego Versiani <diego@fluidweb.co>
2023-12-21 20:31:02 +0100

    Remove option to display the billing address after payment step.

--- inc/admin/admin-settings-checkout.php
+++ inc/admin/admin-settings-checkout.php
@@ -457,7 +457,6 @@ class WC_Settings_FluidCheckout_Checkout_Settings extends WC_Settings_Page {
 							'substep_after_shipping'     => array( 'label' => __( 'As a substep after the shipping address section', 'fluid-checkout' ), 'disabled' => true ),
 							'step_after_shipping'        => array( 'label' => __( 'As a separate step after shipping', 'fluid-checkout' ) ),
 							'substep_before_payment'     => array( 'label' => __( 'As a substep before the payment section', 'fluid-checkout' ), 'disabled' => true ),
-							'substep_after_payment'      => array( 'label' => __( 'As a substep after the payment section', 'fluid-checkout' ), 'disabled' => true ),
 						),
 						'default'           => FluidCheckout_Settings::instance()->get_option_default( 'fc_pro_checkout_billing_address_position' ),
 						'autoload'          => false,
