66a5516f82a214eeac4d7e4a5f473eb9dcc9a4b4
vladdedikov <vlad@vladdedikov.ru>
2024-01-08 20:11:27 +0400

    Fix `select2` fields

--- inc/compat/themes/compat-theme-smart-home.php
+++ inc/compat/themes/compat-theme-smart-home.php
@@ -19,6 +19,9 @@ class FluidCheckout_ThemeCompat_SmartHome extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+
 		// Checkout page template
 		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 110 );
 
@@ -28,6 +31,16 @@ class FluidCheckout_ThemeCompat_SmartHome extends FluidCheckout {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+	}
+
+	/**
+	 * Add or remove very late hooks.
+	 */
+	public function very_late_hooks() {
+		if ( FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) {
+			// Prevent theme from replacing `select2` field assets on checkout page
+			remove_action( 'wp_enqueue_scripts', 'thb_woocommerce_scripts_styles', 10001 );
+		}
 	}
 
 
