978e27180099c8e6fe40616ed941772595300935
vladdedikov <vlad@vladdedikov.ru>
2024-01-08 21:32:30 +0400

    Fix "Change" button not appearing after in collapsed "My contact" step

--- inc/compat/themes/compat-theme-smart-home.php
+++ inc/compat/themes/compat-theme-smart-home.php
@@ -31,6 +31,10 @@ class FluidCheckout_ThemeCompat_SmartHome extends FluidCheckout {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+
+		// Remove theme elements from checkout page
+		remove_action( 'woocommerce_checkout_before_customer_details', 'thb_checkout_before_customer_details', 5 );
+		remove_action( 'woocommerce_checkout_after_customer_details', 'thb_checkout_after_customer_details', 30 );
 	}
 
 	/**
