477c4509676c2f889f9e8d411ce8db6f1b7ba697
vladdedikov <vlad@vladdedikov.ru>
2024-01-08 16:41:22 +0400

    Restructure PHP compat class to improve code readability

--- inc/compat/themes/compat-theme-cartsy.php
+++ inc/compat/themes/compat-theme-cartsy.php
@@ -19,10 +19,21 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		if ( ! FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) {
-			// Prevent theme's page template from being replaced by FC checkout template
-			add_filter( 'fc_enable_checkout_page_template', '__return_false', 10 );
-		}
+		// Checkout templates
+		$this->checkout_layout_hooks();
+	}
+
+
+
+	/*
+	* Checkout templates hooks.
+	*/
+	public function checkout_layout_hooks() {
+		// Bail if using the distraction free template
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
+
+		// Prevent theme's page template from being replaced by FC checkout template
+		add_filter( 'fc_enable_checkout_page_template', '__return_false', 10 );
 	}
 
 }
