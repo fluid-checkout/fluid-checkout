d26314d0a858946d66d482d7b594212c169ee37e
vladdedikov <vlad@vladdedikov.ru>
2024-01-09 16:34:47 +0400

    Move the bail condition back

--- inc/compat/themes/compat-theme-cartsy.php
+++ inc/compat/themes/compat-theme-cartsy.php
@@ -19,9 +19,6 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Bail if using the distraction free template
-		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
-
 		// Checkout templates
 		$this->checkout_layout_hooks();
 	}
@@ -32,6 +29,9 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 	* Checkout templates hooks.
 	*/
 	public function checkout_layout_hooks() {
+		// Bail if using the distraction free template
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
+
 		// Prevent theme's page template from being replaced by FC checkout template
 		add_filter( 'fc_enable_checkout_page_template', '__return_false', 10 );
 	}
