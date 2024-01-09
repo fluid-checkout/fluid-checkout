114571d6e8cb36cdbce089c1450019255a7b2ba7
vladdedikov <vlad@vladdedikov.ru>
2024-01-09 17:07:46 +0400

    Fix missing currency and decimals in cart item prices on checkout page

--- inc/compat/themes/compat-theme-cartsy.php
+++ inc/compat/themes/compat-theme-cartsy.php
@@ -21,6 +21,9 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 	public function hooks() {
 		// Checkout templates
 		$this->checkout_layout_hooks();
+
+		// Very late checkout hooks
+		add_action( 'wp', array( $this, 'very_late_checkout_hooks' ), 100 );
 	}
 
 
@@ -35,6 +38,20 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 		// Prevent theme's page template from being replaced by FC checkout template
 		add_filter( 'fc_enable_checkout_page_template', '__return_false', 10 );
 	}
+
+
+
+	/**
+	 * Very late checkout hooks.
+	 */
+	public function very_late_checkout_hooks() {
+		// Bail if not on checkout page
+		if ( ! function_exists( 'is_checkout' ) || ! is_checkout() ) { return; }
+
+		// Bring back currency and decimals to cart item price values
+		$this->remove_filter_for_class( 'woocommerce_cart_product_price', array( 'Framework\App\WooCommerceLoad', 'cartsyCartProductPrice' ), 10 );
+	}
+
 
 }
 
