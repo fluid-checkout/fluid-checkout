7c3a9005ea9c8864e68ab6fae6c927b502e6211a
Diego Versiani <diego@fluidweb.co>
2024-02-02 19:48:06 +0100

    Improved: Add process to automatically generate the installable zip file when creating a new version.

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,7 @@ var _gulpSettings = loadJsonFile.sync( 'gulp-settings.json' );
 var _gulpSettingsLocal = {};
 var _package = {},
 	_assetsVersion = '';
+var _defaultAction = _gulpSettingsLocal.defaultAction || _gulpSettings.defaultAction || 'watch';
 
 
 
@@ -36,6 +37,7 @@ catch ( err ) {}
 // gulp update-ver
 // Update the project version
 gulp.task( 'update-ver', gulp.series( function( done ) {
+	// Load package.json
 	_package = loadJsonFile.sync( 'package.json' );
 	_assetsVersion = '-' + _package.version.replace( /\./gi, '' );
 
@@ -51,9 +53,14 @@ gulp.task( 'update-ver', gulp.series( function( done ) {
 // gulp update-ver
 // Update the project version for full releases, including the version and date in the changelog
 gulp.task( 'update-ver-release', gulp.series( 'update-ver', function( done ) {
-	var today = new Date();
+	// Load package.json
 	_package = loadJsonFile.sync( 'package.json' );
+
+	// Define assets version
 	_assetsVersion = '-' + _package.version.replace( /\./gi, '' );
+
+	// Get current date
+	var today = new Date();
 
 	// Only update readme.txt for full release versions
 	if ( _package.version.indexOf( 'beta' ) < 0 && _package.version.indexOf( 'dev' ) < 0 ) {
@@ -407,6 +414,9 @@ gulp.task( 'build-js', gulp.series( 'update-ver', 'clean-js', function( done ) {
 // gulp npm-run-build
 // Run the command `npm run build` to build the project assets for WordPress blocks.
 gulp.task( 'npm-run-build', gulp.series( function( done ) {
+	// Bail if the `blocks` folder does not exist
+	if ( ! fs.existsSync( './blocks' ) ) { return done(); }
+
 	exec('npm run build', function (err, stdout, stderr) {
 		console.log( stdout );
 		console.log( stderr );
@@ -419,12 +429,74 @@ gulp.task( 'npm-run-build', gulp.series( function( done ) {
 	done();
 } ) );
 
-
-
 // Run:
 // gulp build
 // Build css and js assets
 gulp.task( 'build', gulp.series( gulp.parallel( 'build-js', 'build-css', 'npm-run-build' ) ) );
+
+
+
+// Run:
+// gulp copy-plugin-files
+// Copy the plugin files which are commited to the project into the export folder.
+gulp.task( 'copy-plugin-files', gulp.series( function( done ) {
+	// Load package.json
+	_package = loadJsonFile.sync( 'package.json' );
+	
+	// Bail if destination path or plugin files does not exist
+	if ( ! _gulpSettingsLocal.pluginZipPath || ! fs.existsSync( _gulpSettingsLocal.pluginZipPath ) ) {
+		console.log( 'Skipping: Plugin zip export path not defined or folder does not exist.' );
+		return done();
+	}
+
+	// Maybe delete existing destination folder
+	var destinationFolder = _gulpSettingsLocal.pluginZipPath + '/' + _package.name;
+	if ( fs.existsSync( destinationFolder ) ) {
+		console.log( 'Deleting existing destination folder: ' + destinationFolder );
+		del.sync( destinationFolder, { force: true } );
+	}
+
+	exec( 'rsync -av --exclude-from=.gitignore ../' + _package.name + ' ' + _gulpSettingsLocal.pluginZipPath, function ( err, stdout, stderr ) {
+		console.log( 'COPYING FILES...' );
+		console.log( stdout );
+		console.log( stderr );
+		if ( err ) {
+			// If an error occurred, make the Gulp task fail
+			done( err );
+		}
+	} );
+
+	done();
+} ) );
+
+// Run:
+// gulp pack-plugin-zip
+// Pack the extracted plugin folder into a new installable zip file with the version number.
+gulp.task( 'pack-plugin-zip', gulp.series( function( done ) {
+	// Load package.json
+	_package = loadJsonFile.sync( 'package.json' );
+
+	// Determine plugin file based on the package name and version
+	var destinationFile = _package.name + '-' + _package.version + '.zip';
+
+	// Maybe delete existing destination file
+	var destinationFileWithPath = _gulpSettingsLocal.pluginZipPath + '/' + destinationFile;
+	if ( fs.existsSync( destinationFileWithPath ) ) {
+		del.sync( destinationFileWithPath, { force: true } );
+	}
+
+	// Zip the plugin folder
+	exec( 'cd ' + _gulpSettingsLocal.pluginZipPath + ' && zip -r ' + destinationFile + ' ' + _package.name, function ( err, stdout, stderr ) {
+		console.log( stdout );
+		console.log( stderr );
+		if ( err ) {
+			// If an error occurred, make the Gulp task fail
+			done( err );
+		}
+	});
+
+	done();
+} ) );
 
 
 
@@ -448,7 +520,7 @@ gulp.task( 'copy-updater', gulp.series( function( done ) {
 // gulp browser-sync
 // Starts browser-sync task for starting the server.
 gulp.task( 'browser-sync', gulp.series( function( done ) {
-	if ( _gulpSettings.browserSyncWatch && _gulpSettingsLocal.browserSyncOptions ) {
+	if ( _gulpSettings.browserSyncWatch && _gulpSettingsLocal && _gulpSettingsLocal.browserSyncOptions ) {
 		browserSync.init( _gulpSettings.browserSyncWatch, _gulpSettingsLocal.browserSyncOptions );
 	}
 
@@ -490,4 +562,4 @@ gulp.task( 'watch-reload', gulp.series( gulp.parallel( 'build-js', 'build-css-de
 // Run:
 // gulp
 // Defines gulp default task
-gulp.task( 'default', gulp.series( _gulpSettings.defaultAction ) );
+gulp.task( 'default', gulp.series( _defaultAction ) );
