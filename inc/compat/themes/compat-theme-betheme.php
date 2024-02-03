56dac01083de212170a06eb993097d62772e841d
vladdedikov <vlad@vladdedikov.ru>
2024-02-03 19:35:35 +0400

    Fix container width and add a relative header selector for sticky elements

---
+++ inc/compat/themes/compat-theme-betheme.php
@@ -0,0 +1,150 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: BeTheme (by Muffin Group).
+ */
+class FluidCheckout_ThemeCompat_BeTheme extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+
+		// Checkout page template
+		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 100 );
+
+		// Template file loader
+		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template_checkout_page_template' ), 100, 3 );
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+	}
+
+
+
+	/**
+	 * Replace the checkout page template with our own file.
+	 *
+	 * @param   String  $template  Template file path.
+	 */
+	public function checkout_page_template( $template ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
+
+		// Bail if checkout page template is not enabled
+		if ( true !== apply_filters( 'fc_enable_checkout_page_template', true ) ) { return $template; }
+
+		// Bail if not on checkout page.
+		if( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $template; }
+
+		// Locate new checkout page template
+		$_template = $this->locate_template_checkout_page_template( $template, 'checkout/page-checkout.php', null );
+
+		// Check if the file exists
+		if ( file_exists( $_template ) ) {
+			$template = $_template;
+		}
+
+		return $template;
+	}
+
+
+
+	/**
+	 * Locate template files from this plugin.
+	 */
+	public function locate_template_checkout_page_template( $template, $template_name, $template_path ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
+		
+		$_template = null;
+
+		// Set template path to default value when not provided
+		if ( ! $template_path ) { $template_path = 'woocommerce/'; };
+
+		// Get plugin path
+		$plugin_path = self::$directory_path . 'templates/compat/themes/betheme/fc/checkout-page-template/';
+
+		// Get the template from this plugin, if it exists
+		if ( file_exists( $plugin_path . $template_name ) ) {
+			$_template = $plugin_path . $template_name;
+
+			// Look for template file in the theme
+			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
+				$_template_override = locate_template( array(
+					trailingslashit( $template_path ) . $template_name,
+					$template_name,
+				) );
+
+				// Check if files exist before changing template
+				if ( file_exists( $_template_override ) ) {
+					$_template = $_template_override;
+				}
+			}
+		}
+
+		// Use default template
+		if ( ! $_template ) {
+			$_template = $template;
+		}
+
+		// Return what we found
+		return $_template;
+	}
+
+
+
+	/**
+	 * Add container class to the main content element.
+	 *
+	 * @param string $class Main content element classes.
+	 */
+	public function change_fc_content_section_class( $class ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
+
+		return $class . ' section_wrapper';
+	}
+
+
+
+	/**
+	 * Change the element used to position the progress bar and order summary when sticky.
+	 * 
+	 * @param  array  $attributes  The elements attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if theme functions isn't available
+		if ( ! function_exists( 'mfn_layout_ID' ) || ! function_exists( 'mfn_opts_get' ) ) { return $attributes; }
+
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $attributes; }
+
+		$layoutID = get_post_meta( get_the_ID(), 'mfn-post-custom-layout', true );
+
+		// Bail if theme's conditions for sticky header are not met
+		if ( ! $layout_id && ! get_post_meta( $layout_id, 'mfn-post-sticky-header', true) && ! mfn_opts_get('sticky-header') ) { return $attributes; }
+
+		$attributes['data-sticky-relative-to'] = '#Top_bar.is-sticky';
+
+		return $attributes;
+	}
+
+}
+
+FluidCheckout_ThemeCompat_BeTheme::instance();
