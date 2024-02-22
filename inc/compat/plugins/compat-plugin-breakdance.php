1011d3125c1d71847a6c94769cdadb8d3f9d023d
Diego Versiani <diego@fluidweb.co>
2024-02-22 23:35:17 +0100

    Fix container classe and sticky header reference for plugin theme breakdance.

--- inc/compat/plugins/compat-plugin-breakdance.php
+++ inc/compat/plugins/compat-plugin-breakdance.php
@@ -22,6 +22,13 @@ class FluidCheckout_Breakdance extends FluidCheckout {
 		// Very late hooks
 		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
 
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
 		// Buttons
 		add_filter( 'fc_apply_button_colors_styles', '__return_true', 10 );
 		add_filter( 'fc_apply_button_design_styles', '__return_true', 10 );
@@ -84,6 +91,22 @@ class FluidCheckout_Breakdance extends FluidCheckout {
 		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $template; }
 
 		return $this->revert_wc_get_template( $template, $template_name, $args, $template_path, $default_path );
+	}
+
+
+
+	/**
+	 * Change the sticky element relative ID.
+	 *
+	 * @param   array   $attributes    HTML element attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $attributes; }
+
+		$attributes['data-sticky-relative-to'] = '.bde-section.sticky-header.is-sticky';
+
+		return $attributes;
 	}
 
 }
