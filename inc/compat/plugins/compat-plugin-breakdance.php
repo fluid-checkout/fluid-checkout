08c915ecae5a3357f634832dd07e3089d034fd33
Diego Versiani <diego@fluidweb.co>
2024-02-23 00:47:08 +0100

    Move breakdance CSS variables to the compat class.

--- inc/compat/plugins/compat-plugin-breakdance.php
+++ inc/compat/plugins/compat-plugin-breakdance.php
@@ -29,6 +29,9 @@ class FluidCheckout_Breakdance extends FluidCheckout {
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
 		// Buttons
 		add_filter( 'fc_apply_button_colors_styles', '__return_true', 10 );
 		add_filter( 'fc_apply_button_design_styles', '__return_true', 10 );
@@ -107,6 +110,53 @@ class FluidCheckout_Breakdance extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '.bde-section.bde-header--sticky.is-sticky';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '48.4px',
+				'--fluidcheckout--field--border-radius' => 'var(--bde-woo-forms__inputs-border-radius)',
+				'--fluidcheckout--field--border-color' => 'var(--bde-form-input-border-color)',
+				'--fluidcheckout--field--background-color' => 'var(--bde-woo-forms__inputs-background-color)',
+				'--fluidcheckout--field--background-color--accent' => 'var(--bde-woo-buttons-and-links__text-link-color-hover)',
+				'--fluidcheckout--field--text-color--accent' => 'var(--bde-woo-base-text-on-primary-color)',
+				'--fluidcheckout--field--text-color--focus' => 'var(--bde-woo-base-text-on-primary-color)',
+
+				// Button design styles
+				'--fluidcheckout--button--height' => '50px',
+				'--fluidcheckout--button--border-radius' => 'var(--bde-button-border-radius)',
+				'--fluidcheckout--button--font-size' => 'var(--bde-button-font-size)',
+				'--fluidcheckout--button--font-weight' => 'var(--bde-button-font-weight)',
+
+				// Button color styles - primary
+				'--fluidcheckout--button--primary--border-color' => 'var(--bde-button-primary-border-color)',
+				'--fluidcheckout--button--primary--background-color' => 'var(--bde-button-primary-background-color)',
+				'--fluidcheckout--button--primary--text-color' => 'var(--bde-button-primary-text-color)',
+				'--fluidcheckout--button--primary--border-color--hover' => 'var(--bde-button-primary-border-color-hover)',
+				'--fluidcheckout--button--primary--background-color--hover' => 'var(--bde-button-primary-background-color-hover)',
+				'--fluidcheckout--button--primary--text-color--hover' => 'var(--bde-button-primary-text-color-hover)',
+
+				// Button color styles - secondary
+				'--fluidcheckout--button--secondary--border-color' => 'var(--bde-button-secondary-border-color)',
+				'--fluidcheckout--button--secondary--background-color' => 'var(--bde-button-secondary-background-color)',
+				'--fluidcheckout--button--secondary--text-color' => 'var(--bde-button-secondary-text-color)',
+				'--fluidcheckout--button--secondary--border-color--hover' => 'var(--bde-button-secondary-border-color-hover)',
+				'--fluidcheckout--button--secondary--background-color--hover' => 'var(--bde-button-secondary-background-color-hover)',
+				'--fluidcheckout--button--secondary--text-color--hover' => 'var(--bde-button-secondary-text-color-hover)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
