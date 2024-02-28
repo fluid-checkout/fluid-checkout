465c6d145dc75dc0255d2607edc6ce168edf51a7
vladdedikov <vlad@vladdedikov.ru>
2024-02-28 21:36:54 +0400

    Change hooks priority

--- inc/compat/plugins/compat-plugin-aperitif-core.php
+++ inc/compat/plugins/compat-plugin-aperitif-core.php
@@ -19,19 +19,25 @@ class FluidCheckout_AperitifCore extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Late hooks
-		add_action( 'init', array( $this, 'late_hooks' ), 100 );
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
 	}
 
 
 
 	/**
-	 * Add or remove late hooks.
+	 * Add or remove very late hooks.
 	 */
-	public function late_hooks() {
+	public function very_late_hooks() {
+		// Bail if not at checkout page, and not an AJAX request to update checkout fragment
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
+
 		// Remove container with ID `qodef-woo-page` added by the plugin
 		remove_action( 'woocommerce_before_checkout_form', 'aperitif_core_add_main_woo_page_holder', 5 );
 		remove_action( 'woocommerce_after_checkout_form', 'aperitif_core_add_main_woo_page_holder_end', 20 );
+
+		// Re-add Woocommerce stylesheet
+		remove_filter( 'woocommerce_enqueue_styles', '__return_false' );
 	}
 
 }
