ff686e69d8240ba9826db60923687a57d533adb6
vladdedikov <vlad@vladdedikov.ru>
2024-03-02 23:59:00 +0400

    Move CSS variables for the theme Avada

--- inc/compat/themes/compat-theme-Avada.php
+++ inc/compat/themes/compat-theme-Avada.php
@@ -31,6 +31,9 @@ class FluidCheckout_ThemeCompat_Avada extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 	/**
@@ -102,6 +105,29 @@ class FluidCheckout_ThemeCompat_Avada extends FluidCheckout {
 		}
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '50px',
+				'--fluidcheckout--field--border-radius' => '7px',
+				'--fluidcheckout--validation-check--horizontal-spacing--select' => '40px',
+				'--fluidcheckout--validation-check--horizontal-spacing--select-alt' => '50px',
+				'--fluidcheckout--field--background-color--accent' => 'var(--awb-color4)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
