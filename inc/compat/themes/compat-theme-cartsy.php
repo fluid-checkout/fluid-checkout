e6f2264bc0f601b7db524d80e112108925df5e61
vladdedikov <vlad@vladdedikov.ru>
2024-03-03 19:33:08 +0400

    Move CSS variables for the theme Cartsy

--- inc/compat/themes/compat-theme-cartsy.php
+++ inc/compat/themes/compat-theme-cartsy.php
@@ -28,6 +28,9 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 	/**
@@ -68,6 +71,27 @@ class FluidCheckout_ThemeCompat_Cartsy extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '.cartsy-menu-area';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '44px',
+				'--fluidcheckout--field--border-radius' => '6px',
+				'--fluidcheckout--field--border-color' => 'var(--colorTextMain, #212121)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
