b03baf153fc9a1a318d65f2c78e09f9b7a8353a2
Diego Versiani <diego@fluidweb.co>
2024-03-07 20:16:27 +0100

    Added: Compatibility with plugin Acowebs Woocommerce Dynamic Pricing by Acowebs. Also PRO version of the plugin.

---
+++ inc/compat/plugins/compat-plugin-aco-woo-dynamic-pricing-pro.php
@@ -0,0 +1,29 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with plugin: Acowebs Woocommerce Dynamic Pricing PRO (by Acowebs).
+ */
+class FluidCheckout_AcoWooDynamicPricingPRO extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->load_compat_plugin_lite();
+		$this->hooks();
+	}
+
+	/**
+	 * Load compatibility with Lite version of the same plugin.
+	 */
+	public function load_compat_plugin_lite() {
+		$compat_file = FluidCheckout::$directory_path . 'inc/compat/plugins/compat-plugin-aco-woo-dynamic-pricing.php';
+		if ( file_exists( $compat_file ) ) {
+			require_once $compat_file;
+		}
+	}
+
+}
+
+FluidCheckout_AcoWooDynamicPricingPRO::instance();
