00a449ae8b1a0dc6607e66d8422efabd1235f75f
vladdedikov <vlad@vladdedikov.ru>
2024-03-08 14:11:02 +0400

    Move CSS variables for the theme Diza

--- inc/compat/themes/compat-theme-diza.php
+++ inc/compat/themes/compat-theme-diza.php
@@ -21,6 +21,9 @@ class FluidCheckout_ThemeCompat_Diza extends FluidCheckout {
 	public function hooks() {
 		// Late hooks
 		add_action( 'init', array( $this, 'late_hooks' ), 100 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
@@ -30,6 +33,36 @@ class FluidCheckout_ThemeCompat_Diza extends FluidCheckout {
 	 */
 	public function late_hooks() {
 		remove_filter( 'woocommerce_cart_item_name', 'diza_woocommerce_cart_item_name', 10, 3 );
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Bail if theme function is not available
+		if ( ! function_exists( 'diza_tbay_get_config' ) ) { return $css_variables; }
+
+		$accent_color = diza_tbay_get_config( 'main_color_second' ) ? : '#52d5e6';
+
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '42px',
+				'--fluidcheckout--field--padding-left' => '15px',
+				'--fluidcheckout--field--border-radius' => '3px',
+				'--fluidcheckout--field--background-color--accent' => $accent_color,
+
+				// Checkout validation styles
+				'--fluidcheckout--validation-check--horizontal-spacing--select-alt' => '30px',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
