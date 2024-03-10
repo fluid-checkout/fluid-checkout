06bb3c1fa8e5dcd0d2524f42713dfc9ebe18c90c
vladdedikov <vlad@vladdedikov.ru>
2024-03-10 17:18:37 +0400

    Move CSS variables for the theme Impreza

--- inc/compat/themes/compat-theme-Impreza.php
+++ inc/compat/themes/compat-theme-Impreza.php
@@ -32,6 +32,9 @@ class FluidCheckout_ThemeCompat_Impreza extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
@@ -157,6 +160,28 @@ class FluidCheckout_ThemeCompat_Impreza extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '#page-header';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '44.8px',
+				'--fluidcheckout--field--padding-left' => '12.8px',
+				'--fluidcheckout--field--border-width' => '0px',
+				'--fluidcheckout--field--background-color' => 'var(--color-content-bg-alt)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
