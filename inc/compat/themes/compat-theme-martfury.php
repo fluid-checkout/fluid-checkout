3d0680be07540bb59ed000455e387373be63a6d3
vladdedikov <vlad@vladdedikov.ru>
2024-03-11 21:30:57 +0400

    Move CSS variables for the theme Martfury and fix `Select2` field styles

--- inc/compat/themes/compat-theme-martfury.php
+++ inc/compat/themes/compat-theme-martfury.php
@@ -26,6 +26,9 @@ class FluidCheckout_ThemeCompat_Martfury extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 30 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 30 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
@@ -56,6 +59,27 @@ class FluidCheckout_ThemeCompat_Martfury extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '.header-main';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '50.5px',
+				'--fluidcheckout--field--padding-left' => '16px',
+				'--fluidcheckout--field--background-color--accent' => 'var(--mf-background-primary-color)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
