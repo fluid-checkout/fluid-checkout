109c2666b7a2d1095ecba3f554643fd7ba114b47
vladdedikov <vlad@vladdedikov.ru>
2024-03-14 19:34:18 +0400

    Move CSS variables for the theme PeakShops

--- inc/compat/themes/compat-theme-peakshops.php
+++ inc/compat/themes/compat-theme-peakshops.php
@@ -19,6 +19,9 @@ class FluidCheckout_ThemeCompat_PeakShops extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'add_content_section_class' ), 10 );
@@ -31,6 +34,30 @@ class FluidCheckout_ThemeCompat_PeakShops extends FluidCheckout {
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+	/**
+	 * Add or remove very late hooks.
+	 */
+	public function very_late_hooks() {
+		// Checkout page hooks
+		$this->checkout_hooks();
+	}
+
+
+
+	/*
+	* Add or remove checkout page hooks.
+	*/
+	public function checkout_hooks() {
+		// Bail if not on checkout page
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
+
+		// Re-add WooCommerce assets and fix `select2` fields initialization
+		remove_action( 'wp_enqueue_scripts', 'thb_woocommerce_scripts_styles', 10001 );
 	}
 
 
@@ -67,6 +94,39 @@ class FluidCheckout_ThemeCompat_PeakShops extends FluidCheckout {
 		$attributes['data-sticky-relative-to'] = '.header.fixed';
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Get accent color from the theme
+		if ( function_exists( 'ot_get_option' ) ) {
+			$accent_color = ot_get_option( 'accent_color', '#bfab80' );
+		}
+		else {
+			$accent_color = '#bfab80';
+		}
+
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '40px',
+				'--fluidcheckout--field--padding-left' => '15px',
+				'--fluidcheckout--field--font-size' => '12px',
+				'--fluidcheckout--field--background-color--accent' => $accent_color,
+
+				// Checkout validation styles
+				'--fluidcheckout--validation-check--horizontal-spacing--select' => '30px',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
