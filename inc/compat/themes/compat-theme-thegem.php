9ee548eb4d27bc290dce19fc03be1fe25676ad5c
vladdedikov <vlad@vladdedikov.ru>
2024-03-23 19:36:44 +0400

    Fix container width

--- inc/compat/themes/compat-theme-thegem.php
+++ inc/compat/themes/compat-theme-thegem.php
@@ -19,12 +19,21 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// CSS variables
-		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+		
+		// Checkout page template
+		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 100 );
+
+		// Template file loader
+		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template_checkout_page_template' ), 100, 3 );
 
 		// Buttons
 		add_filter( 'fc_apply_button_colors_styles', '__return_true', 10 );
 		add_filter( 'fc_apply_button_design_styles', '__return_true', 10 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 
 		// Remove checkout elements added by the theme
 		remove_action( 'woocommerce_before_checkout_form', 'thegem_woocommerce_checkout_scripts', 1 );
@@ -49,6 +58,78 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 
 		// Re-add with higher priority
 		add_action( 'woocommerce_before_checkout_form', 'woocommerce_output_all_notices', 10 );
+	}
+
+
+
+	/**
+	 * Replace the checkout page template with our own file.
+	 *
+	 * @param   String  $template  Template file path.
+	 */
+	public function checkout_page_template( $template ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
+
+		// Bail if checkout page template is not enabled
+		if ( true !== apply_filters( 'fc_enable_checkout_page_template', true ) ) { return $template; }
+
+		// Bail if not on checkout page.
+		if( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $template; }
+
+		// Locate new checkout page template
+		$_template = $this->locate_template_checkout_page_template( $template, 'checkout/page-checkout.php', null );
+
+		// Check if the file exists
+		if ( file_exists( $_template ) ) {
+			$template = $_template;
+		}
+
+		return $template;
+	}
+
+
+
+	/**
+	 * Locate template files from this plugin.
+	 */
+	public function locate_template_checkout_page_template( $template, $template_name, $template_path ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
+		
+		$_template = null;
+
+		// Set template path to default value when not provided
+		if ( ! $template_path ) { $template_path = 'woocommerce/'; };
+
+		// Get plugin path
+		$plugin_path = self::$directory_path . 'templates/compat/themes/thegem/fc/checkout-page-template/';
+
+		// Get the template from this plugin, if it exists
+		if ( file_exists( $plugin_path . $template_name ) ) {
+			$_template = $plugin_path . $template_name;
+
+			// Look for template file in the theme
+			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
+				$_template_override = locate_template( array(
+					trailingslashit( $template_path ) . $template_name,
+					$template_name,
+				) );
+
+				// Check if files exist before changing template
+				if ( file_exists( $_template_override ) ) {
+					$_template = $_template_override;
+				}
+			}
+		}
+
+		// Use default template
+		if ( ! $_template ) {
+			$_template = $template;
+		}
+
+		// Return what we found
+		return $_template;
 	}
 
 
