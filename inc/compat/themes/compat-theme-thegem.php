feb1dee5452bb0332c42bda96b44d2e2aeb492da
Diego Versiani <diego@fluidweb.co>
2024-04-01 22:12:18 +0200

    Fix code standards.

--- inc/compat/themes/compat-theme-thegem.php
+++ inc/compat/themes/compat-theme-thegem.php
@@ -26,7 +26,7 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
-		
+
 		// Checkout page template
 		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 100 );
 
@@ -206,7 +206,7 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
 
 		$page_title = thegem_page_title();
-		
+
 		if ( ! empty( $page_title ) ) {
 			echo $page_title;
 		}
@@ -238,7 +238,7 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 				'--fluidcheckout--button--primary--background-color--hover' => 'transparent',
 				'--fluidcheckout--button--primary--text-color--hover' => 'var(--thegem-to-styled-color4)',
 
-				// // Secondary button color
+				// Secondary button color
 				'--fluidcheckout--button--secondary--border-color' => 'var(--thegem-to-styled-color1)',
 				'--fluidcheckout--button--secondary--background-color' => 'var(--thegem-to-styled-color1)',
 				'--fluidcheckout--button--secondary--text-color' => 'var(--thegem-to-button-basic-color)',
@@ -246,7 +246,7 @@ class FluidCheckout_ThemeCompat_TheGem extends FluidCheckout {
 				'--fluidcheckout--button--secondary--background-color--hover' => 'transparent',
 				'--fluidcheckout--button--secondary--text-color--hover' => 'var(--thegem-to-styled-color1)',
 
-				// // Button design styles
+				// Button design styles
 				'--fluidcheckout--button--border-radius' => '3px',
 				'--fluidcheckout--button--border-width' => '2px',
 				'--fluidcheckout--button--font-size' => '14px',
