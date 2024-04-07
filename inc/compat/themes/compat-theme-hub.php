e2cfa066f868d20af84d1af2ffaf7f35d2f659bf
vladdedikov <vlad@vladdedikov.ru>
2024-04-08 00:03:27 +0400

    Fix `select2` fields' style

---
+++ inc/compat/themes/compat-theme-hub.php
@@ -0,0 +1,52 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Hub (by Liquid Themes).
+ */
+class FluidCheckout_ThemeCompat_Hub extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '45px',
+				'--fluidcheckout--field--padding-left' => '25px',
+				'--fluidcheckout--field--background-color--accent' => 'var(--color-primary)',
+
+				// Checkout validation styles
+				'--fluidcheckout--validation-check--horizontal-spacing--select' => '40px',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
+	}
+
+}
+
+FluidCheckout_ThemeCompat_Hub::instance();
