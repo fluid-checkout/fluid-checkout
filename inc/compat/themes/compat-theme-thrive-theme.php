f4e4a7dac6f1216b20393c1dd8991cd4c448d2a1
vladdedikov <vlad@vladdedikov.ru>
2024-04-25 21:23:12 +0400

    Refactor Thrive theme compatibility files

--- inc/compat/themes/compat-theme-thrive-theme.php
+++ inc/compat/themes/compat-theme-thrive-theme.php
@@ -19,11 +19,8 @@ class FluidCheckout_ThemeCompat_ThriveTheme extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Checkout page template
-		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 110 );
-
-		// Template file loader
-		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template_checkout_page_template' ), 100, 3 );
+		// Checkout template hooks
+		$this->checkout_template_hooks();
 
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
@@ -35,65 +32,53 @@ class FluidCheckout_ThemeCompat_ThriveTheme extends FluidCheckout {
 
 
 	/**
-	 * Replace the checkout page template with our own file.
-	 *
-	 * @param   String  $template  Template file path.
+	 * Add checkout template hooks.
 	 */
-	public function checkout_page_template( $template ) {
-		// Bail if checkout page template is not enabled
-		if ( true !== apply_filters( 'fc_enable_checkout_page_template', true ) ) { return $template; }
+	public function checkout_template_hooks() {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
 
-		// Bail if not on checkout page.
-		if( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $template; }
+		// Theme header elements
+		add_action( 'fc_checkout_before_main_section_wrapper', array( $this, 'add_header_theme_elements' ), 10 );
 
-		// Locate new checkout page template
-		$_template = $this->locate_template_checkout_page_template( $template, 'checkout/page-checkout.php', null );
-
-		// Check if the file exists
-		if ( file_exists( $_template ) ) {
-			$template = $_template;
-		}
-
-		return $template;
+		// Theme footer elements
+		add_action( 'fc_checkout_after_main_section_wrapper', array( $this, 'add_footer_theme_elements' ), 10 );
 	}
 
+
+
 	/**
-	 * Locate template files from this plugin.
+	 * Add header elements from the theme.
 	 */
-	public function locate_template_checkout_page_template( $template, $template_name, $template_path ) {
-		$_template = null;
+	public function add_header_theme_elements() {
+		// Bail if theme's class is not available
+		if ( ! class_exists( 'Thrive_Template' ) ) { return; }
 
-		// Set template path to default value when not provided
-		if ( ! $template_path ) { $template_path = 'woocommerce/'; };
+		// Bail if theme's class method is not available
+		if ( ! method_exists( 'Thrive_Template', 'render_theme_hf_section' ) ) { return; }
 
-		// Get plugin path
-		$plugin_path = self::$directory_path . 'templates/compat/themes/thrive-theme/fc/checkout-page-template/';
+		$header = '<main id="wrapper">';
+		$header .= Thrive_Template::instance()->render_theme_hf_section( THRIVE_HEADER_SECTION );
 
-		// Get the template from this plugin, if it exists
-		if ( file_exists( $plugin_path . $template_name ) ) {
-			$_template = $plugin_path . $template_name;
+		echo $header;
+	}
 
-			// Look for template file in the theme
-			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
-				$_template_override = locate_template( array(
-					trailingslashit( $template_path ) . $template_name,
-					$template_name,
-				) );
-	
-				// Check if files exist before changing template
-				if ( file_exists( $_template_override ) ) {
-					$_template = $_template_override;
-				}
-			}
-		}
 
-		// Use default template
-		if ( ! $_template ) {
-			$_template = $template;
-		}
 
-		// Return what we found
-		return $_template;
+	/**
+	 * Add footer elements from the theme.
+	 */
+	function add_footer_theme_elements() {
+		// Bail if theme's class is not available
+		if ( ! class_exists( 'Thrive_Template' ) ) { return; }
+
+		// Bail if theme's class method is not available
+		if ( ! method_exists( 'Thrive_Template', 'render_theme_hf_section' ) ) { return; }
+
+		$footer = Thrive_Template::instance()->render_theme_hf_section( THRIVE_FOOTER_SECTION );
+		$footer .= '</main>';
+
+		echo $footer;
 	}
 
 
