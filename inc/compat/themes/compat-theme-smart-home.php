3181267a2450cad837a805be2343cce86b833d4c
vladdedikov <vlad@vladdedikov.ru>
2024-04-25 19:35:33 +0400

    Refactor Smart Home theme compatibility files

--- inc/compat/themes/compat-theme-smart-home.php
+++ inc/compat/themes/compat-theme-smart-home.php
@@ -22,11 +22,8 @@ class FluidCheckout_ThemeCompat_SmartHome extends FluidCheckout {
 		// Very late hooks
 		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
 
-		// Checkout page template
-		add_filter( 'template_include', array( $this, 'checkout_page_template' ), 100 );
-
-		// Template file loader
-		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template_checkout_page_template' ), 100, 3 );
+		// Checkout template hooks
+		$this->checkout_template_hooks();
 
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
@@ -69,73 +66,35 @@ class FluidCheckout_ThemeCompat_SmartHome extends FluidCheckout {
 
 
 	/**
-	 * Replace the checkout page template with our own file.
-	 *
-	 * @param   String  $template  Template file path.
+	 * Add checkout template hooks.
 	 */
-	public function checkout_page_template( $template ) {
+	public function checkout_template_hooks() {
 		// Bail if using distraction free header and footer
-		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
 
-		// Bail if checkout page template is not enabled
-		if ( true !== apply_filters( 'fc_enable_checkout_page_template', true ) ) { return $template; }
-
-		// Bail if not on checkout page.
-		if( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $template; }
-
-		// Locate new checkout page template
-		$_template = $this->locate_template_checkout_page_template( $template, 'checkout/page-checkout.php', null );
-
-		// Check if the file exists
-		if ( file_exists( $_template ) ) {
-			$template = $_template;
-		}
-
-		return $template;
+		// Theme's inner containers
+		add_action( 'fc_checkout_before_main_section', array( $this, 'add_inner_container_opening_tag' ), 10 );
+		add_action( 'fc_checkout_after_main_section', array( $this, 'add_inner_container_closing_tag' ), 10 );
 	}
 
 
 
 	/**
-	 * Locate template files from this plugin.
+	 * Add opening tag for inner container from the theme.
 	 */
-	public function locate_template_checkout_page_template( $template, $template_name, $template_path ) {
-		// Bail if using distraction free header and footer
-		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $template; }
-		
-		$_template = null;
+	public function add_inner_container_opening_tag() {
+		?>
+		<div class="large-12 columns">
+		<?php
+	}
 
-		// Set template path to default value when not provided
-		if ( ! $template_path ) { $template_path = 'woocommerce/'; };
-
-		// Get plugin path
-		$plugin_path = self::$directory_path . 'templates/compat/themes/smart-home/fc/checkout-page-template/';
-
-		// Get the template from this plugin, if it exists
-		if ( file_exists( $plugin_path . $template_name ) ) {
-			$_template = $plugin_path . $template_name;
-
-			// Look for template file in the theme
-			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
-				$_template_override = locate_template( array(
-					trailingslashit( $template_path ) . $template_name,
-					$template_name,
-				) );
-
-				// Check if files exist before changing template
-				if ( file_exists( $_template_override ) ) {
-					$_template = $_template_override;
-				}
-			}
-		}
-
-		// Use default template
-		if ( ! $_template ) {
-			$_template = $template;
-		}
-
-		// Return what we found
-		return $_template;
+	/**
+	 * Add closing tag for inner container from the theme.
+	 */
+	public function add_inner_container_closing_tag() {
+		?>
+		</div>
+		<?php
 	}
 
 
