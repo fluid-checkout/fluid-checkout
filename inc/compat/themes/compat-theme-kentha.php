c999c53e722bf17e8d780517033aaa9185d291a4
vladdedikov <vlad@vladdedikov.ru>
2024-05-05 19:15:25 +0400

    Fix container width and style

--- inc/compat/themes/compat-theme-kentha.php
+++ inc/compat/themes/compat-theme-kentha.php
@@ -19,11 +19,55 @@ class FluidCheckout_ThemeCompat_Kentha extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Checkout template hooks
+		$this->checkout_template_hooks();
+
 		// Dark mode
 		add_filter( 'fc_enable_dark_mode_styles', '__return_true', 10 );
 
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+
+
+	/**
+	 * Add checkout template hooks.
+	 */
+	public function checkout_template_hooks() {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// Theme's inner containers
+		add_action( 'fc_checkout_before_main_section', array( $this, 'add_inner_container_opening_tags' ), 10 );
+		add_action( 'fc_checkout_after_main_section', array( $this, 'add_inner_container_closing_tags' ), 10 );
+	}
+
+
+
+	/**
+	 * Add opening tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_opening_tags() {
+		?>
+		<div class="qt-main">
+			<div class="qt-container qt-main-contents">
+				<div class="qt-the-content qt-paper qt-paddedcontent qt-card">
+				<?php
+	}
+
+	/**
+	 * Add closing tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_closing_tags() {
+				?>
+				</div>
+			</div>
+		</div>
+		<?php
 	}
 
 
