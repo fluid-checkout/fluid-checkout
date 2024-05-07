6f12fbf7697fad264e6f0aadcf56099f1ebce892
vladdedikov <vlad@vladdedikov.ru>
2024-05-07 20:52:20 +0400

    Revert "Merge branch 'bugfix/checkout-template-disabled' into feature/compat-theme-kentha"
    This reverts commit e23fd0fd966f33bd2d8c2d95fb55d9179a918879, reversing
    changes made to 001e8a33a2226a5a6dbdf85a408f2dc4c8fb7888.

--- inc/checkout-page-template.php
+++ inc/checkout-page-template.php
@@ -74,8 +74,8 @@ class FluidCheckout_CheckoutPageTemplate extends FluidCheckout {
 	 * Disable custom template for the checkout page content part when using FSE (Full Site Editor).
 	 */
 	public function maybe_disable_checkout_page_template( $is_enabled ) {
-		// Disable if using distraction free header and footer
-		if ( $this->is_distraction_free_header_footer_checkout() ) { return false; }
+		// Disable not if using distraction free header and footer
+		if ( ! $this->is_distraction_free_header_footer_checkout() ) { return false; }
 
 		// Disable if theme is using FSE
 		if ( current_theme_supports( 'block-templates' ) ) { return false; }
