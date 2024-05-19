bc730f880440a120613e0b6f4fff10f415b62d5f
vladdedikov <vlad@vladdedikov.ru>
2024-05-20 00:26:18 +0400

    Fix container width on checkout page

--- inc/compat/themes/compat-theme-zota.php
+++ inc/compat/themes/compat-theme-zota.php
@@ -19,8 +19,52 @@ class FluidCheckout_ThemeCompat_Zota extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Checkout template hooks
+		$this->checkout_template_hooks();
+
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+
+
+	/**
+	 * Add checkout template hooks.
+	 */
+	public function checkout_template_hooks() {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// Theme's inner containers
+		add_action( 'fc_checkout_before_main_section', array( $this, 'add_inner_container_opening_tags' ), 10 );
+		add_action( 'fc_checkout_after_main_section', array( $this, 'add_inner_container_closing_tags' ), 10 );
+	}
+
+
+
+	/**
+	 * Add opening tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_opening_tags() {
+		?>
+		<div id="main-container" class="container">
+			<div class="row">
+				<div id="main-content" class="main-page col-12">
+				<?php
+	}
+
+	/**
+	 * Add closing tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_closing_tags() {
+				?>
+				</div>
+			</div>
+		</div>
+		<?php
 	}
 
 
