f1296649eca2215fe370f9b4f2c5a9b81c9a508f
vladdedikov <vlad@vladdedikov.ru>
2024-05-26 22:57:21 +0400

    Fix container width

---
+++ inc/compat/themes/compat-theme-bb-theme.php
@@ -0,0 +1,69 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Beaver Builder Theme (by Fastline Media).
+ */
+class FluidCheckout_ThemeCompat_BeaverBuilderTheme extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Checkout template hooks
+		$this->checkout_template_hooks();
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+	}
+
+
+
+	/**
+	 * Add checkout template hooks.
+	 */
+	public function checkout_template_hooks() {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return; }
+
+		// Theme's inner containers
+		add_action( 'fc_checkout_before_main_section', array( $this, 'add_inner_container_opening_tags' ), 10 );
+		add_action( 'fc_checkout_after_main_section', array( $this, 'add_inner_container_closing_tags' ), 10 );
+	}
+
+
+
+	/**
+	 * Add opening tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_opening_tags() {
+		?>
+		<div class="fl-content-full container">
+			<div class="row">
+				<div class="fl-content col-md-12">
+				<?php
+	}
+
+	/**
+	 * Add closing tags for inner container from the Hestia theme.
+	 */
+	public function add_inner_container_closing_tags() {
+				?>
+				</div>
+			</div>
+		</div>
+		<?php
+	}
+
+}
+
+FluidCheckout_ThemeCompat_BeaverBuilderTheme::instance();
