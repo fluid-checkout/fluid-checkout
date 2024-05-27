e548a8224d1480224fac812fcca2fc03c2779aa4
vladdedikov <vlad@vladdedikov.ru>
2024-05-27 12:41:37 +0400

    Fix select fields` style

--- inc/compat/themes/compat-theme-bb-theme.php
+++ inc/compat/themes/compat-theme-bb-theme.php
@@ -24,6 +24,9 @@ class FluidCheckout_ThemeCompat_BeaverBuilderTheme extends FluidCheckout {
 
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
@@ -62,6 +65,40 @@ class FluidCheckout_ThemeCompat_BeaverBuilderTheme extends FluidCheckout {
 			</div>
 		</div>
 		<?php
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Get default color values from the theme to use as fallbacks
+		$defaults = FLCustomizer::_get_default_mods();
+
+		// Get current theme colors from the customizer
+		$mods = FLCustomizer::get_mods();
+
+		// Get currently active colors
+		$accent_color = FLColor::hex( array( $mods['fl-accent'], $defaults['fl-accent'] ) );
+
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '34px',
+				'--fluidcheckout--field--padding-left' => '12px',
+				'--fluidcheckout--field--border-radius' => '4px',
+				'--fluidcheckout--field--border-width' => '1px',
+				'--fluidcheckout--field--border-color' => '#e6e6e6',
+				'--fluidcheckout--field--font-size' => '14px',
+				'--fluidcheckout--field--background-color--accent' => $accent_color,
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
