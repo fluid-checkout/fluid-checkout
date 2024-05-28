0016c6f967328a3c36fa01215e6d8616d82a561b
vladdedikov <vlad@vladdedikov.ru>
2024-05-28 23:25:11 +0400

    Fix select fields` style

--- inc/compat/themes/compat-theme-kenta.php
+++ inc/compat/themes/compat-theme-kenta.php
@@ -25,6 +25,9 @@ class FluidCheckout_ThemeCompat_Kenta extends FluidCheckout {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
@@ -39,6 +42,30 @@ class FluidCheckout_ThemeCompat_Kenta extends FluidCheckout {
 		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
 
 		return $class . ' kenta-container container mx-auto px-gutter';
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => 'var(--kenta-form-control-height, 45px)',
+				'--fluidcheckout--field--padding-left' => 'var(--kenta-form-control-paddding, 10.2px)',
+				'--fluidcheckout--field--border-radius' => 'var(--kenta-form-control-radius, 2px)',
+				'--fluidcheckout--field--border-color' => 'var(--kenta-form-border-color)',
+				'--fluidcheckout--field--font-size' => '13.6px',
+				'--fluidcheckout--field--background-color--accent' => 'var(--kenta-primary-color)',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 }
