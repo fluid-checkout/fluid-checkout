7a110d095b3e9277b202cddab83cb4c30bde5028
vladdedikov <vlad@vladdedikov.ru>
2024-05-28 22:52:20 +0400

    Fix container width on checkout page

--- inc/compat/themes/compat-theme-kenta.php
+++ inc/compat/themes/compat-theme-kenta.php
@@ -21,6 +21,24 @@ class FluidCheckout_ThemeCompat_Kenta extends FluidCheckout {
 	public function hooks() {
 		// Remove theme hooks related to checkout page
 		remove_action( 'wp', 'kenta_modify_template_hooks_after_init', 10 );
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
+	}
+
+
+
+	/**
+	 * Add container class to the main content element.
+	 *
+	 * @param string $class Main content element classes.
+	 */
+	public function change_fc_content_section_class( $class ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
+
+		return $class . ' kenta-container container mx-auto px-gutter';
 	}
 
 }
