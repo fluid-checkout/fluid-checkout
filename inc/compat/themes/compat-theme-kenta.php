24b1f002cdd0ecb2c8c997f3be2867f4f7180379
vladdedikov <vlad@vladdedikov.ru>
2024-06-03 12:53:59 +0400

    Fix theme color variables no loading in distraction free mode

--- inc/compat/themes/compat-theme-kenta.php
+++ inc/compat/themes/compat-theme-kenta.php
@@ -19,8 +19,8 @@ class FluidCheckout_ThemeCompat_Kenta extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Remove theme hooks related to checkout page
-		remove_action( 'wp', 'kenta_modify_template_hooks_after_init', 10 );
+		// Template file loader
+		add_filter( 'woocommerce_locate_template', array( $this, 'locate_template_checkout_page_template' ), 100, 3 );
 
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
@@ -32,6 +32,50 @@ class FluidCheckout_ThemeCompat_Kenta extends FluidCheckout {
 
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Remove theme hooks related to checkout page
+		remove_action( 'wp', 'kenta_modify_template_hooks_after_init', 10 );
+	}
+
+
+
+	/**
+	 * Locate template files from this plugin.
+	 */
+	public function locate_template_checkout_page_template( $template, $template_name, $template_path ) {
+		$_template = null;
+
+		// Set template path to default value when not provided
+		if ( ! $template_path ) { $template_path = 'woocommerce/'; };
+
+		// Get plugin path
+		$plugin_path = self::$directory_path . 'templates/compat/themes/kenta/checkout-page-template/';
+
+		// Get the template from this plugin, if it exists
+		if ( file_exists( $plugin_path . $template_name ) ) {
+			$_template = $plugin_path . $template_name;
+
+			// Look for template file in the theme
+			if ( apply_filters( 'fc_override_template_with_theme_file', false, $template, $template_name, $template_path ) ) {
+				$_template_override = locate_template( array(
+					trailingslashit( $template_path ) . $template_name,
+					$template_name,
+				) );
+	
+				// Check if files exist before changing template
+				if ( file_exists( $_template_override ) ) {
+					$_template = $_template_override;
+				}
+			}
+		}
+
+		// Use default template
+		if ( ! $_template ) {
+			$_template = $template;
+		}
+
+		// Return what we found
+		return $_template;
 	}
 
 
---
+++ templates/compat/themes/kenta/checkout-page-template/checkout/page-checkout-header.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The header for the checkout page.
+ *
+  * This template can be overridden by copying it to yourtheme/woocommerce/checkout/page-checkout-header.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package fluid-checkout
+ * @version 3.0.3
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+// Custom attribute handling.
+$custom_attributes_esc = '';
+$custom_attributes = apply_filters( 'fc_checkout_body_custom_attributes', array() );
+if ( is_array( $custom_attributes ) ) {
+    foreach ( $custom_attributes as $attribute => $attribute_value ) {
+        $custom_attributes_esc .= ' ' . esc_attr( $attribute ) . '="' . esc_attr( $attribute_value ) . '"';
+    }
+}
+?>
+<!DOCTYPE html>
+<?php // CHANGE: Added "kenta_html_attributes" function from the theme ?>
+<html <?php language_attributes(); ?> <?php kenta_html_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo( 'charset' ); ?>">
+<link rel="profile" href="http://gmpg.org/xfn/11">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5">
+
+<?php if ( isset( $meta_theme_color ) ) : ?>
+    <meta name="theme-color" content="<?php echo esc_attr( $meta_theme_color ); ?>">
+<?php endif; ?>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php echo $custom_attributes_esc; // WPCS: XSS ok. ?>>
+
+<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'fluid-checkout' ); ?></a>
+
+<?php do_action( 'fc_checkout_header' ); ?>
+
+<main id="main" class="content-area fc-main">
