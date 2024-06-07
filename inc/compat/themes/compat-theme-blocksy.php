e9b958eeb365dfcd3ceeecb062258f84e7a46135
vladdedikov <vlad@vladdedikov.ru>
2024-06-07 16:23:31 +0400

    Change relative header selector for sticky elements

--- inc/compat/themes/compat-theme-blocksy.php
+++ inc/compat/themes/compat-theme-blocksy.php
@@ -22,6 +22,10 @@ class FluidCheckout_ThemeCompat_Blocksy extends FluidCheckout {
 		// Very late hooks
 		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
 
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
 		// Settings
 		add_filter( 'fc_integrations_settings_add', array( $this, 'add_settings' ), 10 );
 
@@ -60,6 +64,64 @@ class FluidCheckout_ThemeCompat_Blocksy extends FluidCheckout {
 
 		// Enqueue
 		add_action( 'wp_enqueue_scripts', array( $this, 'maybe_enqueue_assets' ), 10 );
+	}
+
+
+
+	/**
+	 * Change the element used to position the progress bar and order summary when sticky.
+	 * 
+	 * @param  array  $attributes  The elements attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $attributes; }
+
+		// Bail if Blocksy instance is not available
+		if ( ! class_exists( 'Blocksy\Plugin' ) || ! method_exists( 'Blocksy\Plugin', 'instance' ) ) { return $attributes; }
+
+		$blocksy = Blocksy\Plugin::instance();
+
+		// Bail if header property is not available
+		if ( ! isset( $blocksy->header ) || ! is_object( $blocksy->header ) ) { return $attributes; }
+
+		$blocksy_header = $blocksy->header;
+
+		// Bail if method is not available
+		if ( ! method_exists( $blocksy_header, 'current_screen_has_sticky' ) ) { return $attributes; }
+
+		// Get sticky header settings from Blocksy
+		$sticky_header_settings = $blocksy_header->current_screen_has_sticky();
+
+		// Bail if theme's conditions for sticky header are not met
+		if ( ! $sticky_header_settings || ! is_array( $sticky_header_settings ) ) { return $attributes; }
+
+		// Bail if no devices are selected for sticky header
+		if ( ! isset( $sticky_header_settings['devices'] ) || empty( $sticky_header_settings['devices'] ) ) { return $attributes; }
+
+		// Default values
+		$mobile_settings = '';
+		$desktop_settings = '';
+
+		// Set the sticky header settings based on the selected devices
+		if ( in_array( 'desktop', $sticky_header_settings['devices'] ) ) {
+			$desktop_settings = '"sm": { "breakpointInitial": 1000, "breakpointFinal": 10000, "selector": "header [data-sticky*=yes]" }';
+		}
+		if ( in_array( 'mobile', $sticky_header_settings['devices'] ) ) {
+			$mobile_settings = '"xs": { "breakpointInitial": 0, "breakpointFinal": 999, "selector": "header [data-sticky*=yes]" }';
+		}
+
+		// Only keep non-empty values
+		$settings = array_filter( array( $mobile_settings, $desktop_settings ), function( $value ) {
+			return !empty( $value );
+		} );
+
+		// Concatenate values with a comma
+		$settings = implode( ', ', $settings );
+
+		$attributes['data-sticky-relative-to'] = "{ {$settings} }";
+
+		return $attributes;
 	}
 
 
