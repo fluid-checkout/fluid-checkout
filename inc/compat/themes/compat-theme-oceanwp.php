a6fc9aadab102c0b81529aed9d1ed2f4665ebb0c
vladdedikov <vlad@vladdedikov.ru>
2024-07-05 11:43:14 +0400

    Add CSS variables

--- inc/compat/themes/compat-theme-oceanwp.php
+++ inc/compat/themes/compat-theme-oceanwp.php
@@ -145,12 +145,17 @@ class FluidCheckout_ThemeCompat_OceanWP extends FluidCheckout {
 	 * @param  array  $css_variables  The CSS variables key/value pairs.
 	 */
 	public function add_css_variables( $css_variables ) {
+		// Get theme colors
+		$border_color = get_theme_mod( 'ocean_input_border_color', '#dddddd' );
+
 		// Add CSS variables
 		$new_css_variables = array(
 			':root' => array(
 				// Form field styles
 				'--fluidcheckout--field--height' => '40px',
 				'--fluidcheckout--field--padding-left' => '12px',
+				'--fluidcheckout--field--border-width' => '1px',
+				'--fluidcheckout--field--border-color' => $border_color,
 				'--fluidcheckout--field--border-radius' => '3px',
 				'--fluidcheckout--field--font-size' => '14px',
 			),
