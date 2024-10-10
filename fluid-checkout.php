820f4a46f14ca4afc10bf911dda1312b3650eef2
Diego Versiani <diego@fluidcheckout.com>
2024-10-10 14:52:28 +0200

    * Bump tested up to WooCommerce 9.3.3

--- fluid-checkout.php
+++ fluid-checkout.php
@@ -9,7 +9,7 @@ Version: 3.2.5-beta-14
 Author: Fluid Checkout
 Author URI: https://fluidcheckout.com/
 WC requires at least: 5.0
-WC tested up to: 9.3.1
+WC tested up to: 9.3.3
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 License: GPLv3
 
