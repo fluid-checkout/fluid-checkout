bfba342d06d20a650e570e07d564921633b089be
vladdedikov <vlad@vladdedikov.ru>
2024-10-24 19:04:45 +0900

    Edit comments

--- inc/compat/themes/compat-theme-uncode.php
+++ inc/compat/themes/compat-theme-uncode.php
@@ -87,7 +87,7 @@ class FluidCheckout_ThemeCompat_Uncode extends FluidCheckout {
 
 
 	/**
-	 * Add opening tags for inner container from the Hestia theme.
+	 * Add opening tags for inner container from the theme.
 	 */
 	public function add_inner_container_opening_tags() {
 		// Get container info from the theme
@@ -117,7 +117,7 @@ class FluidCheckout_ThemeCompat_Uncode extends FluidCheckout {
 	}
 
 	/**
-	 * Add closing tags for inner container from the Hestia theme.
+	 * Add closing tags for inner container from the theme.
 	 */
 	public function add_inner_container_closing_tags() {
 			?>
