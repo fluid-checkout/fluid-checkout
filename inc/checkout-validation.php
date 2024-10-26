318822d95fbd6b56a31a0facffb780f4c7198470
Diego Versiani <diego@fluidcheckout.com>
2024-10-26 16:35:19 +0200

    * Fixed: Instant validation skipping some fields added by 3rd-party plugins.

--- inc/checkout-validation.php
+++ inc/checkout-validation.php
@@ -177,8 +177,8 @@ class FluidCheckout_Validation extends FluidCheckout {
 
 		$settings[ 'checkoutValidation' ] = apply_filters( 'fc_checkout_validation_script_settings', array(
 			'formRowSelector'                    => '.form-row, .shipping-method__package',
-			'validateFieldsSelector'             => '.input-text, .input-checkbox, select, .shipping-method__options',
-			'referenceNodeSelector'              => '.input-text, .input-checkbox, select, .shipping-method__options',
+			'validateFieldsSelector'             => 'input:not([type="hidden"]):not([type="checkbox"]):not([type="radio"]):not([type="button"]):not([type="reset"]):not([type="submit"]), .input-text, .input-checkbox, select, .shipping-method__options',
+			'referenceNodeSelector'              => 'input:not([type="hidden"]):not([type="checkbox"]):not([type="radio"]):not([type="button"]):not([type="reset"]):not([type="submit"]), .input-text, .input-checkbox, select, .shipping-method__options',
 			'alwaysValidateFieldsSelector'       => '',
 			'mailcheckSuggestions'               => array(
 				/* translators: %s: html for the email address typo correction suggestion link */
