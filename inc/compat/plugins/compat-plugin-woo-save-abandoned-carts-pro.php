10867f60d11202006585dd4248f7f5b5b2fe6044
vladdedikov <vlad@vladdedikov.ru>
2024-11-27 15:52:42 +0400

    Fix shipping and billing fields data not saving when CartBounty plugins are active

---
+++ inc/compat/plugins/compat-plugin-woo-save-abandoned-carts-pro.php
@@ -0,0 +1,34 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with plugin: CartBounty Pro - Save and recover abandoned carts for WooCommerce (by Streamline.lv).
+ */
+class FluidCheckout_WooSaveAbandonedCartsPro extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Get plugin object instance
+		$class_object = $this->get_object_by_class_name_from_hooks( 'CartBounty_Pro_Public' );
+
+		// Bail if class object is not found in hooks
+		if ( ! $class_object ) { return; }
+
+		// Remove input data restoring feature conflicting with Fluid Checkout
+		remove_filter( 'wp', array( $class_object, 'restore_input_data' ), 10 );
+	}
+
+}
+
+FluidCheckout_WooSaveAbandonedCartsPro::instance();
