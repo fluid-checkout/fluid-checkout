e411ac14ac0d92ea0c9c39ac2feecda92098e87d
vladdedikov <vlad@vladdedikov.ru>
2024-12-29 22:54:04 +0400

    Fix layout issues on checkuot page on mobile

--- inc/compat/themes/compat-theme-loobek.php
+++ inc/compat/themes/compat-theme-loobek.php
@@ -23,15 +23,17 @@ class FluidCheckout_ThemeCompat_Loobek extends FluidCheckout {
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
 		// Coupon bar
 		remove_action( 'woocommerce_before_checkout_form', 'woocommerce_checkout_coupon_form', 20 );
+		remove_action( 'woocommerce_before_checkout_form', 'loobek_before_checkout_form_start', 1 );
+		remove_action( 'woocommerce_before_checkout_form', 'loobek_before_checkout_form_end', 999 );
 
 		// Quantity buttons
 		remove_action( 'woocommerce_before_quantity_input_field', 'loobek_before_quantity_input_field', 1 );
 		remove_action( 'woocommerce_after_quantity_input_field', 'loobek_after_quantity_input_field', 99 );
-
-		// CSS variables
-		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 	}
 
 
