e81c3dddd4b4fab4303804b42d7300875346591e
vladdedikov <vlad@vladdedikov.ru>
2025-01-07 23:10:15 +0400

    Fix duplicated thumbnails in order summary section

--- inc/compat/themes/compat-theme-motta.php
+++ inc/compat/themes/compat-theme-motta.php
@@ -7,6 +7,13 @@ defined( 'ABSPATH' ) || exit;
 class FluidCheckout_ThemeCompat_Motta extends FluidCheckout {
 
 	/**
+	 * Class name for the theme which this compatibility class is related to.
+	 */
+	public const CLASS_NAME = '\Motta\WooCommerce\Checkout';
+
+
+
+	/**
 	 * __construct function.
 	 */
 	public function __construct() {
@@ -19,8 +26,17 @@ class FluidCheckout_ThemeCompat_Motta extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Bail if class method is not available
+		if ( ! method_exists( self::CLASS_NAME, 'instance' ) ) { return $is_verified; }
+
+		// Get class object
+		$class_object = call_user_func( array( self::CLASS_NAME, 'instance' ) );
+
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// Product thumbnails
+		remove_filter( 'woocommerce_cart_item_name', array( $class_object, 'review_product_name_html' ), 10, 3);
 	}
 
 }
