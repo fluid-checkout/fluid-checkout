963dc31c110d7fb7ccd0a195ba4da06f55c6ec1e
vladdedikov <vlad@vladdedikov.ru>
2025-01-08 21:36:12 +0400

    Change relative selector for sticky elements

--- inc/compat/themes/compat-theme-motta.php
+++ inc/compat/themes/compat-theme-motta.php
@@ -38,6 +38,10 @@ class FluidCheckout_ThemeCompat_Motta extends FluidCheckout {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
 
+		// Sticky elements
+		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
+
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 
@@ -58,12 +62,34 @@ class FluidCheckout_ThemeCompat_Motta extends FluidCheckout {
 
 
 	/**
+	 * Change the sticky element relative ID.
+	 *
+	 * @param   array   $attributes    HTML element attributes.
+	 */
+	public function change_sticky_elements_relative_header( $attributes ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $attributes; }
+
+		// Bail if required class method is not available
+		if ( ! method_exists( '\Motta\Helper', 'get_option' ) ) { return $attributes; }
+
+		// Bail if sticky header is not enabled
+		$sticky_header = \Motta\Helper::get_option( 'header_sticky' );
+		if ( ! $sticky_header || 'none' === $sticky ) { return $attributes; }
+
+		$attributes[ 'data-sticky-relative-to' ] = '.header-sticky';
+
+		return $attributes;
+	}
+
+
+
+	/**
 	 * Add CSS variables.
 	 * 
 	 * @param  array  $css_variables  The CSS variables key/value pairs.
 	 */
 	public function add_css_variables( $css_variables ) {
-
 		// Add CSS variables
 		$new_css_variables = array(
 			':root' => array(
