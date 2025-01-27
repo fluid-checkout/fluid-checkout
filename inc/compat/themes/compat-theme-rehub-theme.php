587ed48ad5afe78a9144d482850d55526c7f8c59
vladdedikov <vlad@vladdedikov.ru>
2025-01-27 10:48:52 +0400

    Fix order summary section layout

--- inc/compat/themes/compat-theme-rehub-theme.php
+++ inc/compat/themes/compat-theme-rehub-theme.php
@@ -21,6 +21,10 @@ class FluidCheckout_ThemeCompat_RehubTheme extends FluidCheckout {
 	public function hooks() {
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
+		// Order review section layout
+		remove_action( 'woocommerce_checkout_before_order_review_heading', 'rehub_woo_order_checkout', 10 );
+		remove_action( 'woocommerce_checkout_after_order_review', 'rehub_woo_after_order_checkout', 10 );
 	}
 
 }
