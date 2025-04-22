2f0f2781122cf9cce81b87f4a0fa43e4c2fdbbc5
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-04-22 22:32:53 +0200

    Add style class to buttons

---
+++ inc/compat/themes/compat-theme-twentytwentyfour.php
@@ -0,0 +1,72 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Twenty Twenty-Four (by The WordPress Team).
+ */
+class FluidCheckout_ThemeCompat_TwentyTwentyFour extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+
+		// Buttons
+		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_substep_save_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_coupon_code_apply_button_classes', array( $this, 'add_button_class' ), 10 );
+	}
+
+	/**
+	 * Add or remove very late hooks.
+	 */
+	public function very_late_hooks() {
+		// Order summary
+		// $this->order_summary_hooks();
+	}
+
+	/**
+	 * Add button class from the theme.
+	 * 
+	 * @param  array  $classes  The button classes.
+	 */
+	public function add_button_class( $classes ) {
+		// Add 'wp-element-button' class to apply theme styles and 'qodef-m-checkout-link' to add hover animation
+		if ( is_array( $classes ) ) {
+			array_push( $classes, 'wp-element-button' );
+		} 
+		else {
+			$classes .= ' wp-element-button';
+		}
+
+		return $classes;
+	}
+
+	/**
+	 * Add or remove hooks for the checkout order summary.
+	 */
+	public function order_summary_hooks() {
+		// Bail if not on the checkout page
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
+
+          // Bail if theme classes or functions not available
+		if ( ! class_exists( 'WC_Twenty_Twenty_Four' ) ) { return; }
+
+		// Remove hooks
+		remove_action( 'woocommerce_checkout_before_order_review_heading', array( 'WC_Twenty_Twenty_Four', 'before_order_review' ) );
+		remove_action( 'woocommerce_checkout_after_order_review', array( 'WC_Twenty_Twenty_Four', 'after_order_review' ) );
+	}
+}
+
+FluidCheckout_ThemeCompat_TwentyTwentyFour::instance();
