4efea316683b1e5e8301745b6f18834ce5ef1d42
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-04-23 14:39:30 +0200

    Cleaning compat file

--- inc/compat/themes/compat-theme-twentytwentyfour.php
+++ inc/compat/themes/compat-theme-twentytwentyfour.php
@@ -19,9 +19,6 @@ class FluidCheckout_ThemeCompat_TwentyTwentyFour extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Very late hooks
-		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
-
 		// Buttons
 		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
 		add_filter( 'fc_substep_save_button_classes', array( $this, 'add_button_class' ), 10 );
@@ -29,13 +26,7 @@ class FluidCheckout_ThemeCompat_TwentyTwentyFour extends FluidCheckout {
 		add_filter( 'fc_checkout_login_button_classes', array( $this, 'add_button_class' ), 10 );
 	}
 
-	/**
-	 * Add or remove very late hooks.
-	 */
-	public function very_late_hooks() {
-		// Order summary
-		// $this->order_summary_hooks();
-	}
+
 
 	/**
 	 * Add button class from the theme.
@@ -54,21 +45,6 @@ class FluidCheckout_ThemeCompat_TwentyTwentyFour extends FluidCheckout {
 		return $classes;
 	}
 
-	//! Remove later if not needed
-	// /**
-	//  * Add or remove hooks for the checkout order summary.
-	//  */
-	// public function order_summary_hooks() {
-	// 	// Bail if not on the checkout page
-	// 	if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
-
-     //      // Bail if theme classes or functions not available
-	// 	if ( ! class_exists( 'WC_Twenty_Twenty_Four' ) ) { return; }
-
-	// 	// Remove hooks
-	// 	remove_action( 'woocommerce_checkout_before_order_review_heading', array( 'WC_Twenty_Twenty_Four', 'before_order_review' ) );
-	// 	remove_action( 'woocommerce_checkout_after_order_review', array( 'WC_Twenty_Twenty_Four', 'after_order_review' ) );
-	// }
 }
 
 FluidCheckout_ThemeCompat_TwentyTwentyFour::instance();
