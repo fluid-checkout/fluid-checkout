75db5cc9c362e662654e969f9c4a546f07441ace
Diego Versiani <diego@fluidcheckout.com>
2025-05-07 17:19:14 +0200

    Fix field height for themes Twenty Twenty-Five and Twenty-Four.

--- inc/compat/themes/compat-theme-twentytwentyfive.php
+++ inc/compat/themes/compat-theme-twentytwentyfive.php
@@ -19,11 +19,34 @@ class FluidCheckout_ThemeCompat_TwentyTwentyFive extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
 		// Buttons
 		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
 		add_filter( 'fc_substep_save_button_classes', array( $this, 'add_button_class' ), 10 );
 		add_filter( 'fc_coupon_code_apply_button_classes', array( $this, 'add_button_class' ), 10 );
 		add_filter( 'fc_checkout_login_button_classes', array( $this, 'add_button_class' ), 10 );
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '52.8px',
+				'--fluidcheckout--field--padding-left' => '1.1rem',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
 	}
 
 
