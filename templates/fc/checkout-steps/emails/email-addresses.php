e58e4c4042b4be0330ef39aeff4b33b95f2258d5
Diego Versiani <diego@fluidcheckout.com>
2025-05-31 09:30:36 +0100

    Fix version number on changed template files.

--- templates/fc/checkout-steps/emails/email-addresses.php
+++ templates/fc/checkout-steps/emails/email-addresses.php
@@ -13,7 +13,7 @@
  * @see https://woocommerce.com/document/template-structure/
  * @package WooCommerce\Templates\Emails
  * @version 9.8.0
- * @fc-version 4.0.7
+ * @fc-version 4.0.6
  */
 
 use Automattic\WooCommerce\Utilities\FeaturesUtil;
