78c0fe9ba75615f3617ac67ae35d4e60611c1db9
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-07-21 14:28:18 -0300

    Adds theme classes to buttons

--- inc/compat/themes/compat-theme-cartzilla.php
+++ inc/compat/themes/compat-theme-cartzilla.php
@@ -27,6 +27,13 @@ class FluidCheckout_ThemeCompat_Cartzilla extends FluidCheckout {
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
 
+		// Buttons
+		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_substep_save_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_coupon_code_apply_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_place_order_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_checkout_login_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_add_payment_method_button_classes', array( $this, 'add_button_class' ), 10 );
 	}
 
 
@@ -66,6 +73,26 @@ class FluidCheckout_ThemeCompat_Cartzilla extends FluidCheckout {
 		);
 
 		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
+	}
+
+
+
+
+	/**
+	 * Add button class from the theme.
+	 * 
+	 * @param  array  $classes  The button classes.
+	 */
+	public function add_button_class( $classes ) {
+		// Add 'btn btn-primary' class to apply theme styles
+		if ( is_array( $classes ) ) {
+			array_push( $classes, 'btn btn-primary' );
+		} 
+		else {
+			$classes .= ' btn btn-primary';
+		}
+
+		return $classes;
 	}
 
 }
