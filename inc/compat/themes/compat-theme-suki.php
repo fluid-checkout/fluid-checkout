2dbe6349799ca232f21e5e2fa3cc9ff301cc680f
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-07-23 15:00:07 -0300

    Add late hooks to the Suki theme compatibility class to remove quantity input buttons and their associated scripts.

--- inc/compat/themes/compat-theme-suki.php
+++ inc/compat/themes/compat-theme-suki.php
@@ -17,8 +17,22 @@ class FluidCheckout_ThemeCompat_Suki extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Late hooks
+		add_action( 'init', array( $this, 'late_hooks' ), 100 );
+
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+
+
+	/**
+	 * Add or remove late hooks.
+	 */
+	public function late_hooks() {
+		// Remove plus and minus buttons to the quantity input from suki theme.
+		remove_action( 'woocommerce_after_quantity_input_field', array( Suki_Compatibility_WooCommerce::instance(), 'add_quantity_plus_minus_buttons' ) );
+		remove_action( 'wp_enqueue_scripts', array( Suki_Compatibility_WooCommerce::instance(), 'add_quantity_plus_minus_buttons_scripts' ) );
 	}
 
 
@@ -32,7 +46,7 @@ class FluidCheckout_ThemeCompat_Suki extends FluidCheckout {
 		// Add CSS variables
 		$new_css_variables = array(
 			':root' => array(
-				// Double check if theme is using variables for these values
+				// ! Double check if theme is using variables for these values
 				// Form field styles
 				'--fluidcheckout--field--height' => '40px',
 				'--fluidcheckout--field--padding-left' => '12px',
