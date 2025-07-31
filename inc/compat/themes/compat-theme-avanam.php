29afc7ac871b1d79450547dae49905b7fc9b6a7b
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-07-31 11:45:57 -0300

    Fixes container container class for checkout

--- inc/compat/themes/compat-theme-avanam.php
+++ inc/compat/themes/compat-theme-avanam.php
@@ -19,12 +19,18 @@ class FluidCheckout_ThemeCompat_Avanam extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Container
+		add_filter( 'fc_checkout_container_attributes', array( $this, 'change_container_attributes' ), 20 );
 	}
 
 
