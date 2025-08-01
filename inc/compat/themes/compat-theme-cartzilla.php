7be4bef770dd038a1de06b761f82fe480b967978
Diego Versiani <diego@fluidcheckout.com>
2025-08-01 18:32:59 +0200

    Fix checkout page container class.

--- inc/compat/themes/compat-theme-cartzilla.php
+++ inc/compat/themes/compat-theme-cartzilla.php
@@ -19,13 +19,16 @@ class FluidCheckout_ThemeCompat_Cartzilla extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-
 		// Sticky elements
 		add_filter( 'fc_checkout_progress_bar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 		add_filter( 'fc_checkout_sidebar_attributes', array( $this, 'change_sticky_elements_relative_header' ), 20 );
 
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
+		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
 
 		// Buttons
 		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
@@ -80,6 +83,20 @@ class FluidCheckout_ThemeCompat_Cartzilla extends FluidCheckout {
 		);
 
 		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
+	}
+
+
+
+	/**
+	 * Add container class to the main content element.
+	 *
+	 * @param string $class Main content element classes.
+	 */
+	public function change_fc_content_section_class( $class ) {
+		// Bail if using distraction free header and footer
+		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
+
+		return $class . ' container';
 	}
 
 
