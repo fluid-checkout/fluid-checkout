30e5ce0c58cc907e01f1d9231d776f36607ce244
vladdedikov <vlad@vladdedikov.ru>
2025-08-05 20:06:48 +0400

    Fix wrong data being saved to the customer object when using PayPal express checkout

--- inc/compat/plugins/compat-plugin-pymntpl-paypal-woocommerce.php
+++ inc/compat/plugins/compat-plugin-pymntpl-paypal-woocommerce.php
@@ -19,8 +19,8 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Very late hooks
-		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+		// Late hooks
+		add_action( 'init', array( $this, 'late_hooks' ), 100 );
 
 		// Register assets
 		add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ), 5 );
@@ -30,101 +30,24 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 		add_action( 'woocommerce_checkout_billing', array( $this, 'output_custom_hidden_fields' ), 10 );
 
 		// Prevent saving field values to session
-		add_filter( 'fc_customer_persisted_data_skip_fields', array( $this, 'add_persisted_data_skip_fields' ), 10, 2 );
+		add_filter( 'fc_customer_persisted_data_skip_fields', array( $this, 'add_custom_field_to_persisted_data_skip_fields' ), 10, 2 );
 		add_filter( 'fc_customer_persisted_data_skip_fields', array( $this, 'maybe_add_shipping_and_billing_fields_to_persisted_data_skip_fields' ), 10, 2 );
+
+		// Customer address properties
+		add_action( 'woocommerce_checkout_update_customer', array( $this, 'maybe_reset_customer_address_props_on_process_checkout' ), 10, 2 );
+		add_action( 'woocommerce_before_calculate_totals', array( $this, 'maybe_reset_customer_address_props_on_paypal_rest_request' ), 20 );
+
+		// Saved address
+		add_filter( 'fc_save_new_address_data_shipping_skip_update', array( $this, 'maybe_prevent_update_saved_address' ), 10, 2 );
+		add_filter( 'fc_save_new_address_data_billing_skip_update', array( $this, 'maybe_prevent_update_saved_address' ), 10, 2 );
 	}
 
 	/**
-	 * Add or remove very late hooks.
+	 * Add or remove late hooks.
 	 */
-	public function very_late_hooks() {
-		$this->maybe_replace_customer_address_data_hooks();
-	}
-
-
-
-	/**
-	 * Maybe replace customer address data hooks.
-	 * Required to prevent Fluid Checkout altering field values used in PayPal popup window.
-	 */
-	public function maybe_replace_customer_address_data_hooks() {
-		// Bail if PayPal is not selected as a payment method
-		if ( 'ppcp' !== FluidCheckout_Steps::instance()->get_selected_payment_method() ) { return; }
-
-		// Undo default customer address data hooks
-		FluidCheckout_Steps::instance()->undo_customer_address_data_hooks();
-
-		// Re-add customer address data hooks
-		$this->customer_address_data_hooks();
-	}
-
-	/**
-	 * Add or remove hooks for the customer address data.
-	 */
-	public function customer_address_data_hooks() {
-		// Define fields to add hooks to, even if the fields are not available at checkout.
-		$field_keys = array(
-			'billing_first_name',
-			'billing_last_name',
-			'billing_company',
-			'billing_address_1',
-			'billing_address_2',
-			'billing_city',
-			'billing_state',
-			'billing_postcode',
-			'billing_country',
-			'billing_email',
-			'billing_phone',
-
-			'shipping_first_name',
-			'shipping_last_name',
-			'shipping_company',
-			'shipping_address_1',
-			'shipping_address_2',
-			'shipping_city',
-			'shipping_state',
-			'shipping_postcode',
-			'shipping_country',
-			'shipping_phone',
-		);
-
-		// Iterate fields and add hook
-		foreach ( $field_keys as $field_key ) {
-			add_filter( 'woocommerce_customer_get_' . $field_key, array( $this, 'maybe_change_customer_address_field_value_from_checkout_data' ), 10, 2 );
-		}
-	}
-
-
-
-	/**
-	 * Maybe change the customer address field value to get data saved to the checkout session.
-	 *
-	 * @param   mixed        $value      The field value.
-	 * @param   WC_Customer  $customer   The customer object.
-	 */
-	public function maybe_change_customer_address_field_value_from_checkout_data( $value, $customer ) {
-		// Bail if this is a REST API request or if the checkout process has already started
-		if ( ( defined( 'REST_REQUEST' ) && REST_REQUEST ) || did_action( 'woocommerce_before_checkout_process' ) ) { return $value; }
-
-		// Get name of the current filter hook running this function
-		$hook_name = current_filter();
-
-		// Bail if the hook name is not supported
-		if ( strpos( $hook_name, 'woocommerce_customer_get_' ) !== 0 ) { return $value; }
-
-		// Get field key
-		$field_key = str_replace( 'woocommerce_customer_get_', '', $hook_name );
-
-		// Get checkout session value
-		$session_value = FluidCheckout_Steps::instance()->get_checkout_field_value_from_session_or_posted_data( $field_key );
-
-		// Maybe set new value from session value
-		if ( ! empty( $session_value ) ) {
-			$value = $session_value;
-		}
-
-		// Return new value
-		return $value;
+	public function late_hooks() {
+		// Customer address data
+		add_filter( 'fc_skip_change_customer_address_field_value_from_checkout_data', array( $this, 'maybe_skip_change_customer_address_field_value' ), 10, 2 );
 	}
 
 
@@ -174,12 +97,12 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 
 
 	/**
-	 * Add "altered fields" indicator to the persisted data skip list.
+	 * Add custom field to the persisted data skip list.
 	 *
 	 * @param  array  $skip_field_keys     Checkout field keys to skip from saving to the session.
 	 * @param  array  $parsed_posted_data  All parsed post data.
 	 */
-	public function add_persisted_data_skip_fields( $skip_field_keys, $parsed_posted_data ) {
+	public function add_custom_field_to_persisted_data_skip_fields( $skip_field_keys, $parsed_posted_data ) {
 		$fields_keys = array(
 			'pymntpl-paypal-woocommerce-fields_altered',
 		);
@@ -188,8 +111,8 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 	}
 
 	/**
-	 * Maybe add shipping and billing fields to the persisted data skip list.
-	 * Required to prevent saving wrong field values while keeping the `update_checkout` event running.
+	 * Maybe add shipping and billing address fields to the persisted data skip list.
+	 * Required to prevent saving wrong field values while the `update_checkout` AJAX event is running.
 	 *
 	 * @param  array  $skip_field_keys     Checkout field keys to skip from saving to the session.
 	 * @param  array  $parsed_posted_data  All parsed post data.
@@ -214,6 +137,7 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 			'billing_country',
 			'billing_email',
 			'billing_phone',
+
 			'shipping_first_name',
 			'shipping_last_name',
 			'shipping_company',
@@ -226,6 +150,119 @@ class FluidCheckout_PymntplPayPalWooCommerce extends FluidCheckout {
 		);
 
 		return array_merge( $skip_field_keys, $fields_keys );
+	}
+
+
+
+	/**
+	 * Maybe reset customer address properties after the plugin's REST request handling.
+	 * Required to set the properties back to the saved address values after the plugin sets 
+	 * the customer data to the field values in CartShipping::update_shipping_address().
+	 *
+	 * @param  WC_Customer  $customer    The customer object.
+	 * @param  array        $posted_data  The posted data.
+	 */
+	public function maybe_reset_customer_address_props_on_process_checkout( $customer, $posted_data ) {
+		// Bail if custom field value hasn't been changed
+		if ( empty( $_POST[ 'pymntpl-paypal-woocommerce-fields_altered' ] ) ) { return; }
+
+		// Reset properties to the saved values
+		$this->reset_customer_address_props( $customer );
+	}
+
+	/**
+	 * Maybe reset customer address properties after the plugin's REST request handling.
+	 * Required to set the properties back to the saved address values after WooCommerce 
+	 * sets the customer data to the field values in WC_AJAX::update_order_review().
+	 *
+	 * @param  WC_Cart  $cart  The cart object.
+	 */
+	public function maybe_reset_customer_address_props_on_paypal_rest_request( $cart ) {
+		// Bail if not a REST request from the plugin
+		if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST ) { return; }
+		if ( ! isset( $_SERVER[ 'REQUEST_URI' ] ) || false === strpos( $_SERVER[ 'REQUEST_URI' ], 'cart/shipping' ) ) { return; }
+
+		// Bail if customer object not available
+		if ( ! function_exists( 'WC' ) || null === WC()->customer ) { return; }
+
+		// Bail if custom field value hasn't been changed
+		if ( empty( $_POST[ 'pymntpl-paypal-woocommerce-fields_altered' ] ) ) { return; }
+
+		// Reset properties to the saved values
+		$this->reset_customer_address_props( WC()->customer );
+	}
+
+	/**
+	 * Reset customer address properties to the saved values.
+	 *
+	 * @param  WC_Customer  $customer  The customer object.
+	 */
+	public function reset_customer_address_props( $customer ) {
+		// Get all supported address field keys
+		$customer_supported_field_keys = FluidCheckout_Steps::instance()->get_supported_customer_property_field_keys();
+
+		// Loop through each supported field
+		foreach ( $customer_supported_field_keys as $field_key ) {
+			// Only act on shipping or billing fields
+			if ( strpos( $field_key, 'shipping_' ) !== 0 && strpos( $field_key, 'billing_' ) !== 0 ) { continue; }
+
+			// Replace `shipping_` and `billing_` prefixes with `save_shipping_` and `save_billing_`
+			$save_field_key = preg_replace( '/^(shipping_|billing_)/', 'save_$1', $field_key );
+
+			// Retrieve the original value from the session
+			$new_field_value = FluidCheckout_Steps::instance()->get_checkout_field_value_from_session( $save_field_key );
+
+			// Get the name of the setter method for the customer property
+			$setter = "set_$field_key";
+
+			// Check if the customer object has this setter method
+			if ( is_callable( array( $customer, $setter ) ) ) {
+				// If we have an original value, set it back on the customer object
+				if ( null !== $new_field_value ) {
+					$customer->{$setter}( $new_field_value );
+				}
+			}
+		}
+	}
+
+
+
+	/**
+	 * Maybe skip updating the saved address values.
+	 *
+	 * @param  bool  $skip_update  Whether to skip the update.
+	 */
+	public function maybe_prevent_update_saved_address( $skip_update, $posted_data ) {
+		// Bail if custom field value hasn't been changed
+		if ( empty( $posted_data[ 'pymntpl-paypal-woocommerce-fields_altered' ] ) ) { return $skip_update; }
+
+		// Skip updating address data
+		$skip_update = true;
+
+		return $skip_update;
+	}
+
+
+
+	/**
+	 * Maybe skip changing customer address field value.
+	 * Required to prevent Fluid Checkout altering field values used by PayPal.
+	 * 
+	 * @param  bool  $skip  Whether to skip changing the customer address field value.
+	 */
+	public function maybe_skip_change_customer_address_field_value( $skip ) {
+		// Bail if not on front end
+		if ( is_admin() ) { return $skip; }
+
+		// Bail if PayPal is not selected as a payment method
+		if ( 'ppcp' !== FluidCheckout_Steps::instance()->get_selected_payment_method() ) { return $skip; }
+
+		// Skip if this is a REST request or if the checkout process has started
+		if ( ( defined( 'REST_REQUEST' ) && REST_REQUEST ) || did_action( 'woocommerce_before_checkout_process' ) ) {
+			$skip = true;
+		}
+
+		return $skip;
 	}
 
 }
