c90a76c7cf35ee14d3da6e4547838593b5f432b0
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-08-06 10:05:30 -0300

    Add container class filter and update CSS variable handling in BeTheme compatibility

--- inc/compat/themes/compat-theme-betheme.php
+++ inc/compat/themes/compat-theme-betheme.php
@@ -29,6 +29,7 @@ class FluidCheckout_ThemeCompat_BeTheme extends FluidCheckout {
 		$this->checkout_template_hooks();
 
 		// Container class
+		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
 
 		// Sticky elements
