5be342e0ab61f0201c665d601f2aaa6d120813a8
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-08-06 09:56:19 -0300

    Fix content classes

--- inc/compat/themes/compat-theme-betheme.php
+++ inc/compat/themes/compat-theme-betheme.php
@@ -29,7 +29,6 @@ class FluidCheckout_ThemeCompat_BeTheme extends FluidCheckout {
 		$this->checkout_template_hooks();
 
 		// Container class
-		add_filter( 'fc_add_container_class', '__return_false', 10 );
 		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
 
 		// Sticky elements
@@ -138,7 +137,7 @@ class FluidCheckout_ThemeCompat_BeTheme extends FluidCheckout {
 		// Bail if using distraction free header and footer
 		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
 
-		return $class . ' content_wrapper';
+		return $class . ' container';
 	}
 
 
