25c25d954494bf490bab4562bac5c05d9d79546e
vladdedikov <vlad@vladdedikov.ru>
2025-08-10 15:16:08 +0400

    Override FooEvents plugin script

--- inc/compat/plugins/compat-plugin-fooevents.php
+++ inc/compat/plugins/compat-plugin-fooevents.php
@@ -19,8 +19,30 @@ class FluidCheckout_FooEvents extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Late hooks
+		add_action( 'init', array( $this, 'late_hooks' ), 100 );
+
 		// Validation script settings
 		add_filter( 'fc_checkout_validation_script_settings', array( $this, 'add_email_selector_to_validation_script_settings' ), 10 );
+	}
+
+	/**
+	 * Add or remove late hooks.
+	 */
+	public function late_hooks() {
+		// Scripts
+		add_action( 'wp_enqueue_scripts', array( $this, 'maybe_replace_plugin_scripts' ), 5 );
+	}
+
+	/**
+	 * Replace plugin scripts with modified version.
+	 */
+	public function maybe_replace_plugin_scripts() {
+		// Bail if not at checkout
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return; }
+
+		// Plugin's script
+		wp_register_script( 'woocommerce-events-front-script', FluidCheckout_Enqueue::instance()->get_script_url( 'js/compat/plugins/fooevents/events-frontend' ), array( 'jquery' ), NULL, array( 'in_footer' => true, 'strategy' => 'defer' ) );
 	}
 
 
