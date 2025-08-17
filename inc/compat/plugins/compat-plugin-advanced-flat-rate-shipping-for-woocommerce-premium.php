696db10a14a983a6b6b025003eae185842d2e853
vladdedikov <vlad@vladdedikov.ru>
2025-08-17 10:23:51 +0500

    Fix tooltip missing for "forceall" shipping method

--- inc/compat/plugins/compat-plugin-advanced-flat-rate-shipping-for-woocommerce-premium.php
+++ inc/compat/plugins/compat-plugin-advanced-flat-rate-shipping-for-woocommerce-premium.php
@@ -7,9 +7,28 @@ defined( 'ABSPATH' ) || exit;
 class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidCheckout {
 
 	/**
+	 * Class name for the plugin which this compatibility class is related to.
+	 */
+	public const PUBLIC_CLASS_NAME = 'Advanced_Flat_Rate_Shipping_For_WooCommerce_Pro_Public';
+
+
+	/**
+	 * Plugin's public class object.
+	 */
+	public $public_class_object;
+
+
+
+	/**
 	 * __construct function.
 	 */
 	public function __construct( ) {
+		// Maybe set class object from the plugin
+		if ( class_exists( self::PUBLIC_CLASS_NAME ) ) {
+			// Get object
+			$this->public_class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( self::PUBLIC_CLASS_NAME );
+		}
+
 		$this->hooks();
 	}
 
@@ -19,11 +38,17 @@ class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidC
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Bail if class object is not available
+		if ( ! is_object( $this->public_class_object ) ) { return; }
+
 		// Shipping method tooltip
 		$this->maybe_change_shipping_method_tooltip_position();
 
 		// Shipping method subtitle
 		add_action( 'woocommerce_after_shipping_rate', array( $this, 'maybe_add_shipping_method_subtitle' ), 10 );
+
+		// Order summary
+		remove_filter( 'woocommerce_cart_shipping_method_full_label', array( $this->public_class_object, 'afrsm_pro_wc_cart_shipping_method_label_callback' ), 10, 2 );
 	}
 
 
@@ -32,20 +57,11 @@ class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidC
 	 * Maybe Change shipping method tooltip position.
 	 */
 	public function maybe_change_shipping_method_tooltip_position() {
-		// Define class name
-		$class_name = 'Advanced_Flat_Rate_Shipping_For_WooCommerce_Pro_Public';
-
-		// Bail if class is not available
-		if ( ! class_exists( $class_name ) ) { return; }
-
-		// Get class object
-		$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
-
 		// Bail if class object or its method is not available
-		if ( ! is_object( $class_object ) || ! method_exists( $class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return; }
+		if ( ! is_object( $this->public_class_object ) || ! method_exists( $this->public_class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return; }
 
 		// Change tooltip position
-		remove_filter( 'woocommerce_after_shipping_rate', array( $class_object, 'afrsm_add_tooltip_and_subtitle_callback' ), 10 );
+		remove_filter( 'woocommerce_after_shipping_rate', array( $this->public_class_object, 'afrsm_add_tooltip_and_subtitle_callback' ), 10 );
 		add_filter( 'fc_shipping_method_option_label_markup', array( $this, 'maybe_add_tooltip_icon_to_shipping_method_label' ), 10, 2 );
 	}
 
@@ -89,20 +105,11 @@ class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidC
 		// Bail if tooltip type is not subtitle
 		if ( 'subtitle' !== $tooltip_type ) { return; }
 
-		// Define class name
-		$class_name = 'Advanced_Flat_Rate_Shipping_For_WooCommerce_Pro_Public';
-
-		// Bail if class is not available
-		if ( ! class_exists( $class_name ) ) { return; }
-
-		// Get class object
-		$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
-
 		// Bail if class object or its method is not available
-		if ( ! is_object( $class_object ) || ! method_exists( $class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return; }
+		if ( ! is_object( $this->public_class_object ) || ! method_exists( $this->public_class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return; }
 
 		// Output the subtitle
-		$class_object->afrsm_add_tooltip_and_subtitle_callback( $method );
+		$this->public_class_object->afrsm_add_tooltip_and_subtitle_callback( $method );
 	}
 
 
@@ -114,17 +121,8 @@ class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidC
 	 * @param  object  $method  The shipping method object.
 	 */
 	public function maybe_add_tooltip_icon_to_shipping_method_label( $label, $method ) {
-		// Define class name
-		$class_name = 'Advanced_Flat_Rate_Shipping_For_WooCommerce_Pro_Public';
-
-		// Bail if class is not available
-		if ( ! class_exists( $class_name ) ) { return $label; }
-
-		// Get class object
-		$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
-
 		// Bail if class object or its method is not available
-		if ( ! is_object( $class_object ) || ! method_exists( $class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return $label; }
+		if ( ! is_object( $this->public_class_object ) || ! method_exists( $this->public_class_object, 'afrsm_add_tooltip_and_subtitle_callback' ) ) { return $label; }
 
 		// Maybe get tooltip type from post meta
 		$tooltip_type = $this->get_tooltip_type_for_shipping_method( $method );
@@ -132,10 +130,26 @@ class FluidCheckout_AdvancedFlatRateShippingForWooCommercePremium extends FluidC
 		// Bail if not a tooltip
 		if ( 'tooltip' !== $tooltip_type ) { return $label; }
 
-		// Get tooltip HTML from the plugin
-		ob_start();
-		$class_object->afrsm_add_tooltip_and_subtitle_callback( $method );
-		$tooltip = ob_get_clean();
+		$tooltip = '';
+
+		// The plugin's standard tooltip function, `afrsm_add_tooltip_and_subtitle_callback`, does not work for "forceall" method
+		// because the plugin adds the tooltip via the `cart-shipping.php` template override.
+		// Therefore, we need to reconstruct the tooltip here.
+		if ( false !== strpos( $method->id, 'forceall' ) ) {
+			// Get method label
+			$forceall_label = FluidCheckout_Settings::instance()->get_option( 'forceall_label' , __( 'Combine Shipping', 'advanced-flat-rate-shipping-for-woocommerce' ) );
+
+			// Get tooltip HTML from the plugin
+			$method_data = $this->public_class_object->afrsm_pro_forceall_label_for_cart__premium_only( '', '', $method, $forceall_label );
+			$tooltip = $method_data[ 'tool_tip_html' ];
+		}
+		// Otherwise, use general tooltip method from the plugin
+		else {
+			// Get tooltip HTML from the plugin
+			ob_start();
+			$this->public_class_object->afrsm_add_tooltip_and_subtitle_callback( $method );
+			$tooltip = ob_get_clean();
+		}
 
 		// Insert tooltip before closing `span` tag
 		if ( ! empty( $tooltip ) ) {
