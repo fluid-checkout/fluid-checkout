08ed1b3df4f4f79a46a9c3951bf38bc505dd0373
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-10-13 11:03:52 -0300

    Includes button style files and draft files for select2

---
+++ inc/compat/themes/compat-theme-kiosko.php
@@ -0,0 +1,79 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with theme: Kiosko (by Automattic).
+ */
+class FluidCheckout_ThemeCompat_Kiosko extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Buttons
+		add_filter( 'fc_next_step_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_substep_save_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_coupon_code_apply_button_classes', array( $this, 'add_button_class' ), 10 );
+		add_filter( 'fc_checkout_login_button_classes', array( $this, 'add_button_class' ), 10 );
+	}
+
+
+
+	/**
+	 * Add CSS variables.
+	 * 
+	 * @param  array  $css_variables  The CSS variables key/value pairs.
+	 */
+	public function add_css_variables( $css_variables ) {
+		// Add CSS variables
+		$new_css_variables = array(
+			':root' => array(
+				// Form field styles
+				'--fluidcheckout--field--height' => '50.3px',
+				'--fluidcheckout--field--padding-left' => '1.1rem',
+
+				'--fluidcheckout--field--height' => '47.7969px',
+				'--fluidcheckout--field--padding-left' => '17.6px',
+				'--fluidcheckout--field--border-radius' => '4px',
+				'--fluidcheckout--field--border-color' => 'var(--wc-form-border-color, #212121)',
+				'--fluidcheckout--field--text-color' => 'inherit',
+			),
+		);
+
+		return FluidCheckout_DesignTemplates::instance()->merge_css_variables( $css_variables, $new_css_variables );
+	}
+
+
+
+	/**
+	 * Add button class from the theme.
+	 * 
+	 * @param  array  $classes  The button classes.
+	 */
+	public function add_button_class( $classes ) {
+		// Add 'wp-element-button' class to apply theme styles and 'qodef-m-checkout-link' to add hover animation
+		if ( is_array( $classes ) ) {
+			array_push( $classes, 'wp-element-button' );
+		} 
+		else {
+			$classes .= ' wp-element-button';
+		}
+
+		return $classes;
+	}
+
+}
+
+FluidCheckout_ThemeCompat_Kiosko::instance();
