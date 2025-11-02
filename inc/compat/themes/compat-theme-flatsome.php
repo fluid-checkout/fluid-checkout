727ebc86936851fef576bf0930a027cfaaabb598
vladdedikov <vlad@vladdedikov.ru>
2025-11-02 14:07:53 +0400

    Fix page container width

--- inc/compat/themes/compat-theme-flatsome.php
+++ inc/compat/themes/compat-theme-flatsome.php
@@ -32,7 +32,6 @@ class FluidCheckout_ThemeCompat_Flatsome extends FluidCheckout {
 
 		// Container class
 		add_filter( 'fc_add_container_class', '__return_false', 10 );
-		add_filter( 'fc_content_section_class', array( $this, 'change_fc_content_section_class' ), 10 );
 
 		// Make header static (not sticky at the top)
 		add_filter( 'theme_mod_header_sticky', array( $this, 'change_theme_mod_header_sticky' ), 10 );
@@ -126,20 +125,6 @@ class FluidCheckout_ThemeCompat_Flatsome extends FluidCheckout {
 		if ( ! get_theme_mod( 'checkout_floating_labels', 0 ) ) { return; }
 
 		FluidCheckout_Enqueue::instance()->dequeue_enhanced_select_assets();
-	}
-
-
-
-	/**
-	 * Add container class to the main content element.
-	 *
-	 * @param string $class Main content element classes.
-	 */
-	public function change_fc_content_section_class( $class ) {
-		// Bail if using distraction free header and footer
-		if ( FluidCheckout_CheckoutPageTemplate::instance()->is_distraction_free_header_footer_checkout() ) { return $class; }
-
-		return $class . ' container';
 	}
 
 
