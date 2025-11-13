240711aa9a0a349ac289571a7aa70ed6dfc775f5
vladdedikov <vlad@vladdedikov.ru>
2025-11-13 23:09:28 +0400

    Change "Available coupons" section location

---
+++ inc/compat/plugins/compat-plugin-woocommerce-smart-coupons.php
@@ -0,0 +1,37 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with plugin: WooCommerce Smart Coupons (by StoreApps).
+ */
+class FluidCheckout_WooCommerceSmartCoupons extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Bail if class is not available
+		$class_name = 'WC_SC_Display_Coupons';
+		if ( ! class_exists( $class_name ) ) { return; }
+
+		// Bail if class object is not found
+		$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
+		if ( ! is_object( $class_object ) ) { return; }
+
+		// Available coupons section
+		remove_action( 'woocommerce_checkout_before_customer_details', array( $class_object, 'show_available_coupons_on_classic_checkout' ), 11 );
+		add_action( 'fc_checkout_before_steps', array( $class_object, 'show_available_coupons_on_classic_checkout' ), 10 );
+	}
+
+}
+
+FluidCheckout_WooCommerceSmartCoupons::instance();
