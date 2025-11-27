5286f675f0749b478ca21978119b683cf422fe18
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-11-27 15:26:02 -0300

    Add compatibility for WC City Select plugin with asset registration.

---
+++ inc/compat/plugins/compat-plugin-wc-city-select.php
@@ -0,0 +1,37 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with plugin: WC City Select (by 8manos).
+ */
+class FluidCheckout_WC_City_Select extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Register assets
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_assets' ), 5 );
+	}
+
+
+	/**
+	 * Register assets.
+	 */
+	public function register_assets() {
+		// Scripts
+		wp_register_script( 'wc-city-select', FluidCheckout_Enqueue::instance()->get_script_url( 'js/compat/plugins/states-cities-and-places-for-woocommerce/place-select' ), array( 'jquery', 'woocommerce' ), NULL, array( 'in_footer' => true, 'strategy' => 'defer' ) );
+	}
+
+}
+
+FluidCheckout_WC_City_Select::instance();
+
