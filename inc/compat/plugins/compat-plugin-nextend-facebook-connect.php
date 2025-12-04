cbe48004ef1e0136ddd35e9a935f821a52ec100a
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-12-04 14:10:24 -0300

    Refactor social login button output logic in checkout contact step

--- inc/compat/plugins/compat-plugin-nextend-facebook-connect.php
+++ inc/compat/plugins/compat-plugin-nextend-facebook-connect.php
@@ -17,20 +17,27 @@ class FluidCheckout_NextendSocialLogin extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Add social login buttons below the login CTA at the checkout contact step.
-		add_action( 'fc_checkout_social_login', array( $this, 'maybe_output_checkout_social_login_buttons' ), 20 );
+		// Maybe add the action to output the Nextend Social Login buttons at the checkout contact step.
+		$this->maybe_add_action_to_output_checkout_social_login_buttons();
 	}
 
 	/**
-	 * Maybe outputs the Nextend Social Login buttons at the checkout contact step.
+	 * Maybe adds the action to output the Nextend Social Login buttons at the checkout contact step.
 	 */
-	public function maybe_output_checkout_social_login_buttons() {
+	public function maybe_add_action_to_output_checkout_social_login_buttons() {
 		// Bail if Nextend Social Login add on pro 
 		if ( ! class_exists( 'NextendSocialLoginPRO' ) ) { return; }
 
 		// Bail if option "No Connect button in billing form" is enabled.
 		if ( NextendSocialLogin::$settings->get( 'woocommerce_billing' ) === '' ) { return; }
 
+		add_action( 'fc_checkout_social_login', array( $this, 'output_checkout_social_login_buttons' ), 20 );
+	}
+
+	/**
+	 * Outputs the Nextend Social Login buttons at the checkout contact step.
+	 */
+	public function output_checkout_social_login_buttons() {
 		// Output the Nextend Social Login buttons with the alignment
 		$align = NextendSocialLogin::$settings->get( 'woocoommerce_form_button_align' );
 		// ! we should get all settings from the plugin and pass them to the renderButtonsWithContainer function
@@ -39,6 +46,8 @@ class FluidCheckout_NextendSocialLogin extends FluidCheckout {
 		// Output the Nextend Social Login buttons.
 		echo $buttons;
 	}
+
+
 
 }
 
