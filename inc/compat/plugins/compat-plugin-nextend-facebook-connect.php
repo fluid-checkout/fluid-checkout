8caca876ae2f0d4037f472ff9d426abc450edced
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2025-12-08 15:40:12 -0300

    Add compatibility for Nextend Social Login plugin
    - Introduced new CSS styles for social login buttons and separators.
    - Implemented PHP class for handling social login integration at the checkout.
    - Updated checkout form template to include social login options.
    - Adjusted existing styles to accommodate new elements in both LTR and RTL stylesheets.

---
+++ inc/compat/plugins/compat-plugin-nextend-facebook-connect.php
@@ -0,0 +1,92 @@
+<?php
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Compatibility with plugin: Nextend Social Login (by Nextend).
+ */
+class FluidCheckout_NextendSocialLogin extends FluidCheckout {
+
+	/**
+	 * __construct function.
+	 */
+	public function __construct() {
+		$this->hooks();
+	}
+
+	/**
+	 * Initialize hooks.
+	 */
+	public function hooks() {
+		// Maybe add the action to output the Nextend Social Login buttons at the checkout contact step.
+		$this->maybe_add_action_to_output_checkout_social_login_buttons();
+
+		// Remove billing form hooks from Nextend Social Login.
+		$this->remove_billing_form_hooks_from_nextend_social_login();
+	}
+
+
+
+	/**
+	 * Maybe adds the action to output the Nextend Social Login buttons at the checkout contact step.
+	 */
+	public function maybe_add_action_to_output_checkout_social_login_buttons() {
+		// Bail if Nextend Social Login Pro isn't active as the hooks originate from the add-on.
+		if ( ! class_exists( 'NextendSocialLoginPRO' ) ) { return; }
+
+		// Bail if option "No Connect button in billing form" is enabled.
+		if ( NextendSocialLogin::$settings->get( 'woocommerce_billing' ) === '' ) { return; }
+
+		add_action( 'fc_checkout_social_login', array( $this, 'output_checkout_social_login_buttons' ), 20 );
+	}
+
+
+
+	/**
+	 * Outputs the Nextend Social Login buttons at the checkout contact step.
+	 */
+	public function output_checkout_social_login_buttons() {
+		// Output the Nextend Social Login buttons using the plugin's settings.
+		$style     = NextendSocialLogin::$settings->get( 'woocoommerce_form_button_style' );
+		$align     = NextendSocialLogin::$settings->get( 'woocoommerce_form_button_align' );
+		$labelType = 'login';
+		$buttons   = NextendSocialLogin::renderButtonsWithContainer( $style, false, false, false, $align, $labelType );
+
+		// Output the Nextend Social Login buttons.
+		echo $buttons;
+	}
+
+
+
+	/**
+	 * Removes the billing form hooks from Nextend Social Login.
+	 */
+	public function remove_billing_form_hooks_from_nextend_social_login() {
+		// Bail if Nextend Social Login Pro isn't active as the hooks originate from the add-on.
+		if ( ! class_exists( 'NextendSocialLoginPRO' ) ) {
+			return;
+		}
+
+		$hooks = array(
+			'woocommerce_before_checkout_billing_form',
+			'woocommerce_after_checkout_billing_form',
+			'woocommerce_before_checkout_registration_form',
+			'woocommerce_after_checkout_registration_form',
+			'woocommerce_before_checkout_shipping_form',
+			'woocommerce_after_checkout_shipping_form',
+		);
+
+		$callbacks = array(
+			'NextendSocialLoginPRO::woocommerce_before_checkout_billing_form',
+			'NextendSocialLoginPRO::woocommerce_after_checkout_billing_form',
+		);
+
+		foreach ( $hooks as $hook ) {
+			foreach ( $callbacks as $callback ) {
+				remove_action( $hook, $callback );
+			}
+		}
+	}
+
+}
+
+FluidCheckout_NextendSocialLogin::instance();
--- templates/fc/checkout-steps/checkout/form-contact-login.php
+++ templates/fc/checkout-steps/checkout/form-contact-login.php
@@ -49,6 +49,15 @@ defined( 'ABSPATH' ) || exit;
 			<?php endif; ?>
 		</div>
 
+		<?php if ( has_action( 'fc_checkout_social_login' ) ) : ?>
+			<div class="fc-contact-login__social-login-separator">
+				<span class="fc-contact-login__social-login-separator-text">
+					<?php echo esc_html( apply_filters( 'fc_checkout_social_login_separator_text', __( 'Or', 'fluid-checkout' ) ) ); ?>
+				</span>
+			</div>
+			<?php do_action( 'fc_checkout_social_login' ); ?>
+		<?php endif; ?>
+
 		<div class="fc-contact-login__separator">
 			<?php if ( 'yes' === FluidCheckout_Settings::instance()->get_option( 'woocommerce_enable_guest_checkout' ) ) : ?>
 				<span class="fc-contact-login__separator-text"><?php echo esc_html( apply_filters( 'fc_checkout_login_separator_text', _x( 'Or continue as a guest', 'Log in separator label at for when guest checkout is disabled', 'fluid-checkout' ) ) ); ?></span>
