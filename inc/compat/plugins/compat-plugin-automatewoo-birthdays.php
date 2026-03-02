c7b0809a99c17cec71c3d60b420d9d1578d9c1a8
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2026-03-02 14:41:44 -0300

    Refactor

--- inc/compat/plugins/compat-plugin-automatewoo-birthdays.php
+++ inc/compat/plugins/compat-plugin-automatewoo-birthdays.php
@@ -17,34 +17,19 @@ class FluidCheckout_AutomateWooBirthdays extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		add_action( 'woocommerce_before_checkout_form', array( $this, 'maybe_add_birthday_field_hook' ), 20 );
+		// Checkout fields
+		add_action( 'fc_checkout_after_step_billing_fields', array( $this, 'maybe_output_birthday_field' ), 10 );
 	}
 
 	/**
-	 * Add the birthday field to fc_checkout_after_step_billing_fields when
-	 * AutomateWoo Birthdays placement is set to "After billing details".
+	 * Output the AutomateWoo birthday field when placement is "After billing details".
 	 */
-	public function maybe_add_birthday_field_hook() {
+	public function maybe_output_birthday_field() {
 		// Bail if AutomateWoo Birthdays is not available
-		if ( ! function_exists( 'AW_Birthdays' ) ) {
-			return;
-		}
+		if ( ! function_exists( 'AW_Birthdays' ) ) { return; }
 
 		// Bail if placement is not "after billing details"
-		if ( 'after_billing_details' !== AW_Birthdays()->options()->checkout_field_placement() ) {
-			return;
-		}
-
-		add_action( 'fc_checkout_after_step_billing_fields', array( $this, 'output_birthday_field' ), 10 );
-	}
-
-	/**
-	 * Output the AutomateWoo birthday field.
-	 */
-	public function output_birthday_field() {
-		if ( ! function_exists( 'AW_Birthdays' ) ) {
-			return;
-		}
+		if ( 'after_billing_details' !== AW_Birthdays()->options()->checkout_field_placement() ) { return; }
 
 		\AutomateWoo\Birthdays\Frontend::add_birthday_field_to_checkout_form();
 	}
