87ec47509bbd68db8382680cadbd67e8808cf7b5
Diego Versiani <diego@fluidcheckout.com>
2026-03-24 22:22:28 +0100

    Display estimated delivery time for FedEx shipping.

--- inc/compat/plugins/compat-plugin-woocommerce-shipping-pro-fedex.php
+++ inc/compat/plugins/compat-plugin-woocommerce-shipping-pro-fedex.php
@@ -7,18 +7,6 @@ defined( 'ABSPATH' ) || exit;
 class FluidCheckout_WooCommerce_Shipping_Pro_FedEx extends FluidCheckout {	
 
 	/**
-	 * Class name for hooks from the plugin.
-	 */
-	private $class_name = 'WSPF_WC_Shipping';
-
-	/**
-	 * Class object for hooks from the plugin.
-	 */
-	private $class_object;
-
-
-
-	/**
 	 * __construct function.
 	 */
 	public function __construct() {
@@ -31,26 +19,95 @@ class FluidCheckout_WooCommerce_Shipping_Pro_FedEx extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
+		// Very late hooks
+		add_action( 'wp', array( $this, 'very_late_hooks' ), 100 );
+
 		// Init hooks
-		$this->init_hooks();
+		$this->shipping_init_hooks();
 	}
 
 	/**
-	 * Add or remove late hooks.
+	 * Add or remove very late hooks.
 	 */
-	public function init_hooks() {
+	public function very_late_hooks() {
+		// Shipping method hooks
+		$this->shipping_method_hooks();
+	}
+
+	/**
+	 * Add or remove shipping init hooks.
+	 */
+	public function shipping_init_hooks() {
+		// Initialize variables
+		$class_name = 'WSPF_WC_Shipping';
+		$class_object = null;
+
 		// Maybe set class object
-		if ( class_exists( $this->class_name ) ) {
+		if ( class_exists( $class_name ) ) {
 			// Get object
-			$this->class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $this->class_name );
+			$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
 		}
 
 		// Bail if class object is not set
-		if ( ! $this->class_object ) { return; }
+		if ( ! $class_object ) { return; }
 
 		// Move shipping method initialization to init hook to make it available earlier
-		remove_action( 'woocommerce_shipping_init', array( $this->class_object, 'wspf_fedex_method_init' ) );
-		add_action( 'init', array( $this->class_object, 'wspf_fedex_method_init' ), 100 );
+		remove_action( 'woocommerce_shipping_init', array( $class_object, 'wspf_fedex_method_init' ) );
+		add_action( 'init', array( $class_object, 'wspf_fedex_method_init' ), 100 );
+	}
+
+	/**
+	 * Add or remove shipping method hooks.
+	 */
+	public function shipping_method_hooks() {
+		// Initialize variables
+		$class_name = 'WS_Shipping_FedEx';
+		$class_object = null;
+
+		// Maybe set class object
+		if ( class_exists( $class_name ) ) {
+			// Get object
+			$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
+		}
+
+		// Bail if class object is not set
+		if ( ! $class_object ) { return; }
+
+		// Remove delivery time filter
+		remove_filter( 'woocommerce_cart_shipping_method_full_label', array( $class_object, 'wspf_add_delivery_time' ), 10, 2 );
+		add_filter( 'fc_shipping_method_option_description', array( $this, 'add_method_delivery_time' ), 10, 2 );
+	}
+
+
+
+	/**
+	 * Add delivery time to shipping method option markup.
+	 */
+	public function add_method_delivery_time( $description, $method ) {
+		// Bail if not target method
+		if ( 'wspf_fedex' !== $method->method_id ) { return $description; }
+
+		// Initialize variables
+		$class_name = 'WS_Shipping_FedEx';
+		$class_object = null;
+
+		// Maybe set class object
+		if ( class_exists( $class_name ) ) {
+			// Get object
+			$class_object = FluidCheckout::instance()->get_object_by_class_name_from_hooks( $class_name );
+		}
+
+		// Bail if class object is not set
+		if ( ! $class_object ) { return $description; }
+
+		// Get delivery time from shipping method
+		$description = $class_object->wspf_add_delivery_time( $description, $method );
+
+		// Remove first extra line break, and `small` tags
+		$description = preg_replace( '/<br\s*\/?>/', '', $description, 1 );
+		$description = preg_replace( '/<small\s*\/?>/', '', $description );
+
+		return $description;
 	}
 
 }
