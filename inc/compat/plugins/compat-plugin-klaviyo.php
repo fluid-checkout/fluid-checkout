b8bea73f1c45bb1cf0f6d826e66e6b787bfb112d
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2026-06-02 11:54:23 -0300

    Fix compatibility with Klaviyo plugin to prevent fatal errors at checkout due to renamed SMS compliance callback in newer versions.

--- inc/compat/plugins/compat-plugin-klaviyo.php
+++ inc/compat/plugins/compat-plugin-klaviyo.php
@@ -48,8 +48,14 @@ class FluidCheckout_Klaviyo extends FluidCheckout {
 		// Bail if SMS subscribe checkbox not enabled
 		if ( ! isset( $klaviyo_settings[ 'klaviyo_sms_subscribe_checkbox' ] ) || ! $klaviyo_settings[ 'klaviyo_sms_subscribe_checkbox' ] || empty( $klaviyo_settings[ 'klaviyo_sms_list_id' ] ) ) { return; }
 
+		// Klaviyo renamed the compliance callback in newer versions.
+		$sms_compliance_callback = $this->get_sms_compliance_callback();
+
 		// Move SMS compliance fields
-		remove_filter( 'woocommerce_after_checkout_billing_form', 'kl_sms_compliance_text', 10 );
+		if ( $sms_compliance_callback ) {
+			remove_filter( 'woocommerce_after_checkout_billing_form', $sms_compliance_callback, 10 );
+		}
+
 		add_filter( 'woocommerce_checkout_fields', array( $this, 'maybe_change_sms_compliance_checkbox_field_args' ), 100 );
 		add_filter( 'fc_checkout_contact_step_field_ids', array( $this, 'maybe_add_sms_compliance_checkout_field_to_contact_step' ), 10 );
 	}
@@ -104,10 +110,15 @@ class FluidCheckout_Klaviyo extends FluidCheckout {
 	public function maybe_change_sms_compliance_checkbox_field_args( $fields ) {
 		// SMS Compliance
 		if ( array_key_exists( 'billing', $fields ) && array_key_exists( 'kl_sms_consent_checkbox', $fields[ 'billing' ] ) ) {
-			// Get SMS compliance text
-			ob_start();
-			kl_sms_compliance_text();
-			$sms_compliance_text = ob_get_clean();
+			$sms_compliance_text = '';
+
+			// Get SMS compliance text from the callback available in the active Klaviyo version.
+			$sms_compliance_callback = $this->get_sms_compliance_callback();
+			if ( $sms_compliance_callback ) {
+				ob_start();
+				call_user_func( $sms_compliance_callback );
+				$sms_compliance_text = ob_get_clean();
+			}
 
 			// Change field args
 			$fields[ 'billing' ][ 'kl_sms_consent_checkbox' ][ 'priority' ] = 210;
@@ -115,6 +126,21 @@ class FluidCheckout_Klaviyo extends FluidCheckout {
 		}
 
 		return $fields;
+	}
+
+
+
+	/**
+	 * Get the SMS compliance callback name available for the active Klaviyo version.
+	 */
+	public function get_sms_compliance_callback() {
+		// Klaviyo 3.8+.
+		if ( function_exists( 'kl_mobile_compliance_text' ) ) { return 'kl_mobile_compliance_text'; }
+
+		// Klaviyo <= 3.6.x.
+		if ( function_exists( 'kl_sms_compliance_text' ) ) { return 'kl_sms_compliance_text'; }
+
+		return false;
 	}
 
 }
--- readme.txt
+++ readme.txt
@@ -354,6 +354,10 @@ The plugin provides widget areas in strategic positions on the checkout page for
 
 == Changelog ==
 
+= Unreleased =
+
+* Fixed: Compatibility with plugin Klaviyo. Prevent fatal error at checkout with newer versions where SMS compliance callback names changed.
+
 = 4.2.3 - 2026-05-25 =
 
 * Bump tested up to WordPress 7.0 and WooCommerce 10.8.0
