589ab8758d7010d1be9e591b748e40cc08ad2ba7
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2026-06-02 09:06:53 -0300

    Refactor button class comment and add Stripe UPE styling in Twenty Twenty-Five theme
    - Updated comment in the add_button_class method to clarify the purpose of the 'wp-element-button' class.
    - Added a comment for the Stripe UPE payment method styling in the SCSS file.
    - Ensured proper formatting in the order summary SCSS file by adding a newline at the end.

--- inc/compat/themes/compat-theme-twentytwentyfive.php
+++ inc/compat/themes/compat-theme-twentytwentyfive.php
@@ -58,7 +58,7 @@ class FluidCheckout_ThemeCompat_TwentyTwentyFive extends FluidCheckout {
 	 * @param  array  $classes  The button classes.
 	 */
 	public function add_button_class( $classes ) {
-		// Add 'wp-element-button' class to apply theme styles and 'qodef-m-checkout-link' to add hover animation
+		// Add 'wp-element-button' class to apply theme styles
 		if ( is_array( $classes ) ) {
 			array_push( $classes, 'wp-element-button' );
 		} 
