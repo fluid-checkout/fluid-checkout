c7cad7761c55b5f850f7dca1c28dca480d511c4f
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2026-06-29 11:06:06 -0300

    Add support to disable Avada dropdown styles on Fluid Checkout pages

--- inc/compat/themes/compat-theme-Avada.php
+++ inc/compat/themes/compat-theme-Avada.php
@@ -37,6 +37,9 @@ class FluidCheckout_ThemeCompat_Avada extends FluidCheckout {
 
 		// CSS variables
 		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+
+		// Avada dropdown styles
+		add_filter( 'avada_setting_get_avada_styles_dropdowns', array( $this, 'maybe_disable_avada_dropdown_styles' ), 10 );
 	}
 
 	/**
@@ -122,6 +125,20 @@ class FluidCheckout_ThemeCompat_Avada extends FluidCheckout {
 		}
 
 		return $attributes;
+	}
+
+
+
+	/**
+	 * Disable Avada dropdown styles on Fluid Checkout pages.
+	 *
+	 * @param  string|int  $value  The Avada dropdown styles option value.
+	 */
+	public function maybe_disable_avada_dropdown_styles( $value ) {
+		// Bail if not on checkout page or fragment
+		if ( ! FluidCheckout_Steps::instance()->is_checkout_page_or_fragment() ) { return $value; }
+
+		return '0';
 	}
 
 
