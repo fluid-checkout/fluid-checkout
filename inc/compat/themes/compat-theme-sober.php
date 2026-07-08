d1c5284ef39c3784a38a836243675d4099028010
Luiggi Bellincanta <luiggibellincanta@gmail.com>
2026-07-08 10:30:28 -0300

    Enhance Sober theme compatibility by adding late hooks and CSS variable integration. Remove unused SCSS file and update color variables for consistency across styles.

--- inc/compat/themes/compat-theme-sober.php
+++ inc/compat/themes/compat-theme-sober.php
@@ -19,13 +19,27 @@ class FluidCheckout_ThemeCompat_Sober extends FluidCheckout {
 	 * Initialize hooks.
 	 */
 	public function hooks() {
-		// Set class name from the theme
+		// Late hooks
+		add_action( 'wp', array( $this, 'late_hooks' ), 150 );
+
+		// CSS variables
+		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+	}
+
+
+
+	/**
+	 * Add or remove late hooks.
+	 */
+	public function late_hooks() {
+		// Define class name
 		$class_name = 'Sober_WooCommerce';
 
-		// Bail if class is not available.
-		if ( ! class_exists( $class_name ) ) {
-			return;
-		}
+		// Bail if class is not available
+		if ( ! class_exists( $class_name ) ) { return; }
+
+		// Bail if class method is not available
+		if ( ! method_exists( $class_name, 'instance' ) ) { return; }
 
 		// Get class object
 		$class_object = call_user_func( array( $class_name, 'instance' ) );
@@ -34,9 +48,7 @@ class FluidCheckout_ThemeCompat_Sober extends FluidCheckout {
 		remove_action( 'woocommerce_checkout_before_customer_details', array( $class_object, 'billing_title' ), 10 );
 		remove_filter( 'woocommerce_loop_add_to_cart_link', array( $class_object, 'add_to_cart_catalog_button' ), 10, 3 );
 		remove_filter( 'woocommerce_cart_item_quantity', array( $class_object, 'cart_item_quantity' ), 10, 3 );
-
-		// CSS variables
-		add_action( 'fc_css_variables', array( $this, 'add_css_variables' ), 20 );
+		remove_filter( 'woocommerce_form_field_args', array( $class_object, 'form_field_args' ), 10, 2 );
 	}
 
 
@@ -54,12 +66,13 @@ class FluidCheckout_ThemeCompat_Sober extends FluidCheckout {
 				'--fluidcheckout--field--height' => 'auto',
 				'--fluidcheckout--field--padding-left' => '0',
 				'--fluidcheckout--field--font-size' => '16px',
+				'--fluidcheckout--field--text-color' => '#23232c',
 
 				// Border styles
 				'--fluidcheckout--field--border-color' => '#e4e6eb',
 				'--fluidcheckout--field--border-width' => '2px',
 				'--fluidcheckout--field--border-style' => 'solid',
-				
+
 			),
 		);
 
